bdb214b12146748fafcd790bc058a03847ce1367
HEI <contact@hei.school>
2021-10-14 16:18:04 +0200

    feat: GET /student-timetable and /student-grades

--- src/providers/dataProvider.js
+++ src/providers/dataProvider.js
@@ -2,10 +2,25 @@ import jsonServerProvider from 'ra-data-json-server'
 
 const haDataProvider = jsonServerProvider('https://virtserver.swaggerhub.com/hei-admin/hei-admin_api/1.0')
 
-const connectedUserId = 'string'
+const connectedUserId = 'string' // TODO: connect with redux-store
 
 const dataProvider = {
   getList(resource, params) {
+    if (resource === 'student-timetable' || resource === 'student-grades') {
+      const student = this.getOne('profile', params)
+      return student.then(response => {
+        const enrolled_courses = response.data.enrolled_courses
+        response.data = enrolled_courses.map(course => {
+          return {
+            id: course.course_id, // RA needs an id but HA provide course_id instead
+            group_name: response.data.group.name,
+            ...course
+          }
+        })
+        response.total = enrolled_courses.length
+        return response
+      })
+    }
     return haDataProvider.getList(resource, params)
   },
   getOne(resource, params) {
