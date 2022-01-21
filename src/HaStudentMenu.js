3e1dd7b1aaa22964dbc1411904c18f226763ae4f
HEI <contact@hei.school>
2022-01-21 17:15:39 +0100

    fix: do not let non-implemented buttons crash in student view

--- src/HaStudentMenu.js
+++ src/HaStudentMenu.js
@@ -3,12 +3,15 @@ import Receipt from '@material-ui/icons/Receipt'
 import Money from '@material-ui/icons/AttachMoney'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
-export const HaStudentMenu = props => (
-  <Menu {...props}>
-    <MenuItemLink replace to='/profile' primaryText='Mon profil' leftIcon={<AccountCircleIcon />} />
-    <MenuItemLink replace to='/fees' primaryText='Frais' leftIcon={<Money />} />
-    <MenuItemLink replace to='/student-grades' primaryText='Notes' leftIcon={<Receipt />} />
-  </Menu>
-)
+export const HaStudentMenu = props => {
+  const alertNotImplemented = () => alert('En cours de développement. Ce qui présage quelques exercices pour vous ;)')
+  return (
+    <Menu {...props}>
+      <MenuItemLink replace to='/profile' primaryText='Mon profil' leftIcon={<AccountCircleIcon />} />
+      <MenuItemLink replace to='/' primaryText='Frais' leftIcon={<Money />} onClick={alertNotImplemented} />
+      <MenuItemLink replace to='/' primaryText='Notes' leftIcon={<Receipt />} onClick={alertNotImplemented} />
+    </Menu>
+  )
+}
 
 export default HaStudentMenu
