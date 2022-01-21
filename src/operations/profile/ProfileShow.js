35984d049a8e4a2e5d38313c699ea734d91e5304
HEI <contact@hei.school>
2022-01-21 20:11:36 +0100

    feat: change request on profile

--- src/operations/profile/ProfileShow.js
+++ src/operations/profile/ProfileShow.js
@@ -1,12 +1,23 @@
 import { DateField, EmailField, FunctionField, SimpleShowLayout, Show, TextField } from 'react-admin'
+import { Typography } from '@material-ui/core'
+
 import { useAsync } from 'react-async'
 import { useState, useEffect } from 'react'
 import authProvider from '../../providers/authProvider'
 
+const ChangeRequest = () => (
+  <div style={{ width: 200, margin: '1em' }}>
+    <Typography variant='h6'>Un changement ?</Typography>
+    <Typography variant='body2'>
+      Signalez-le immédiatement à <a href='mailto:contact@hei.school'>contact@hei.school</a> avec les justificatifs associés
+    </Typography>
+  </div>
+)
+
 const getPermission = async ({ authProvider }) => {
-  const res = await authProvider.getUserInformations()
-  return res
+  return await authProvider.getUserInformations()
 }
+
 const ProfileShow = () => {
   const [id, setId] = useState()
   const { data, isPending } = useAsync({ promiseFn: getPermission, authProvider: authProvider })
@@ -24,7 +35,7 @@ const ProfileShow = () => {
   const sexRenderer = user => (user.sex === 'M' ? 'Homme' : user.sex === 'F' ? 'Femme' : unexpectedValue)
   const statusRenderer = user => (user.status === 'ENABLED' ? 'Actif·ve' : user.status === 'DISABLED' ? withRedWarning('Suspendu·e') : unexpectedValue)
   return (
-    <Show id={id} resource='profile' basePath='/profile' title='Mon profil'>
+    <Show id={id} aside={<ChangeRequest />} resource='profile' basePath='/profile' title='Mon profil'>
       <SimpleShowLayout record={{ sex: 'Homme' }}>
         <TextField source='ref' label='Référence' />
         <TextField source='first_name' label='Prénom(s)' />
