33e8a04bed9bb0d191937a5a779a641b416cb835
HEI <contact@hei.school>
2022-01-21 19:53:36 +0100

    chore: change userMenu to logoutMenu

--- src/HaAppBar.js
+++ src/HaAppBar.js
@@ -1,4 +1,24 @@
 import { AppBar } from 'react-admin'
 
-const HaAppBar = props => <AppBar {...props} />
+import { IconButton, Tooltip } from '@material-ui/core'
+import LockIcon from '@material-ui/icons/Lock'
+import authProvider from './providers/authProvider'
+
+const LogoutMenu = props => {
+  const onClick = () => {
+    authProvider.logout()
+    window.location.reload()
+  }
+  return (
+    <div onClick={onClick}>
+      <Tooltip title='Se déconnecter'>
+        <IconButton color='inherit'>
+          <LockIcon />
+        </IconButton>
+      </Tooltip>
+    </div>
+  )
+}
+
+const HaAppBar = props => <AppBar {...props} userMenu={<LogoutMenu {...props} />} />
 export default HaAppBar
