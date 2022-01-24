425f822a3153d96a04cc97788131c2b89ff7d973
HEI <contact@hei.school>
2022-01-24 19:22:52 +0100

    chore: formatting

--- src/operations/teachers/TeacherCreate.js
+++ src/operations/teachers/TeacherCreate.js
@@ -1,17 +1,17 @@
 import { Create, SimpleForm, TextInput, DateTimeInput, DateInput } from 'react-admin'
 const TeacherCreate = props => (
-    <Create {...props} title='Enseignants'>
-      <SimpleForm>
-        <TextInput source='ref' label='Référence' />
-        <TextInput source='first_name' label='Prénoms' />
-        <TextInput source='last_name' label='Nom' />
-        <TextInput source='sex' label='Sexe' />
-        <TextInput source='phone' label='Téléphone' />
-        <DateInput source='birth_date' label='Date de naissance' />
-        <TextInput source='address' label='Adresse' />
-        <TextInput source='email' label='Email' />
-        <DateTimeInput source='entrance_datetime' label="Date d'entrée chez HEI" />
-      </SimpleForm>
-    </Create>
+  <Create {...props} title='Enseignants'>
+    <SimpleForm>
+      <TextInput source='ref' label='Référence' />
+      <TextInput source='first_name' label='Prénoms' />
+      <TextInput source='last_name' label='Nom' />
+      <TextInput source='sex' label='Sexe' />
+      <TextInput source='phone' label='Téléphone' />
+      <DateInput source='birth_date' label='Date de naissance' />
+      <TextInput source='address' label='Adresse' />
+      <TextInput source='email' label='Email' />
+      <DateTimeInput source='entrance_datetime' label="Date d'entrée chez HEI" />
+    </SimpleForm>
+  </Create>
 )
 export default TeacherCreate
