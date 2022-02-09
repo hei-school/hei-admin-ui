34407b39601dfae9b9db856668fa4572c2f3a71b
HEI <contact@hei.school>
2022-02-09 19:59:23 +0100

    chore: styles

--- src/operations/profile/ProfileEdit.js
+++ src/operations/profile/ProfileEdit.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { SimpleForm, TextInput, DateInput, BooleanInput, Edit } from 'react-admin'
 import EditToolbar from '../utils/EditToolBar'
+import SexRadioButton from '../utils/SexRadioButton'
 
 const ProfileEdit = props => (
   <Edit {...props}>
     <SimpleForm toolbar={<EditToolbar />}>
-      <TextInput source='first_name' label='Prénom·s' />
-      <TextInput source='last_name' label='Nom·s' />
-      <TextInput source='email' />
-      <TextInput source='address' label='Adresse' />
-      <TextInput source='sex' label='Sexe' />
-      <DateInput source='birth_date' label='Date de naissance' />
-      <DateInput source='entrance_datetime' label="Date d'entrée chez HEI" />
-      <BooleanInput source='status' label='Actif·ve ?' />
+      <TextInput source='first_name' label='Prénom·s' fullWidth={true} />
+      <TextInput source='last_name' label='Nom·s' fullWidth={true} />
+      <TextInput source='email' fullWidth={true} />
+      <TextInput multiline source='address' label='Adresse' fullWidth={true} />
+      <SexRadioButton />
+      <DateInput source='birth_date' label='Date de naissance' fullWidth={true} />
+      <DateInput source='entrance_datetime' label="Date d'entrée chez HEI" fullWidth={true} />
+      <BooleanInput source='status' label='Actif·ve ?' fullWidth={true} />
     </SimpleForm>
   </Edit>
 )
