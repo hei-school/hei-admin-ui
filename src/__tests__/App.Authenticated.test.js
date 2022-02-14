b43fcc561b83a285c94d0904a5d2e7f0986bbb69
HEI <contact@hei.school>
2022-02-14 19:55:35 +0100

    chore: App tests working for authenticated and unauthenticated

---
+++ src/__tests__/App.Authenticated.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+
+jest.doMock('aws-amplify')
+jest.doMock('../providers/authProvider', () => ({
+  login: () => Promise.resolve(),
+  logout: () => Promise.resolve(),
+  checkAuth: () => Promise.resolve(),
+  getIdentity: () => Promise.resolve('whoami.id'),
+  getPermissions: () => Promise.resolve(['whoami.role']),
+  getToken: () => Promise.resolve('dummy'),
+  isNewPassword: () => false
+}))
+const App = require('../App').default
+
+describe('App', () => {
+  it('is no longer on login page if authentication succeeded', async () => {
+    render(<App />)
+
+    expect(screen.queryByText('Connexion')).toBeNull() // we are not asked to connect anymore
+  })
+})
