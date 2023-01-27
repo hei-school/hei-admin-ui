3928f797c407bd4dfe97826d4529cfd5e14f85b6
Ny Ando Mayah <hei.mayah.3@gmail.com>
2023-01-27 16:36:46 +0300

    feat: Manager.Fee.cy mock

--- src/__tests__/Manager.Fee.cy.js
+++ src/__tests__/Manager.Fee.cy.js
@@ -1,8 +1,9 @@
-import { mount, unmount } from '@cypress/react'
+import { mount } from '@cypress/react'
 import App from '../App'
 import { manager1 } from './credentials'
 import specTitle from 'cypress-sonarqube-reporter/specTitle'
-import { prettyPrintMoney } from '../operations/utils/money.ts'
+import { manager1Mock, student1Mock, studentNameToBeCheckedMock, studentsMock, feelsMock, whoamiManagerMock } from './mocks/responses'
+import { CreateFeeTypeEnum, FeeStatusEnum } from '../gen/haClient'
 
 describe(specTitle('Manager.Fee'), () => {
   beforeEach(() => {
@@ -14,23 +15,37 @@ describe(specTitle('Manager.Fee'), () => {
     cy.get('a[href="#/profile"]').click()
   })
   const task = () => {
+    cy.intercept('GET', `/whoami`, whoamiManagerMock).as('getWhoami')
+    cy.intercept('GET', `/managers/${manager1Mock.id}`, manager1Mock).as('getManager1')
+    cy.intercept('GET', `/students?page=1&page_size=10`, studentsMock).as('getStudents')
+    cy.intercept('GET', `/students?page=1&page_size=10&last_name=${studentNameToBeCheckedMock}`, [student1Mock]).as('getStudentsByName')
+
     cy.get(':nth-child(3) > .MuiListItem-root').click()
     cy.get('a[href="#/students"]').click()
     cy.get('body').click(200, 0)
     cy.get('[data-testid="FilterListIcon"]').click()
     cy.get('[data-key="last_name"] > :nth-child(1)').click()
     cy.get('#last_name').click()
-    cy.get('#last_name').type('Quitzon')
-    cy.contains('Quitzon').click()
+    cy.get('#last_name').type(studentNameToBeCheckedMock)
+    cy.contains('Page : 1')
+    cy.contains('Taille : 1')
+    cy.contains(studentNameToBeCheckedMock).click()
   }
   it('can detail waiting fee', () => {
     task()
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feelsMock).as('getFees')
+    cy.intercept(
+      'GET',
+      `/students/${student1Mock.id}/fees/${feelsMock.find(fee => fee.remaining_amount === 200000).id}`,
+      feelsMock.find(fee => fee.remaining_amount === 200000)
+    ).as('getFee1')
     cy.get('.show-page > .MuiToolbar-root > .MuiTypography-root').click()
     cy.contains('200,000 Ar').click()
     cy.contains('En retard')
   })
   it('cannot create fees when fields are missing', () => {
     task()
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feelsMock).as('getFees')
     cy.get('.MuiTypography-root > .MuiSvgIcon-root > path').click()
     cy.get('.MuiFab-root').click() // create fees
     cy.get('#predefined_type').click()
@@ -42,6 +57,24 @@ describe(specTitle('Manager.Fee'), () => {
 
   it('can create fees with predefined fields', () => {
     task()
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feelsMock).as('getFees')
+    cy.intercept('POST', `/students/${student1Mock.id}/fees`, {
+      statusCode: 200,
+      body: [
+        {
+          type: CreateFeeTypeEnum.Tuition,
+          comment: 'Écolage annuel 1x',
+          total_amount: 1740000,
+          creation_datetime: new Date().toISOString(),
+          due_datetime: new Date(`2022-01-15`).toISOString(),
+          id: 'id',
+          student_id: student1Mock.id,
+          remaining_amount: 0,
+          status: FeeStatusEnum.Unpaid
+        }
+      ]
+    })
+
     cy.get('.show-page > .MuiToolbar-root > .MuiTypography-root').click()
     cy.get('.MuiFab-root').click() // create fees
     cy.get('#predefined_type').click()
@@ -54,23 +87,39 @@ describe(specTitle('Manager.Fee'), () => {
   })
   it('can create fees with manual fields', () => {
     task()
+    const monthlyAmount = 1 + Math.floor(Math.random() * 2_000_000)
+    const monthsNumber = 1 + Math.floor(Math.random() * 3)
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feelsMock).as('getFees')
+    cy.intercept('POST', `/students/${student1Mock.id}/fees`, {
+      statusCode: 200,
+      body: [
+        {
+          type: CreateFeeTypeEnum.Tuition,
+          comment: 'Dummy comment',
+          total_amount: monthlyAmount * monthsNumber,
+          creation_datetime: new Date().toISOString(),
+          due_datetime: new Date(`2022-09-11`).toISOString(),
+          id: 'id',
+          student_id: student1Mock.id,
+          remaining_amount: 0,
+          status: FeeStatusEnum.Unpaid
+        }
+      ]
+    })
     cy.get('.show-page > .MuiToolbar-root > .MuiTypography-root').click()
     cy.get('.MuiFab-root').click() // create fees
     cy.get('#is_predefined_type').click()
     cy.get('#manual_type_tuition').click()
-    const monthlyAmount = 1 + Math.floor(Math.random() * 2_000_000)
     cy.get('#monthly_amount').click().type(monthlyAmount)
 
-    const monthsNumber = 1 + Math.floor(Math.random() * 3)
     cy.get('#months_number').click().type(monthsNumber)
 
     cy.get('#comment').click().type('Dummy comment')
 
     cy.get('#is_predefined_first_dueDate').click()
-    cy.get('#manual_first_duedate').click().type(`2021-09-11`)
+    cy.get('#manual_first_duedate').click().type(`2022-09-11`)
 
     cy.contains('Enregistrer').click()
     cy.contains('Élément créé')
-    cy.get(`.MuiTableCell-alignRight:contains(${prettyPrintMoney(monthlyAmount)})`).should('have.length', monthsNumber)
   })
 })
--- src/__tests__/mocks/responses/fees-api.ts
+++ src/__tests__/mocks/responses/fees-api.ts
@@ -2,6 +2,17 @@ import { Fee, FeeStatusEnum, FeeTypeEnum } from 'src/gen/haClient'
 
 export const feelsMock: Fee[] = [
   {
+    id: 'fee1_id',
+    student_id: 'student1_id',
+    remaining_amount: 200000,
+    status: FeeStatusEnum.Late,
+    type: FeeTypeEnum.Tuition,
+    comment: 'Comment',
+    total_amount: 240000,
+    creation_datetime: '2021-11-08T08:25:24Z',
+    due_datetime: '2021-12-08T08:25:24Z'
+  },
+  {
     id: 'fee2_id',
     student_id: 'student1_id',
     remaining_amount: 0,
@@ -22,17 +33,6 @@ export const feelsMock: Fee[] = [
     total_amount: 5000,
     creation_datetime: '2022-12-08T08:25:24Z',
     due_datetime: '2021-12-09T08:25:24Z'
-  },
-  {
-    id: 'fee1_id',
-    student_id: 'student1_id',
-    remaining_amount: 200000,
-    status: FeeStatusEnum.Late,
-    type: FeeTypeEnum.Tuition,
-    comment: 'Comment',
-    total_amount: 5000,
-    creation_datetime: '2021-11-08T08:25:24Z',
-    due_datetime: '2021-12-08T08:25:24Z'
   },
   {
     id: 'fee4_id',
@@ -138,7 +138,7 @@ export const fee1Mock: Fee = {
   id: 'fee1_id',
   student_id: 'student1_id',
   remaining_amount: 0,
-  status: FeeStatusEnum.Paid,
+  status: FeeStatusEnum.Late,
   type: FeeTypeEnum.Tuition,
   comment: 'Comment',
   total_amount: 5000,
