37414c31b7bcadad3bf95d46dd5a9b588db12f0a
mayah <hei.mayah.3@gmail.com>
2023-02-28 14:46:06 +0300

    fix: can edit students

--- src/providers/studentProvider.ts
+++ src/providers/studentProvider.ts
@@ -12,17 +12,17 @@ const studentProvider: HaDataProviderType = {
     return result.data
   },
   async saveOrUpdate(payload: any) {
-    if (payload.length == 1) {
-      // for editing
-      const result = await usersApi().createOrUpdateStudents(payload)
-      return result.data
-    } else {
+    if (payload[0].length > 1) {
       // when we want to create student
       const [fees, student] = payload[0]
       Object.assign(student, { status: EnableStatus.Enabled })
       const [studentResponse] = (await usersApi().createOrUpdateStudents([student])).data
       fees.length !== 0 && (await payingApi().createStudentFees(studentResponse?.id!, fees))
       return [studentResponse]
+    } else {
+      // for editing
+      const result = await usersApi().createOrUpdateStudents(payload)
+      return result.data
     }
   }
 }
