26ef2fc71aac463ba0fecc19175d91c99fb2a577
Ny Ando Mayah <hei.mayah.3@gmail.com>
2023-03-09 15:12:45 +0300

    refactor: add the new prop created in all the instance of CustomDateField

--- src/operations/fees/ByStatusFeeList.js
+++ src/operations/fees/ByStatusFeeList.js
@@ -24,10 +24,10 @@ const ByStatusFeeList = ({ status, ...props }) => {
       perPage={maxPageSize}
     >
       <Datagrid rowClick={id => `/fees/${id}/show`} rowStyle={rowStyle}>
-        <CustomDateField source='due_datetime' label='Date limite' />
+        <CustomDateField source='due_datetime' label='Date limite' showTime={false}/>
         <TextField source='comment' label='Commentaire' />
         <FunctionField label='Reste à payer' render={record => prettyPrintMoney(record.remaining_amount)} textAlign='right' />
-        <CustomDateField source='creation_datetime' label='Date de création' />
+        <CustomDateField source='creation_datetime' label='Date de création' showTime={false}/>
         <ShowButton basePath='/fees' />
       </Datagrid>
     </List>
