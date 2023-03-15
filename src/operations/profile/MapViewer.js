5c868318773a34e95d20439ecb5983e32d40501b
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-03-15 21:34:08 +0300

    chore: add popup index location

--- src/operations/profile/MapViewer.js
+++ src/operations/profile/MapViewer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup } from 'react-leaflet'
 import { MarkerLayer, Marker } from 'react-leaflet-marker'
 import { Box } from '@mui/material'
 import 'leaflet/dist/leaflet.css'
@@ -15,6 +15,9 @@ const MapViewer = ({ latitude, longitude }) => {
     <MapContainer center={position} zoom={20} style={{ height: '95%', width: 'min(100vw, 600px)' }} scrollWheelZoom={false}>
       <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' />
       <MarkerLayer>
+        <Popup position={position} size={[70, 70]}>
+          latitude: {latitude}, longitude: {longitude}
+        </Popup>
         <Marker position={position} placement='center' size={[70, 70]}>
           <Box borderRadius='50%' {...customMarkerProps} />
         </Marker>
