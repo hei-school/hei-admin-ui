3b5aef74fd88c0a385db4cc29e35d08ec5c9ccd4
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-03-16 13:25:08 +0300

    feat(map): make map switchable to satellite view

--- src/operations/profile/MapViewer.js
+++ src/operations/profile/MapViewer.js
@@ -1,19 +1,28 @@
 import React from 'react'
-import { MapContainer, TileLayer, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, LayersControl } from 'react-leaflet'
 import { MarkerLayer, Marker } from 'react-leaflet-marker'
 import { Box } from '@mui/material'
 import 'leaflet/dist/leaflet.css'
 
 const MapViewer = ({ latitude, longitude }) => {
   const position = { lat: latitude, lng: longitude }
+  const { BaseLayer } = LayersControl
 
   const customMarkerProps = {
     bgcolor: 'rgba(255, 0, 0, 0.36)',
     style: { width: '5rem', height: '5rem' }
   }
+
   return (
-    <MapContainer center={position} zoom={20} style={{ height: '95%', width: 'min(100vw, 600px)' }} scrollWheelZoom={false}>
-      <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' />
+    <MapContainer center={position} zoom={19} style={{ height: '95%', width: 'min(100vw, 600px)' }} scrollWheelZoom={false}>
+      <LayersControl>
+        <BaseLayer checked name='Vue standard'>
+          <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' maxZoom={19} />
+        </BaseLayer>
+        <BaseLayer name='Vue satellite'>
+          <TileLayer url='http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}' maxZoom={19} />
+        </BaseLayer>
+      </LayersControl>
       <MarkerLayer>
         <Popup position={position} size={[70, 70]}>
           latitude: {latitude}, longitude: {longitude}
