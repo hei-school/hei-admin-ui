2b6c9c5eb017846ff67129cc17132f6ea1f1c670
Ranomenjanahary <hei.jaonary@gmail.com>
2023-03-16 19:24:11 +0300

    feat: add geolocation adress

--- src/operations/profile/ProfileShow.js
+++ src/operations/profile/ProfileShow.js
@@ -2,6 +2,7 @@ import { EmailField, FunctionField, SimpleShowLayout, Show, TextField } from 're
 import { Link } from '@mui/material'
 import authProvider from '../../providers/authProvider'
 import { unexpectedValue, CustomDateField } from '../utils'
+import GpsAdress from './gpsAdress'
 
 export const ProfileLayout = () => {
   const sexRenderer = user => {
@@ -9,6 +10,21 @@ export const ProfileLayout = () => {
     if (user.sex === 'F') return 'Femme'
     return unexpectedValue
   }
+  // const position = [ -18.913, 47.5296 ]
+        
+  //   render(
+  //       <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+  //         <TileLayer
+  //           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  //         />
+  //         <Marker position={position}>
+  //           <Popup>
+  //             A pretty CSS3 popup. <br /> Easily customizable.
+  //           </Popup>
+  //         </Marker>
+  //       </MapContainer>
+  //   )
   const statusRenderer = user => {
     if (user.status === 'ENABLED') return 'Actif·ve'
     if (user.status === 'DISABLED') return 'Suspendu·e'
@@ -24,6 +40,7 @@ export const ProfileLayout = () => {
       <FunctionField label='Téléphone' render={phoneRenderer} />
       <CustomDateField source='birth_date' label='Date de naissance' showTime={false} />
       <TextField source='address' label='Adresse' component='pre' />
+      <FunctionField label='Localisation' render={GpsAdress} />
       <EmailField source='email' label='Email' />
       <CustomDateField source='entrance_datetime' label="Date d'entrée chez HEI" showTime={false} />
       <FunctionField label='Statut' render={statusRenderer} />
