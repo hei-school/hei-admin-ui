cef03e2075ff8220fed52a8e4ee2c34cdac10b86
mayah <hei.mayah.3@gmail.com>
2023-03-18 01:15:24 +0300

    feat: implement geolocalisation for student

--- src/operations/profile/ProfileEdit.js
+++ src/operations/profile/ProfileEdit.js
@@ -1,40 +1,50 @@
-import { SimpleForm, TextInput, DateInput, RadioButtonGroupInput, Edit } from 'react-admin'
+import { EmailField, FunctionField, SimpleShowLayout, Show, TextField } from 'react-admin'
+import { Link } from '@mui/material'
+import authProvider from '../../providers/authProvider'
+import { unexpectedValue, CustomDateField } from '../utils'
+import { WhoamiRoleEnum } from '../../gen/haClient'
 
-import { SexRadioButton, EditToolBar } from '../utils'
-
-const StatusRadioButton = () => (
-  <RadioButtonGroupInput
-    source='status'
-    label='Statut'
-    choices={[
-      { id: 'ENABLED', name: 'Actif·ve' },
-      { id: 'DISABLED', name: 'Inactif·ve' }
-    ]}
-  />
-)
-const transformUser = user => {
-  let regex = /\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\dZ/i // format 2023-02-28T21:00:00.00Z
-  let regex2 = /\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\Z/i // 2023-02-28T21:00:00Z
-  let condition = !regex.test(user.entrance_datetime) && !regex2.test(user.entrance_datetime)
-  user.entrance_datetime = condition ? user.entrance_datetime.concat('T10:00:00.000Z') : user.entrance_datetime
-  return user
+export const ProfileLayout = () => {
+  const sexRenderer = user => {
+    if (user.sex === 'M') return 'Homme'
+    if (user.sex === 'F') return 'Femme'
+    return unexpectedValue
+  }
+  const statusRenderer = user => {
+    if (user.status === 'ENABLED') return 'Actif·ve'
+    if (user.status === 'DISABLED') return 'Suspendu·e'
+    return unexpectedValue
+  }
+  const role = authProvider.getCachedRole()
+  const phoneRenderer = data => <Link href={`tel:${data.phone}`}>{data.phone}</Link>
+  const addressRenderer = data => <Link href={`https://www.google.com/maps?q=${data.address_latitude},${data.address_longitude}`}>{data.address}</Link>
+  return (
+    <SimpleShowLayout>
+      <TextField source='ref' label='Référence' />
+      <TextField source='first_name' id='first_name' label='Prénom(s)' />
+      <TextField source='last_name' label='Nom(s)' />
+      <FunctionField label='Sexe' render={sexRenderer} />
+      <FunctionField label='Téléphone' render={phoneRenderer} />
+      <CustomDateField source='birth_date' label='Date de naissance' showTime={false} />
+      {role === WhoamiRoleEnum.Student ? (
+        <TextField source='address' label='Adresse' component='pre' />
+      ) : (
+        <FunctionField label='Adresse' render={addressRenderer} component='pre' />
+      )}
+      <EmailField source='email' label='Email' />
+      <CustomDateField source='entrance_datetime' label="Date d'entrée chez HEI" showTime={false} />
+      <FunctionField label='Statut' render={statusRenderer} />
+    </SimpleShowLayout>
+  )
 }
 
-const ProfileEdit = () => (
-  <Edit transform={transformUser}>
-    <SimpleForm toolbar={<EditToolBar />}>
-      <TextInput source='ref' label='Référence' fullWidth={true} />
-      <TextInput source='first_name' label='Prénom·s' fullWidth={true} />
-      <TextInput source='last_name' label='Nom·s' fullWidth={true} />
-      <TextInput source='email' fullWidth={true} />
-      <TextInput multiline source='address' label='Adresse' fullWidth={true} />
-      <SexRadioButton />
-      <TextInput source='phone' label='Téléphone' fullWidth={true} />
-      <DateInput source='birth_date' label='Date de naissance' fullWidth={true} />
-      <DateInput source='entrance_datetime' label="Date d'entrée chez HEI" fullWidth={true} />
-      <StatusRadioButton />
-    </SimpleForm>
-  </Edit>
-)
+const ProfileShow = () => {
+  const id = authProvider.getCachedWhoami().id
+  return (
+    <Show id={id} resource='profile' basePath='/profile' title='Mon profil'>
+      <ProfileLayout />
+    </Show>
+  )
+}
 
 export default ProfileEdit
