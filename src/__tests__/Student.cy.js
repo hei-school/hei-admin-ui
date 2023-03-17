2c3d198d83cc059606e9eb6eb4d8d1efa0e1fbe4
heiherilala <hei.herilala@gmail.com>
2023-03-17 21:47:37 +0300

    test: check the correct show on the location of students

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -1,15 +1,15 @@
-import { mount } from '@cypress/react'
+import { mount, unmount } from '@cypress/react'
 import App from '../App'
 import { student1 } from './credentials'
 import specTitle from 'cypress-sonarqube-reporter/specTitle'
-import { createPaymentMock, feesMock, student1Mock, studentNameToBeCheckedMock, whoamiStudentMock } from './mocks/responses'
+import { createPaymentMock, feesMock, student1Mock, studentNameToBeCheckedMock, student1WithLocationMock, student1withoutLocationMock, whoamiStudentMock } from './mocks/responses'
 
 describe(specTitle('Student'), () => {
   beforeEach(() => {
     mount(<App />)
     cy.get('#username').type(student1.username)
     cy.get('#password').type(student1.password)
-    cy.get('button').contains('Connexion').click()
+    
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.intercept('GET', `/students/${student1Mock.id}`, student1Mock).as('getStudent')
     cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as(
@@ -24,10 +24,26 @@ describe(specTitle('Student'), () => {
   })
 
   it('lands on profile page if succeeds', () => {
+    cy.get('button').contains('Connexion').click()
     cy.get('#first_name').contains(studentNameToBeCheckedMock)
+    unmount()
+  })
+
+  it('lands on profile page if succeeds', () => {
+    cy.intercept('GET', `/students/${student1Mock.id}`, student1WithLocationMock).as('getStudent')
+    cy.get('button').contains('Connexion').click()
+    cy.get('#localisation').contains(student1WithLocationMock.location.latitude)
+    cy.get('#localisation').contains(student1WithLocationMock.location.longitude)
+  })
+
+  it('lands on profile page if succeeds', () => {
+    cy.intercept('GET', `/students/${student1Mock.id}`, student1withoutLocationMock).as('getStudent')
+    cy.get('button').contains('Connexion').click()
+    cy.get('#localisation').contains("Coordonnées absante")
   })
 
   it('can detail fee (click on fee row)', () => {
+    cy.get('button').contains('Connexion').click()
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
     cy.get('body').click(200, 0) //note(uncover-menu)
@@ -36,6 +52,7 @@ describe(specTitle('Student'), () => {
   })
 
   it('can detail fee (click on fee button)', () => {
+    cy.get('button').contains('Connexion').click()
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
     cy.get('body')
       .click(200, 0) //note(uncover-menu)
