1602ad57c0a6eeeb3454775046bdacc824c97bc7
Fabien <hei.fabien.2@gmail.com>
2023-03-17 23:43:24 +0300

    feat : add location

--- src/operations/profile/ProfileShow.js
+++ src/operations/profile/ProfileShow.js
@@ -7,20 +7,17 @@ import { unexpectedValue, CustomDateField } from '../utils'
 import { useState } from "react";
 
 export const ProfileLayout = () => {
-  const sexRenderer = (user) => {
-    if (user.sex === "M") return "Homme";
-    if (user.sex === "F") return "Femme";
-    return unexpectedValue;
-  };
-  const statusRenderer = (user) => {
-    if (user.status === "ENABLED") return "Actif·ve";
-    if (user.status === "DISABLED") return "Suspendu·e";
-    return unexpectedValue;
-  };
-  const phoneRenderer = (data) => (
-    <Link href={`tel:${data.phone}`}>{data.phone}</Link>
-  );
-
+  const sexRenderer = user => {
+    if (user.sex === 'M') return 'Homme'
+    if (user.sex === 'F') return 'Femme'
+    return unexpectedValue
+  }
+  const statusRenderer = user => {
+    if (user.status === 'ENABLED') return 'Actif·ve'
+    if (user.status === 'DISABLED') return 'Suspendu·e'
+    return unexpectedValue
+  }
+  const phoneRenderer = data => <Link href={`tel:${data.phone}`}>{data.phone}</Link>
   const [open, setOpen] = useState(false);
   const handleClickOpen = () => {
     setOpen(true);
@@ -60,42 +57,30 @@ export const ProfileLayout = () => {
       </>
     );
   };
-
   return (
     <SimpleShowLayout>
-      <TextField source="ref" label="Référence" />
-      <TextField source="first_name" id="first_name" label="Prénom(s)" />
-      <TextField source="last_name" label="Nom(s)" />
-      <FunctionField label="Sexe" render={sexRenderer} />
-      <FunctionField label="Téléphone" render={phoneRenderer} />
-      <CustomDateField
-        source="birth_date"
-        label="Date de naissance"
-        showTime={false}
-      />
-      <TextField source="address" label="Adresse" component="pre" />
-      <FunctionField
-        label="Localisation"
-        render={() => LocalisationRenderer()}
-      />
-      <EmailField source="email" label="Email" />
-      <CustomDateField
-        source="entrance_datetime"
-        label="Date d'entrée chez HEI"
-        showTime={false}
-      />
-      <FunctionField label="Statut" render={statusRenderer} />
+      <TextField source='ref' label='Référence' />
+      <TextField source='first_name' id='first_name' label='Prénom(s)' />
+      <TextField source='last_name' label='Nom(s)' />
+      <FunctionField label='Sexe' render={sexRenderer} />
+      <FunctionField label='Téléphone' render={phoneRenderer} />
+      <CustomDateField source='birth_date' label='Date de naissance' showTime={false} />
+      <TextField source='address' label='Adresse' component='pre' />
+      <EmailField source='email' label='Email' />
+      <CustomDateField source='entrance_datetime' label="Date d'entrée chez HEI" showTime={false} />
+      <FunctionField label='Statut' render={statusRenderer} />
     </SimpleShowLayout>
-  );
-};
+  )
+}
 
 const ProfileShow = () => {
-  const id = authProvider.getCachedWhoami().id;
+  const id = authProvider.getCachedWhoami().id
   return (
-    <Show id={id} resource="profile" basePath="/profile" title="Mon profil">
+    <Show id={id} resource='profile' basePath='/profile' title='Mon profil'>
       <ProfileLayout />
     </Show>
-  );
-};
+  )
+}
 
-export default ProfileShow;
+
+export default ProfileShow
