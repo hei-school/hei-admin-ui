10adbb86e8485b9a260d505a251ca75d5ccc693e
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-18 00:23:13 +0300

    feat: add GPS attribut in student profile show

--- src/operations/profile/ProfileShow.js
+++ src/operations/profile/ProfileShow.js
@@ -15,6 +15,15 @@ export const ProfileLayout = () => {
     return unexpectedValue
   }
   const phoneRenderer = data => <Link href={`tel:${data.phone}`}>{data.phone}</Link>
+
+  const latitudeRenderer = data => {
+    return data.location.latitude ? data.location.latitude : "Non renseigné"
+  }
+
+  const longitudeRenderer = data => {
+    return data.location.longitude ? data.location.longitude : "Non renseigné"
+  }
+
   return (
     <SimpleShowLayout>
       <TextField source='ref' label='Référence' />
@@ -27,6 +36,8 @@ export const ProfileLayout = () => {
       <EmailField source='email' label='Email' />
       <CustomDateField source='entrance_datetime' label="Date d'entrée chez HEI" showTime={false} />
       <FunctionField label='Statut' render={statusRenderer} />
+      <FunctionField label='Latitude' render={latitudeRenderer} />
+      <FunctionField label='Longitude' render={longitudeRenderer} />
     </SimpleShowLayout>
   )
 }
