ac4e76b1ffe3a62f4403e0456796ddd77038cd93
mayah <hei.mayah.3@gmail.com>
2023-03-17 22:26:12 +0300

    feat: release of apiclient manually

--- src/gen/haClient/base.ts
+++ src/gen/haClient/base.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 /**
  * HEI Admin API
- * _Programmatically connect to a computer programming [school](https://hei.school)._ After [joining us](mailto:contact@hei.school), you can get an identification token from our [dev](https://dev-hei-admin.auth.eu-west-3.amazoncognito.com/oauth2/authorize?client_id=5s8cg50doahmu855rlc8fr6qmp&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fapi-dev.hei.school%2Fwhoami) or [prod](https://prod-hei-admin.auth.eu-west-3.amazoncognito.com/oauth2/authorize?client_id=i8bg538jpfu6mqmqb61m26trd&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fapi-prod.hei.school%2Fwhoami) authentication service. Then, start playing with our system!  The implementation of our API is [publicly disclosed](https://github.com/hei-school/hei-admin-api). You are welcome to try and compromise it. Happy hacking!
+ * _Programmatically connect to a computer programming [school](https://hei.school)._ After [joining us](mailto:contact@hei.school), you can get an identification token from our [dev](https://dev-hei-admin.auth.eu-west-3.amazoncognito.com/oauth2/authorize?client_id=5s8cg50doahmu855rlc8fr6qmp&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fapi-dev.hei.school%2Fwhoami) or [prod](https://prod-hei-admin.auth.eu-west-3.amazoncognito.com/oauth2/authorize?client_id=i8bg538jpfu6mqmqb61m26trd&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fapi-prod.hei.school%2Fwhoami) authentication service. Then, start playing with our system! The implementation of our API is [publicly disclosed](https://github.com/hei-school/hei-admin-api). You are welcome to try and compromise it. Happy hacking!
  *
  * The version of the OpenAPI document: 1.0
  *
@@ -12,13 +12,12 @@
  * Do not edit the class manually.
  */
 
-import type { Configuration } from './configuration'
+import { Configuration } from './configuration'
 // Some imports not used depending on template conditions
 // @ts-ignore
-import type { AxiosPromise, AxiosInstance, AxiosRequestConfig } from 'axios'
-import globalAxios from 'axios'
+import globalAxios, { AxiosPromise, AxiosInstance, AxiosRequestConfig } from 'axios'
 
-export const BASE_PATH = (process.env.REACT_APP_API_URL || 'undefined').replace(/\/+$/, '')
+export const BASE_PATH = 'https://api-dev.hei.school'.replace(/\/+$/, '')
 
 /**
  *
@@ -64,8 +63,8 @@ export class BaseAPI {
  * @extends {Error}
  */
 export class RequiredError extends Error {
+  name: 'RequiredError' = 'RequiredError'
   constructor(public field: string, msg?: string) {
     super(msg)
-    this.name = 'RequiredError'
   }
 }
