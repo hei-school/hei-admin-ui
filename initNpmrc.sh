262042a65ac0ec993ad50edeedb7cd9e3e1545f9
Fanirykeziah <99541592+Fanirykeziah@users.noreply.github.com>
2023-03-17 14:07:50 +0100

    Update initNpmrc.sh

--- initNpmrc.sh
+++ initNpmrc.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-aws codeartifact login --tool npm --repository "hei-store" --domain "npm-hei-school" --domain-owner "088312068315"
-hei-store npm-hei-school 088312068315
+aws codeartifact login --tool npm --repository "$1" --domain "$2" --domain-owner "$3"
