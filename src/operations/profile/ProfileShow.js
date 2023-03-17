adbaad56602719403409fc461497116fb3c74737
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-03-17 21:28:22 +0300

    feat:add GPS to student

--- src/operations/profile/ProfileShow.js
+++ src/operations/profile/ProfileShow.js
@@ -14,6 +14,10 @@ export const ProfileLayout = () => {
     if (user.status === 'DISABLED') return 'Suspendu·e'
     return unexpectedValue
   }
+  const addressGPSRenderer = user => {
+    if (user.addressGps === undefined) return 'No GPS location found'
+    return user.addressGps
+  }
   const phoneRenderer = data => <Link href={`tel:${data.phone}`}>{data.phone}</Link>
   return (
     <SimpleShowLayout>
@@ -24,6 +28,7 @@ export const ProfileLayout = () => {
       <FunctionField label='Téléphone' render={phoneRenderer} />
       <CustomDateField source='birth_date' label='Date de naissance' showTime={false} />
       <TextField source='address' label='Adresse' component='pre' />
+      <FunctionField label='GPS' render={addressGPSRenderer} />
       <EmailField source='email' label='Email' />
       <CustomDateField source='entrance_datetime' label="Date d'entrée chez HEI" showTime={false} />
       <FunctionField label='Statut' render={statusRenderer} />
