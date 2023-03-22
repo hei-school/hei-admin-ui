a2ba035c4bb5f00018f4502539b36988391f9f1d
mayah <hei.mayah.3@gmail.com>
2023-03-22 13:55:02 +0300

    refactor: utils and style

--- src/security/ConfirmForgotPassword.js
+++ src/security/ConfirmForgotPassword.js
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import authProvider from '../providers/authProvider'
 import { Typography } from '@mui/material'
-import { Button } from 'react-admin'
-import { indigo } from '@mui/material/colors'
-import { CustomTextField } from './utils'
+import { CustomTextField, CustomSubmitButton } from './utils'
 
 const ConfirmForgotPassword = ({ setUsername, setOpenSubmitModal, setOpenModal }) => {
   const [email, setEmail] = useState('')
@@ -16,15 +14,7 @@ const ConfirmForgotPassword = ({ setUsername, setOpenSubmitModal, setOpenModal }
     setOpenSubmitModal(true)
   }
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        justifyItems: 'center',
-        margin: 'auto'
-      }}
-    >
+    <div>
       <Typography
         variant='h7'
         sx={{
@@ -33,19 +23,8 @@ const ConfirmForgotPassword = ({ setUsername, setOpenSubmitModal, setOpenModal }
       >
         Un mail de confirmation avec un code vous sera envoyé
       </Typography>
-      <CustomTextField placeholder='Votre mail ici' onChange={handleEmailChange} type='text' />
-      <Button
-        onClick={sendEmail}
-        sx={{
-          backgroundColor: indigo[800],
-          width: '300px',
-          color: '#FFFF',
-          padding: '0.5vw',
-          margin: '0.75vw'
-        }}
-      >
-        ENVOYER
-      </Button>
+      <CustomTextField placeholder='Votre mail ici' onChange={handleEmailChange} type='email' />
+      <CustomSubmitButton onClick={sendEmail} text='ENVOYER' />
     </div>
   )
 }
