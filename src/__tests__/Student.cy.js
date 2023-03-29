e4687ece54a0708b43f8db092abb1dee882f3c78
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-29 22:20:36 +0200

    chore: test fees listing by student

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -5,42 +5,41 @@ import specTitle from 'cypress-sonarqube-reporter/specTitle'
 import { createPaymentMock, feesMock, student1Mock, studentNameToBeCheckedMock, whoamiStudentMock } from './mocks/responses'
 
 describe(specTitle('Student'), () => {
-  beforeEach(() => {
-    mount(<App />)
-    cy.get('#username').type(student1.username)
-    cy.get('#password').type(student1.password)
-    cy.get('button').contains('Connexion').click()
-    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
-    cy.intercept('GET', `/students/${student1Mock.id}`, student1Mock).as('getStudent')
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as(
-      'getPaymentsOfFee1'
-    )
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[0])).as(
-      'getPaymentsOfFee2'
-    )
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}`, feesMock[7 - 1]).as('getFee1')
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}`, feesMock[0]).as('getFee2')
-    cy.intercept('GET', `/whoami`, whoamiStudentMock).as('getWhoami')
-  })
-
-  it('lands on profile page if succeeds', () => {
-    cy.get('#first_name').contains(studentNameToBeCheckedMock)
-  })
-
-  it('can detail fee (click on fee row)', () => {
-    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
-    cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
-    cy.get('body').click(200, 0) //note(uncover-menu)
-    cy.contains('200,000 Ar').click()
-    cy.contains('En retard')
-  })
-
-  it('can detail fee (click on fee button)', () => {
-    cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
-    cy.get('body')
-      .click(200, 0) //note(uncover-menu)
-      .wait(['@getStudent', '@getWhoami'])
-    cy.get(':nth-child(7) > :nth-child(5)').click()
-    cy.contains('En retard')
-  })
+beforeEach(() => {
+mount(<App />)
+cy.get('#username').type(student1.username)
+cy.get('#password').type(student1.password)
+cy.get('button').contains('Connexion').click()
+cy.intercept('GET', /students/${student1Mock.id}/fees?page=1&page_size=500, feesMock).as('getFees')
+cy.intercept('GET', /students/${student1Mock.id}, student1Mock).as('getStudent')
+cy.intercept('GET', /students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10, createPaymentMock(feesMock[7 - 1])).as(
+'getPaymentsOfFee1'
+)
+cy.intercept('GET', /students/${student1Mock.id}/fees/${feesMock[0].id}/payments?page=1&page_size=10, createPaymentMock(feesMock[0])).as(
+'getPaymentsOfFee2'
+)
+cy.intercept('GET', /students/${student1Mock.id}/fees/${feesMock[7 - 1].id}, feesMock[7 - 1]).as('getFee1')
+cy.intercept('GET', /students/${student1Mock.id}/fees/${feesMock[0].id}, feesMock[0]).as('getFee2')
+cy.intercept('GET', /whoami, whoamiStudentMock).as('getWhoami')
 })
+
+it('lands on profile page if succeeds', () => {
+cy.get('#first_name').contains(studentNameToBeCheckedMock)
+})
+
+it.only('can list fees', () => {
+const newFee = {
+id: 'newFeeId',
+title: 'New Fee',
+amount: 10000,
+status: 'PARTIALLY_PAID'
+}
+cy.intercept('GET', /students/${student1Mock.id}/fees?page=1&page_size=500, [...feesMock, newFee]).as('getFees')
+})
+
+it('can detail fee (click on fee button)', () => {
+cy.get([href="#/students/${student1Mock.id}/fees"]).click()
+cy.get('body')
+.click(200, 0) //note (uncover-menu)
+.wait(['@getStudent', '@getWhoami'])
+cy.get(':
