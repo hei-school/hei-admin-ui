f4c4a48f43383197dce7cb7c194ad3b77aebb4e6
NyAndoMayah <hei.mayah.3@gmail.com>
2023-03-30 10:30:33 +0300

    fix: student.cy.js

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -3,23 +3,33 @@ import App from '../App'
 import { student1 } from './credentials'
 import specTitle from 'cypress-sonarqube-reporter/specTitle'
 import { createPaymentMock, feesMock, student1Mock, studentNameToBeCheckedMock, whoamiStudentMock } from './mocks/responses'
+import { FeeStatusEnum } from '../gen/haClient'
 
+const newFeesMock = feesMock.slice(0, feesMock.length)
+newFeesMock.forEach(element => {
+  if (element.status != FeeStatusEnum.Paid && element.status != FeeStatusEnum.Unpaid && element.status != FeeStatusEnum.Late) {
+    const avalaibleStatus = [FeeStatusEnum.Paid, FeeStatusEnum.Unpaid, FeeStatusEnum.Late]
+    let min = 0
+    let max = avalaibleStatus.length - 1
+    element.status = avalaibleStatus[Math.random() * (max - min + 1) + min]
+  }
+})
 describe(specTitle('Student'), () => {
   beforeEach(() => {
     mount(<App />)
     cy.get('#username').type(student1.username)
     cy.get('#password').type(student1.password)
     cy.get('button').contains('Connexion').click()
-    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, newFeesMock).as('getFees')
     cy.intercept('GET', `/students/${student1Mock.id}`, student1Mock).as('getStudent')
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as(
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${newFeesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(newFeesMock[7 - 1])).as(
       'getPaymentsOfFee1'
     )
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[0])).as(
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${newFeesMock[0].id}/payments?page=1&page_size=10`, createPaymentMock(newFeesMock[0])).as(
       'getPaymentsOfFee2'
     )
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}`, feesMock[7 - 1]).as('getFee1')
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}`, feesMock[0]).as('getFee2')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${newFeesMock[7 - 1].id}`, newFeesMock[7 - 1]).as('getFee1')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${newFeesMock[0].id}`, newFeesMock[0]).as('getFee2')
     cy.intercept('GET', `/whoami`, whoamiStudentMock).as('getWhoami')
   })
 
@@ -28,7 +38,7 @@ describe(specTitle('Student'), () => {
   })
 
   it('can list fees', () => {
-    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&pageconst totalNumbers = _size=500`, newFeesMock).as('getFees')
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
     cy.get('body').click(200, 0) //note(uncover-menu)
     cy.contains('200,000 Ar')
