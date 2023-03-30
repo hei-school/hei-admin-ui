6ecf0ec9346a8fc2c6d1a87d5a63e983b8f26068
MiRandria <hei.minosoa@gmail.com>
2023-03-30 10:35:57 +0300

    fix: bug

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -12,13 +12,13 @@ describe(specTitle('Student'), () => {
     cy.get('button').contains('Connexion').click()
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.intercept('GET', `/students/${student1Mock.id}`, student1Mock).as('getStudent')
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as(
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[11 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[11 - 1])).as(
       'getPaymentsOfFee1'
     )
     cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[0])).as(
       'getPaymentsOfFee2'
     )
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}`, feesMock[7 - 1]).as('getFee1')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[11 - 1].id}`, feesMock[11 - 1]).as('getFee1')
     cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}`, feesMock[0]).as('getFee2')
     cy.intercept('GET', `/whoami`, whoamiStudentMock).as('getWhoami')
   })
