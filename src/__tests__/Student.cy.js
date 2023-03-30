e32484d661d994b8d2881aff62c5f40edecb0550
Tiantsoa <hei.tiantsoa.4@gmail.com>
2023-03-30 10:23:27 +0300

    fix: formatting in student test file

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -12,12 +12,8 @@ describe(specTitle('Student'), () => {
     cy.get('button').contains('Connexion').click()
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.intercept('GET', `/students/${student1Mock.id}`, student1Mock).as('getStudent')
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as(
-      'getPaymentsOfFee1'
-    )
-    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[0])).as(
-      'getPaymentsOfFee2'
-    )
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as('getPaymentsOfFee1')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[0])).as('getPaymentsOfFee2')
     cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}`, feesMock[7 - 1]).as('getFee1')
     cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[0].id}`, feesMock[0]).as('getFee2')
     cy.intercept('GET', `/whoami`, whoamiStudentMock).as('getWhoami')
@@ -30,14 +26,14 @@ describe(specTitle('Student'), () => {
   it('can list fees', () => {
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
-    cy.get('body').click(200, 0) //note(uncover-menu)
+    cy.get('body').click(200, 0) // note (uncover-menu)
     cy.contains('200,000 Ar')
   })
 
   it('can detail fee (click on fee button)', () => {
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
     cy.get('body')
-      .click(200, 0) //note(uncover-menu)
+      .click(200, 0) // note (uncover-menu)
       .wait(['@getStudent', '@getWhoami'])
     cy.get(':nth-child(7) > :nth-child(5)').click()
     cy.contains('En retard')
