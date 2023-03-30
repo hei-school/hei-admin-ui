2348f5dcd61a9bf8376995b99f2b27a01f5e242c
Maheri29 <104850649+Maheri29@users.noreply.github.com>
2023-03-30 20:25:41 +1300

    fix: fix formating style

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -30,8 +30,10 @@ describe(specTitle('Student'), () => {
   it('can list fees', () => {
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
-    cy.get('body').click(200, 0) //note(uncover-menu)
     cy.contains('200,000 Ar')
+    .click();
+
+cy.contains('En retard');
   })
 
   it('can detail fee (click on fee button)', () => {
