aa221b4645323c825388cd3c8f82963ac29237bf
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-03-30 09:48:19 +0300

    fix: add test for partially paid fee status

--- src/__tests__/Student.cy.js
+++ src/__tests__/Student.cy.js
@@ -10,7 +10,20 @@ describe(specTitle('Student'), () => {
     cy.get('#username').type(student1.username)
     cy.get('#password').type(student1.password)
     cy.get('button').contains('Connexion').click()
-    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
+    cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, [
+      ...feesMock,
+      {
+        id: 'fee99_id',
+        student_id: student1Mock.id,
+        remaining_amount: 200000,
+        status: FeeStatusEnum.PARTIALLY_PAID,
+        type: FeeTypeEnum.Tuition,
+        comment: 'Comment',
+        total_amount: 400000,
+        creation_datetime: '2021-11-08T08:25:24Z',
+        due_datetime: '2021-12-08T08:25:24Z'
+      }
+    ]).as('getFees')
     cy.intercept('GET', `/students/${student1Mock.id}`, student1Mock).as('getStudent')
     cy.intercept('GET', `/students/${student1Mock.id}/fees/${feesMock[7 - 1].id}/payments?page=1&page_size=10`, createPaymentMock(feesMock[7 - 1])).as(
       'getPaymentsOfFee1'
@@ -27,10 +40,11 @@ describe(specTitle('Student'), () => {
     cy.get('#first_name').contains(studentNameToBeCheckedMock)
   })
 
-  it('can list fees', () => {
+  it.only('can list fees', () => {
     cy.intercept('GET', `/students/${student1Mock.id}/fees?page=1&page_size=500`, feesMock).as('getFees')
     cy.get(`[href="#/students/${student1Mock.id}/fees"]`).click()
     cy.get('body').click(200, 0) //note(uncover-menu)
+    cy.contains('PARTIALLY PAID')
     cy.contains('200,000 Ar')
   })
 
