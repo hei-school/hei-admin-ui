00739807a9af1ed2d14020c1aff601d94d80f8ec
heiherilala <hei.herilala@gmail.com>
2023-03-31 13:57:23 +0300

    feat: add DateHandling

---
+++ src/operations/utils/DateHandling .js
@@ -0,0 +1,16 @@
+import { DateField } from 'react-admin'
+
+export const CustomDateField = ({ source, label, showTime }) => {
+  let optionsObject = { year: 'numeric', month: 'long', day: 'numeric' }
+  showTime && Object.assign(optionsObject, { hour: 'numeric', minute: 'numeric', second: 'numeric' })
+  return <DateField source={source} label={label} locales='fr-FR' options={optionsObject} />
+}
+
+export const TurnsStringIntoDate = stringDate => {
+  const datePart = stringDate.split('-')
+  return new Date(datePart[0], datePart[1] - 1, datePart[2]).toISOString()
+}
+
+export const TurnsYearMonthDayIntoDate = ({ year, month, day }) => {
+  return new Date(year, month - 1, day).toISOString()
+}
