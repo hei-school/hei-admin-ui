196ae8382b920ffdd41d9599b8865dfd2bb85905
NyAndoMayah <hei.mayah.3@gmail.com>
2023-04-26 15:30:56 +0300

    fix: conform examProvider to the spec

--- src/providers/examProvider.ts
+++ src/providers/examProvider.ts
@@ -1,24 +1,19 @@
 import { teachingApi } from './api'
 import { HaDataProviderType } from './HaDataProviderType'
 
-const raSeparator = '--'
-const toRaId = (courseId: string, examId: string): string => courseId + raSeparator + examId
-
 const examProvider: HaDataProviderType = {
   async getList(filter: any) {
-    const courseId = filter.course_id
+    const courseId = filter
     const result = await teachingApi().getExamsByCourseId(courseId)
-    return result.data.map((exam: any) => ({
-      ...exam,
-      id: toRaId(courseId, exam?.id!)
-    }))
+    return result.data
   },
   async getOne(_raId: string) {
     throw new Error('Function not implemented.')
   },
   async saveOrUpdate(resources: Array<any>) {
     const exams = resources[0]
-    const result = await teachingApi().crupdateExams(exams.course_id, exams.exam_info)
+    const exam = exams[0]
+    const result = await teachingApi().crupdateExams(exam.courseId, exam.examInfo)
     return result.data
   }
 }
