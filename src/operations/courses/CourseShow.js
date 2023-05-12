f44fdf4544ead45d0a6662ccf8d1f7b4f62894dc
heiherilala <hei.herilala@gmail.com>
2023-05-12 14:56:42 +0300

    feat: add creat and update coureses

--- src/operations/courses/CourseShow.js
+++ src/operations/courses/CourseShow.js
@@ -1,11 +1,10 @@
-import { EmailField, FunctionField, SimpleShowLayout, Show, TextField } from 'react-admin'
-import { Link } from '@mui/material'
-import authProvider from '../../providers/authProvider'
-import { unexpectedValue, CustomDateField } from '../utils'
+import { SimpleShowLayout, Show, TextField, TopToolbar, FilterButton, CreateButton } from 'react-admin'
 import ExamList from '../exams/ExamList'
 import { useParams } from 'react-router-dom'
+import authProvider from '../../providers/authProvider'
+import { WhoamiRoleEnum } from '../../gen/haClient'
 
-export const ProfileLayout = () => {
+export const CourseLayout = () => {
   const params = useParams()
   const courseId = params.id
   return (
@@ -20,11 +19,23 @@ export const ProfileLayout = () => {
 }
 
 const CourseShow = course => {
+  const role = authProvider.getCachedRole()
+  const ListActions = () => {
+    if (role === WhoamiRoleEnum.Manager) {
+      return (
+        <TopToolbar>
+          <CreateButton />
+        </TopToolbar>
+      )
+    }
+    return <></>
+  }
+
   const courseId = course.id
   const courseName = course.code
   return (
-    <Show id={courseId} resource='courses' title={courseName}>
-      <ProfileLayout courseId={courseId} />
+    <Show id={courseId} resource='courses' title={courseName} actions={<ListActions />}>
+      <CourseLayout courseId={courseId} />
     </Show>
   )
 }
