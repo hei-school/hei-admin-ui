674dd6edf4d5286e6d47d84ab65cca517107bc5e
NyAndoMayah <hei.mayah.3@gmail.com>
2023-05-24 11:08:59 +0300

    feat: ExamList row called GradeExamEdit

--- src/operations/exams/ExamList.js
+++ src/operations/exams/ExamList.js
@@ -1,33 +1,44 @@
-import { List } from '@react-admin/ra-rbac'
-import { Datagrid, TextField, TopToolbar, CreateButton } from 'react-admin'
-import { CustomDateField } from '../utils'
-import { WhoamiRoleEnum } from '../../gen/haClient'
-import authProvider from '../../providers/authProvider'
+import { List } from "@react-admin/ra-rbac";
+import { Datagrid, TextField, TopToolbar, CreateButton } from "react-admin";
+import { CustomDateField } from "../utils";
+import { WhoamiRoleEnum } from "../../gen/haClient";
+import authProvider from "../../providers/authProvider";
 
 const Actions = ({ basePath, resource }) => (
   <TopToolbar disableGutters>
-    <CreateButton to={basePath + '/create'} resource={resource} />
+    <CreateButton to={basePath + "/create"} resource={resource} />
   </TopToolbar>
-)
+);
 
 const ExamList = ({ courseId }) => {
-  const role = authProvider.getCachedRole()
+  const role = authProvider.getCachedRole();
   return (
     <List
-      title=' ' // is appended to ContainingComponent.title, default is ContainingComponent.title... so need to set it!
-      resource={'exams'}
+      title="Examens"
+      resource={"exams"}
       hasCreate={role === WhoamiRoleEnum.Manager}
-      actions={role === WhoamiRoleEnum.Manager && <Actions basePath={`/courses/${courseId}/exams`} />}
+      actions={
+        role === WhoamiRoleEnum.Manager && (
+          <Actions basePath={`/courses/${courseId}/exams`} />
+        )
+      }
       pagination={false}
-      sort={{ field: 'examination_date', order: 'DESC' }}
+      sort={{ field: "examination_date", order: "DESC" }}
     >
-      <Datagrid bulkActionButtons={false}>
-        <TextField source='title' label='Détails' />
-        <CustomDateField source='examination_date' label='Date' showTime={false} />
-        <TextField source='coefficient' label='Coefficient' />
+      <Datagrid
+        bulkActionButtons={false}
+        rowClick={(examId) => `/courses/${courseId}/exams/${examId}/show`}
+      >
+        <TextField source="title" label="Détails" />
+        <CustomDateField
+          source="examination_date"
+          label="Date"
+          showTime={false}
+        />
+        <TextField source="coefficient" label="Coefficient" />
       </Datagrid>
     </List>
-  )
-}
+  );
+};
 
-export default ExamList
+export default ExamList;
---
+++ src/operations/exams/GradeExamEdit.js
@@ -0,0 +1,13 @@
+import { RowForm } from "@react-admin/ra-editable-datagrid";
+import { TextField, TextInput } from "react-admin";
+
+const GradeExamEdit = (props) => {
+  return (
+    <RowForm>
+      <TextField label="Identifiant" source="id" />
+      <TextField label="Email" source="email" />
+      <TextInput label="Note" source="grade.score" />
+    </RowForm>
+  );
+};
+export default GradeExamEdit;
