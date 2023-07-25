954273c822042844d06bd177e6bfbfe9728f5613
NyAndoMayah <hei.mayah.3@gmail.com>
2023-07-25 13:33:19 +0300

    refactor: make the code more prettier

--- src/providers/utils.ts
+++ src/providers/utils.ts
@@ -1,7 +1,9 @@
 export const raSeparator = '--'
 
+//combine two ids into one so that we can split it to get two ids after
 export const toRaId = (id1: string, id2: string): string => `${id1}${raSeparator}${id2}`
 
+//split the id and get the two ids
 export const toApiIds = (raId: string, property1: string, property2: string): { [key: string]: string } => {
   const ids = raId.split(raSeparator)
   const obj: { [key: string]: string } = {}
