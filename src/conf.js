709c75b6848de72f1dc3410811cfe542449fa703
RickaPrincy <rckprincy@gmail.com>
2023-09-13 11:47:05 +0300

    fix: make the year of predefinedFirstDueDate dynamic

--- src/conf.js
+++ src/conf.js
@@ -36,8 +36,8 @@ export const manualFeeTypes = {
 
 export const predefinedFirstDueDates = {
   // /!\ note(js-months): months are zero-based in JS
-  date1: { name: '15 octobre 2022', value: new Date(2022, 9, 15) },
-  date2: { name: '15 janvier 2023', value: new Date(2023, 0, 15) }
+  date1: { name: `15 octobre ${currentYear}`, value: new Date(currentYear, 9, 15) },
+  date2: { name: `15 janvier ${currentYear + 1}`, value: new Date(currentYear + 1, 0, 15) }
 }
 
 export const paymentTypes = {
