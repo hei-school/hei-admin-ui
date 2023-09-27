f6150c34116cd78a7ac6853848d9ddb88ff7f73d
NyAndoMayah <hei.mayah.3@gmail.com>
2023-09-27 17:04:48 +0300

    feat: add dialog and fix file input

--- src/operations/students/utils.js
+++ src/operations/students/utils.js
@@ -1,11 +1,10 @@
 import { manualFeeTypes, predefinedFeeTypes, predefinedFirstDueDates } from '../../conf'
 import { commentRenderer } from '../utils'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Papa from 'papaparse'
 import studentProvider from '../../providers/studentProvider'
-import { Box, Button, IconButton, Modal, Dialog, DialogTitle, DialogContent, DialogActions, useMediaQuery } from '@mui/material'
-import { Upload, UploadFile } from '@mui/icons-material'
-import { styled } from '@mui/styles'
+import { Button, IconButton, Dialog, DialogTitle, DialogContent, DialogActions, useMediaQuery } from '@mui/material'
+import { Upload} from '@mui/icons-material'
 
 const toDate = str => {
   const parts = str.split('-')
@@ -46,80 +45,61 @@ export const createFees = (fees, feesConf, payload, isPredefinedType) => {
     }
   }
 }
-export const InsertFileModal = ({ open, onClose }) => {
-  const [data, setData] = useState([])
-  const handleChange = e => {
-    const files = e.target.files
-    if (files) {
-      Papa.parse(files[0], {
-        header: true,
-        complete: results => {
-          setData(results.data)
-        }
-      })
-    }
-  }
+const ConfirmDialog = ({ open, handleClose, data, setOpen }) => {
   const addStudents = async () => {
+    setOpen(false)
     await studentProvider.saveOrUpdate(data)
   }
-  const ConfirmDialog = (open, setOpen) => {
-    return (
-      <>
-        <Dialog open={open} onClose={setOpen(false)}>
-          <DialogTitle>
-            Importer ce fichier?
-          </DialogTitle>
-          <DialogContent>
-            Si vous importer ce fichier, les actions seront irréverssibles.
-          </DialogContent>
-          <Button onClick={addStudents}>IMPORTER</Button>
-        </Dialog>
-        <DialogActions>
-          <Button>oui</Button>
-          <Button>non</Button>
-        </DialogActions>
-      </>
-    );
-  }
-  const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
-  });
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  return (
-    <Modal open={open} onClose={onClose}>
-      <Box>
-        <Button startIcon={<UploadFile />}>
-          Insérer un fichier
-          <VisuallyHiddenInput type="file" onChange={handleChange} accept='.csv,.xlsx,.xls' onSubmit={() => setIsSubmitted(true)}/>
-        </Button>
-        <ConfirmDialog open={isSubmitted} setOpen={setIsSubmitted()}/>
-      </Box>
-    </Modal>
-  )
-}
-
-export const ImportButton = () => {
-  const [open, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-  const isSmall = useMediaQuery("(max-width: 625px)");
   return (
     <>
-      <Button onClick={handleOpen}>
-        <IconButton color='primary'>
-          <Upload/>
-        </IconButton>
-        {!isSmall && <span>Importer</span>}
-      </Button>
-      <InsertFileModal open={open} onClose={handleClose} />
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+          Importer ce fichier?
+        </DialogTitle>
+        <DialogContent>
+          Si vous importer ce fichier, les actions seront irréversibles.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={addStudents}>oui</Button>
+          <Button onClick={() => setOpen(false)}>non</Button>
+        </DialogActions>
+      </Dialog>
+
     </>
-  )
+  );
 }
+  export const ImportButton = () => {
+  const [data, setData] = useState([])
+    const isSmall = useMediaQuery("(max-width: 625px)");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const hiddenFileInput = useRef(null);
+    const handleClick = event => {
+      hiddenFileInput.current.click();
+    };
+    const handleClose = () => {
+      setIsSubmitted(false);
+    };
+    const handleChange = e => {
+      const files = e.target.files
+      setIsSubmitted(true)
+      if (files) {
+        Papa.parse(files[0], {
+          header: true,
+          complete: results => {
+            setData(results.data)
+          }
+        })
+      }
+    }
+
+    return (
+      <>
+        <Button size='small' onClick={handleClick}>
+          <Upload fontSize='small'/>
+          <input type="file" ref={hiddenFileInput} style={{ display: 'none' }} onChange={handleChange} />
+          {!isSmall && <span>Importer</span>}
+        </Button>
+        <ConfirmDialog open={isSubmitted} onClose={handleClose}  setOpen={setIsSubmitted} data={data}/>
+      </>
+    )
+  }
