3c20501dde57f1dc0d1660db7d52ac6671a8ba08
NyAndoMayah <hei.mayah.3@gmail.com>
2023-10-06 17:03:49 +0300

    feat: common docs implementation

--- src/operations/docs/DocShow.js
+++ src/operations/docs/DocShow.js
@@ -1,21 +1,18 @@
-import { Show, SimpleShowLayout, useDataProvider, useRedirect } from 'react-admin'
-import { Button, styled, Typography, useMediaQuery } from '@mui/material'
+import { useDataProvider, useRedirect } from 'react-admin'
+import { Button, Divider, styled, Typography, useMediaQuery } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import FilePreviewer from 'react-file-previewer'
 import { ArrowBack, Download } from '@mui/icons-material'
-import authProvider from '../../providers/authProvider'
 
-const DocShow = () => {
+const DocLayout = ({ resource }) => {
   const params = useParams()
   const redirect = useRedirect()
   const dataProvider = useDataProvider()
   const isSmall = useMediaQuery('(max-width: 900px)')
   const [file, setFile] = useState({})
-  const [studentRef, setStudentRef]= useState('')
-  const whoamiId = authProvider.getCachedWhoami().id;
-
   const id = params.id
+
   const FilePreviewerContainer = styled('div')({
     height: '80vh',
     width: '60vw',
@@ -24,15 +21,10 @@ const DocShow = () => {
   })
 
   useEffect(() => {
-    const getSelfStudent = () => dataProvider.getOne('students', {id: whoamiId})
-    const getFile = async () => await dataProvider.getOne('docs', { id: id })
+    const getFile = async () => await dataProvider.getOne(resource, { id: id })
     getFile().then((result) => setFile(result.data))
-      .then(() => {
-      // TODO: temporary until we can get the response from the backend
-        getSelfStudent().then((result) => setFile({ ...file, url: `${file.url}${result.data.ref}/${result.data.ref}.pdf`}))
-    })
   }, [])
-  const handleClose = () => redirect('/docs')
+  const handleClose = () => redirect('list', resource)
   const downloadFile = () => {
     fetch(file.url)
       .then((response) => response.blob()).then((blob) => {
@@ -54,28 +46,27 @@ const DocShow = () => {
       text: 'retour',
       onClick: handleClose
     }
-    ]
+  ]
 
   return (
-    <Show title='Document'>
-      <SimpleShowLayout>
-        <div style={{ display: 'grid', gridTemplateColumns: '5fr 2fr' }}>
-          <Typography variant='h6'>Document : {file.fileName}</Typography>
-          <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridGap: '1vw' }}>
-            {
-              buttons.map((button) => (<Button onClick={button.onClick} variant='outlined' size='small'>
-                {isSmall ? button.icon : button.text}
-              </Button>))
-            }
-          </div>
+    <>
+      <div style={{ display: 'grid', gridTemplateColumns: '5fr 2fr' }}>
+        <Typography variant='h6'>Document : {file.fileName}</Typography>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridGap: '1vw' }}>
+          {
+            buttons.map((button) => (<Button onClick={button.onClick} variant='outlined' size='small'>
+              {isSmall ? button.icon : button.text}
+            </Button>))
+          }
         </div>
-        <FilePreviewerContainer>
-          <FilePreviewer
-            file={file}
-            hideControls={true} />
-        </FilePreviewerContainer>
-      </SimpleShowLayout>
-    </Show>
+      </div>
+      <Divider sx={{ mt: 2, mb: 1 }} />
+      <FilePreviewerContainer>
+        <FilePreviewer
+          file={file}
+          hideControls={true} />
+      </FilePreviewerContainer>
+    </>
   )
 }
-export default DocShow
+export default DocLayout
