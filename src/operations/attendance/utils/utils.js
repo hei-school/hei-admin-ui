9a23654ec895d1c98fff87b8a2e7237c2c152297
RickaPrincy <rckprincy@gmail.com>
2023-10-18 22:42:42 +0300

    feat: create simple attendance show one

--- src/operations/attendance/utils/utils.js
+++ src/operations/attendance/utils/utils.js
@@ -1,3 +1,24 @@
 export function getObjValue(obj, path) {
   return path.split('.').reduce((acc, key) => (acc && acc[key] !== 'undefined' ? acc[key] : undefined), {...obj});
 }
+
+export const dateOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+};
+
+export function formatDate(dateIso) {
+  if (!dateIso) {
+    return
+  }
+
+  const date = new Date(dateIso)
+  const formatter = new Intl.DateTimeFormat('fr-FR', dateOptions);
+  const dateFormater = formatter.format(date)
+  return dateFormater.at(0).toUpperCase() + dateFormater.slice(1) 
+}
