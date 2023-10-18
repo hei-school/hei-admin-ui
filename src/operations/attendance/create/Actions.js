2cfbf706e67d19fbb1f4cde4befb44477d8d63c7
RickaPrincy <rckprincy@gmail.com>
2023-10-19 00:40:37 +0300

    feat: change button interraction to the user

--- src/operations/attendance/create/Actions.js
+++ src/operations/attendance/create/Actions.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Button } from '@mui/material'
-import { useNotify } from 'react-admin'
 import { AttendanceMovementType } from '../../../gen/haClient';
 import { qrcode } from './config';
 
 function Actions({ studentId, sx = {} }) {
-  const notify = useNotify();
-  const submitActions = type => {
-    qrcode.addAttendance(studentId, type)
-    notify('Succès')
+  const [ clicked, setClicked ] = useState('')
+  
+  const removeClicked = async ()=> setTimeout(()=>setClicked(''), 2500)
+  
+  const handlerClick = (type)=>{
+    if(clicked === ''){
+      qrcode.addAttendance(studentId, type)
+      setClicked(type) 
+      removeClicked()
+    }
   }
 
   return (
     <Box component='div' sx={{ display: 'flex',alignItems: 'center', gap: 1, ...sx }}>
-      <Button variant='outlined' color='primary' onClick={() => submitActions(AttendanceMovementType.In)}>
-        Arriver
+      <Button variant='outlined' color='primary' onClick={()=>handlerClick(AttendanceMovementType.In)}>
+        {clicked === AttendanceMovementType.In ? 'Succès' : 'Arriver'}
       </Button>
-      <Button variant='outlined' color='warning' onClick={() => submitActions(AttendanceMovementType.Out)}>
-        Sortie
+      <Button variant='outlined' color='warning' onClick={() => handlerClick(AttendanceMovementType.Out)}>
+        {clicked === AttendanceMovementType.Out ? 'Succès' : 'Sortie'}
       </Button>
     </Box>
   )
