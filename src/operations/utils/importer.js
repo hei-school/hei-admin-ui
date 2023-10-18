d187b4889030e6523c0f40a92a2e62d52a1c5243
NyAndoMayah <hei.mayah.3@gmail.com>
2023-10-18 16:43:10 +0300

    chore: refactor codes in utils because this will be usefull in other operations

---
+++ src/operations/utils/importer.js
@@ -0,0 +1,23 @@
+export const importHeaders = ['ref','first_name','last_name','email','sex','birth_date','address','phone','entrance_datetime']
+
+export const importValidator = (data) => {
+  let isValidate = false;
+  let message = "";
+
+  switch (true) {
+    case data.length === 0:
+      message = "Il n'y a pas d'élément à insérer";
+      break;
+    case Object.keys(data[0]).toString() !== importHeaders.toString():
+      message = 'Veuillez re-vérifier les en-têtes de votre fichier';
+      break;
+    case data.length >= 10:
+      message = 'Vous ne pouvez importer que 10 éléments à la fois.';
+      break;
+    default:
+      isValidate = true;
+      break;
+  }
+
+  return { isValidate, message };
+};
