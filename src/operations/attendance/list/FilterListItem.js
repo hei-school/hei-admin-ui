2cbea8178b2b7021072a5c6bc4285ffcbd5b05b9
RickaPrincy <rckprincy@gmail.com>
2023-10-20 07:54:14 +0300

    refactor: attendance code refactored

--- src/operations/attendance/list/FilterListItem.js
+++ src/operations/attendance/list/FilterListItem.js
@@ -6,23 +6,23 @@ import { useEffect, useState } from 'react'
 export const FilterListItem = ({ label, value, type }) => {
   const { filterValues, setFilters } = useListFilterContext()
   const [values, setValues] = useState(filterValues[type] || [])
+  const isSelected = values.indexOf(value) !== -1
 
   useEffect(() => {
     filterValues[type] !== values && setValues(filterValues[type] || [])
   }, [filterValues[type]])
 
-  const isSelected = () => values.indexOf(value) !== -1
   const toggleFilter = () => {
-    const newFilter = !isSelected(value) ? [...values, value] : [...values].filter(el => el !== value)
+    const newFilter = !isSelected ? [...values, value] : [...values].filter(el => el !== value)
 
     setValues(newFilter)
     setFilters({ ...filterValues, [type]: newFilter })
   }
 
   return (
-    <ListItemButton onClick={toggleFilter} selected={isSelected()} className={FilterListItemClasses.listItemButton}>
+    <ListItemButton onClick={toggleFilter} selected={isSelected} className={FilterListItemClasses.listItemButton}>
       <ListItemText primary={label} className={FilterListItemClasses.listItemText} />
-      {isSelected() && (
+      {isSelected && (
         <ListItemSecondaryAction onClick={toggleFilter}>
           <IconButton size='small'>
             <Cancel />
--- src/operations/attendance/list/ShowOne.js
+++ src/operations/attendance/list/ShowOne.js
@@ -13,11 +13,10 @@ const StyledTypography = styled('h3')({
 })
 
 function Title({ content, label, link }) {
-  const style = { color: '#4a4b4d', my: 0.4, fontSize: '14px' }
-  const getContent = !link ? <span style={{ color: '#282829' }}>{content}</span> : <Link to={link}>{content}</Link>
+  const getContent = link ? <Link to={link}>{content}</Link> : <span style={{ color: '#282829' }}>{content}</span>
 
   return (
-    <Typography sx={style}>
+    <Typography sx={{ color: '#4a4b4d', my: 0.4, fontSize: '14px' }}>
       {label}: {getContent}
     </Typography>
   )
@@ -25,52 +24,49 @@ function Title({ content, label, link }) {
 
 function ShowOne({ showOne, setShowOne }) {
   const { status, record } = showOne
+  if (!record) return null
+
+  const isSession = record.course_session.id
+  const awareded_course = isSession ? record.course_session.awareded_course : {}
 
   const close = () => setShowOne({ status: false, record: null })
-  const student = record ? `${record.student.first_name} ${record.student.ref}` : ''
-  const teacher =
-    record && record.course_session.id
-      ? `${record.course_session.awareded_course.main_teacher.first_name} ` + record.course_session.awareded_course.main_teacher.ref
-      : ''
+  const student = `${record.student.first_name} ${record.student.ref}`
+  console.log(record)
+  const teacher = isSession ? `${awareded_course.main_teacher.first_name} ${awareded_course.main_teacher.ref}` : ''
 
-  const data = !(record && record.course_session.id)
-    ? []
-    : [
-        { label: 'Group', content: record.course_session.awareded_course.group.ref },
-        { label: 'Cours', content: record.course_session.awareded_course.course.code },
-        { label: 'Enseignant', link: `/teachers/${record.course_session.awareded_course.main_teacher.id}/show`, content: teacher },
+  const data = isSession
+    ? [
+        { label: 'Group', content: awareded_course.group.ref },
+        { label: 'Cours', content: awareded_course.course.code },
+        { label: 'Enseignant', link: `/teachers/${awareded_course.main_teacher.id}/show`, content: teacher },
         { label: 'Debut', content: formatDate(record.course_session.begin) },
         { label: 'Fin', content: formatDate(record.course_session.end) }
       ]
+    : []
 
   return (
     <Dialog open={status} onClose={close}>
-      {record && (
-        <>
-          <DialogTitle component='div' sx={{ pb: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <Typography sx={{ fontSize: '1em', fontWeight: 'bold' }}> ID: #{record.id} </Typography>
-            <IconButton onClick={close}>
-              {' '}
-              <Close />{' '}
-            </IconButton>
-          </DialogTitle>
-          <DialogContent sx={{ minWidth: '300px' }}>
-            <StyledTypography>Informations</StyledTypography>
-            <Title label='Heure' content={formatDate(record.created_at) || '---'} />
-            <Title label='Étudiant' link={`/students/${record.student.id}/show`} content={student} />
-            <Title label='Lieu' content={record.place} />
-            {record.course_session.id && (
-              <>
-                {record.is_late && <Title label='En retard de' content={record.late_of} />}
-                <StyledTypography>Cours</StyledTypography>
-                {data.map((el, index) => (
-                  <Title key={index} {...el} />
-                ))}
-              </>
-            )}
-          </DialogContent>
-        </>
-      )}
+      <DialogTitle component='div' sx={{ pb: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Typography sx={{ fontSize: '1em', fontWeight: 'bold' }}> ID: #{record.id} </Typography>
+        <IconButton onClick={close}>
+          <Close />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent sx={{ minWidth: '300px' }}>
+        <StyledTypography>Informations</StyledTypography>
+        <Title label='Heure' content={formatDate(record.created_at) || '---'} />
+        <Title label='Étudiant' link={`/students/${record.student.id}/show`} content={student} />
+        <Title label='Lieu' content={record.place} />
+        {isSession && (
+          <>
+            {record.is_late && <Title label='En retard de' content={record.late_of} />}
+            <StyledTypography>Cours</StyledTypography>
+            {data.map((el, index) => (
+              <Title key={index} {...el} />
+            ))}
+          </>
+        )}
+      </DialogContent>
     </Dialog>
   )
 }
