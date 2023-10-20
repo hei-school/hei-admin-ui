312ba1fa694f14627ec1e84e526d0f4efbbf7599
RickaPrincy <rckprincy@gmail.com>
2023-10-20 07:54:14 +0300

    refactor: attendance code refactored

--- src/operations/attendance/utils/index.js
+++ src/operations/attendance/utils/index.js
@@ -1,5 +1,3 @@
-import Items from './Items'
-import SelectLoading from './SelectLoading'
+export * from './Items'
+export * from './SelectLoading'
 export * from './utils'
-
-export { Items, SelectLoading }
