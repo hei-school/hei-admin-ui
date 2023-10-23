b4eb2957e8ad90c85ad6c491cabd423decc6aa20
RickaPrincy <rckprincy@gmail.com>
2023-10-23 14:49:05 +0300

    style: refactor code with prettier

--- src/operations/attendance/utils/Items.js
+++ src/operations/attendance/utils/Items.js
@@ -1,4 +1,4 @@
-import { MenuItem, CircularProgress,IconButton } from '@mui/material'
+import { MenuItem, CircularProgress, IconButton } from '@mui/material'
 import { Cancel } from '@mui/icons-material'
 import { getObjValue } from './utils'
 
@@ -7,18 +7,18 @@ export function Items({ options, labelKey, valueKey, onClick, checked }) {
     return options.data.map((el, index) => {
       const currentItem = { label: getObjValue(el, labelKey), value: getObjValue(el, valueKey) }
       return (
-        <MenuItem 
-          sx={{ display:'flex', justifyContent:'space-between', alignItems:'center',py:.7}} 
-          onClick={() => onClick(currentItem)} 
-          key={index} 
+        <MenuItem
+          sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', py: 0.7 }}
+          onClick={() => onClick(currentItem)}
+          key={index}
           value={currentItem.value}
         >
           {currentItem.label}
-          { checked(currentItem) && 
-            <IconButton sx={{p:0}} size='small'>
+          {checked(currentItem) && (
+            <IconButton sx={{ p: 0 }} size='small'>
               <Cancel />
             </IconButton>
-          }
+          )}
         </MenuItem>
       )
     })
