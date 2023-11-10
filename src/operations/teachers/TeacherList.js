63296b6fddbef51db9bea0f83cecedcf4232fef8
RickaPrincy <rckprincy@gmail.com>
2023-11-10 11:50:36 +0300

    feat: change teacher list style

--- src/operations/teachers/TeacherList.js
+++ src/operations/teachers/TeacherList.js
@@ -1,16 +1,29 @@
 import { List, Datagrid, TextField, EditButton } from 'react-admin'
-
+import { HaList } from '../../ui/haList'
+import { WorkOutlined } from '@mui/icons-material'
 import { profileFilters } from '../profile'
 import { pageSize, PrevNextPagination } from '../utils'
 
-const TeacherList = () => (
+const HaTeacherList = () => (
   <List filters={profileFilters} pagination={<PrevNextPagination />} perPage={pageSize} resource={'teachers'}>
     <Datagrid bulkActionButtons={false} rowClick='show'>
-      <TextField source='ref' label='Rérérence' />
-      <TextField source='first_name' label='Prénom·s' />
-      <TextField source='last_name' label='Nom·s' />
       <EditButton />
     </Datagrid>
   </List>
 )
 export default TeacherList
+
+export function TeacherList(){
+  return (
+    <HaList
+      icon={<WorkOutlined />}
+      title='Listes des enseignants'
+      datagridProps={{ bulkActionButtons: false, rowClick: 'show'}}
+    >
+      <TextField source='ref' label='Rérérence' />
+      <TextField source='first_name' label='Prénom·s' />
+      <TextField source='last_name' label='Nom·s' />
+      <EditButton />
+    </HaList>
+  )
+}
