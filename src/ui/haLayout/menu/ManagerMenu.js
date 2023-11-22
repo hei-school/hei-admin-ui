a7237ae262f0f8ada117a28cf385fae21770a0b1
RickaPrincy <rckprincy@gmail.com>
2023-11-22 11:37:25 +0300

    feat: add scanners menu

--- src/ui/haLayout/menu/ManagerMenu.js
+++ src/ui/haLayout/menu/ManagerMenu.js
@@ -1,15 +1,16 @@
 import { ListMenu, ListMenuItem, SingleMenu } from './utils'
-import { WorkOutlined, SchoolOutlined, PeopleOutlined, WarningOutlined, Groups } from '@mui/icons-material'
+import { WorkOutlined, SchoolOutlined, PeopleOutlined, WarningOutlined, Groups, HowToRegOutlined } from '@mui/icons-material'
 
 function ManagerMenu() {
   return (
     <>
       <SingleMenu to='/teachers' label='Enseignants' icon={<WorkOutlined />} />
+      <SingleMenu to='/groups' label='Groupes' icon={<Groups />} data-testid='groups' />
+      <SingleMenu to='/scanners' label='Scanners' icon={<HowToRegOutlined/>} />
       <ListMenu label='Étudiants' icon={<SchoolOutlined />} data-testid='students-menu'>
         <ListMenuItem label='Liste des étudiants' icon={<PeopleOutlined />} to='/students' />
         <ListMenuItem label='Frais en retards' icon={<WarningOutlined />} to='/fees' />
       </ListMenu>
-      <SingleMenu to='/groups' label='Groupes' icon={<Groups />} data-testid='groups' />
     </>
   )
 }
