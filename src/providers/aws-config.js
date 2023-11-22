c1dcd99dd4ce8a739c5c3702e5ea37286f2f4026
Onitsiky <hei.onitsiky@gmail.com>
2023-11-22 10:50:09 +0300

    chore: use env variables for aws amplify configurations

---
+++ src/providers/aws-config.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+export const awsConfig = {
+  aws_project_region: process.env.REACT_APP_AWS_REGION,
+  aws_cognito_region: process.env.REACT_APP_AWS_REGION,
+  aws_user_pools_id: process.env.REACT_APP_USERPOOL_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_WEBCLIENT_ID,
+  oauth: {
+    domain: process.env.REACT_APP_OAUTH_DOMAIN,
+  },
+  aws_cognito_social_providers: [],
+  aws_cognito_username_attributes: ['EMAIL'],
+  aws_cognito_signup_attributes: ['EMAIL'],
+  aws_cognito_mfa_configuration: 'OFF',
+  aws_cognito_mfa_types: [],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: ['REQUIRES_LOWERCASE', 'REQUIRES_UPPERCASE', 'REQUIRES_NUMBERS', 'REQUIRES_SYMBOLS'],
+  },
+  aws_cognito_verification_mechanisms: ['EMAIL'],
+};
