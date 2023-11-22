47f7aa5d4cafea60e661a595da6022781227743c
RickaPrincy <rckprincy@gmail.com>
2023-11-22 11:44:55 +0300

    fix: scanner resources fixed

--- src/operations/scanners/ScannerShow.js
+++ src/operations/scanners/ScannerShow.js
@@ -1,9 +1,10 @@
 import { Show } from 'react-admin'
 import { ProfileLayout } from '../profile/ProfileShow'
+import { ActionsOnShow } from '../utils'
 
 const ScannerShow = () => {
   return (
-    <Show title='Enseignants' actions={<ActionsOnShow />}>
+    <Show title='Scanners' actions={<ActionsOnShow />} resource='scanners'>
       <ProfileLayout />
     </Show>
   )
