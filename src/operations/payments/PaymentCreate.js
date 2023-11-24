a1043f5878190626ad31b4719e9c367efa11546a
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2023-11-24 09:50:36 +0300

    feat: optional payment creation datetime  (#389)
    * feat: new client for payment creation-datime
    
    * feat: implement payment creation datetime
    
    * feat: use switch for paiement creation-datetime
    
    * style: format code with prettier
    
    * feat: notify error on payment creation

--- src/operations/payments/PaymentCreate.js
+++ src/operations/payments/PaymentCreate.js
@@ -1,9 +1,21 @@
 import { useState, useEffect } from 'react'
 
-import { Create, SimpleForm, TextInput, RadioButtonGroupInput, useDataProvider, required, minValue, number, useNotify } from 'react-admin'
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  RadioButtonGroupInput,
+  useDataProvider,
+  required,
+  minValue,
+  number,
+  useNotify,
+  DateTimeInput,
+  BooleanInput
+} from 'react-admin'
 import { useParams } from 'react-router-dom'
 import { paymentTypes } from '../../conf'
-
+import { useToggle } from '../../hooks/useToggle'
 import { studentIdFromRaId } from '../../providers/feeProvider'
 
 const PaymentCreate = props => {
@@ -11,6 +23,7 @@ const PaymentCreate = props => {
   const notify = useNotify()
   const feeId = params.feeId
   const studentId = studentIdFromRaId(feeId)
+  const [notSpecifiedDate, setSpecifyDate] = useToggle(true)
   const [studentRef, setStudentRef] = useState('...')
   const dataProvider = useDataProvider()
   useEffect(() => {
@@ -20,20 +33,23 @@ const PaymentCreate = props => {
     }
     doEffect()
   })
-
   const validateConditions = [required()]
   const [paymentChoice, setPaymentChoice] = useState('cash')
-  const paymentConfToPaymentApi = ({ type, amount, comment }) => {
-    return [{ feeId: feeId, type: paymentTypes[type].type, amount: amount, comment: comment }]
+  const notifyError = error => {
+    let message = "Une erreur s`'est produite"
+    if (error.response && error.response.status === 400) {
+      message = 'Paiement pour date future non autorisé'
+    }
+    notify(message, { type: 'error', autoHideDuration: 2500 })
+  }
+  const paymentConfToPaymentApi = ({ type, amount, comment, creation_datetime }) => {
+    const datetimeValue = notSpecifiedDate ? new Date().toISOString() : creation_datetime
+    return [{ feeId, type: paymentTypes[type].type, amount, comment, creation_datetime: datetimeValue }]
   }
 
   return (
     <Create
-      mutationOptions={{
-        onError: error => {
-          notify(`Une erreur s'est produite`, { type: 'error', autoHideDuration: 1000 })
-        }
-      }}
+      mutationOptions={{ onError: notifyError }}
       {...props}
       title={`Paiement de ${studentRef}`}
       resource='payments'
@@ -45,6 +61,7 @@ const PaymentCreate = props => {
           {...props}
           source='type'
           label='Type'
+          validate={validateConditions}
           choices={Object.keys(paymentTypes).map(id => ({ id: id, name: paymentTypes[id].name }))}
           onChange={e => {
             setPaymentChoice(e.target.value)
@@ -52,6 +69,16 @@ const PaymentCreate = props => {
         />
         <TextInput source='amount' label='Montant du paiement' fullWidth={true} validate={validateConditions} />
         <TextInput source='comment' label='Commentaire' fullWidth={true} validate={paymentChoice === 'mobileMoney' && validateConditions} />
+        <BooleanInput
+          source='specify-date'
+          label={"Date de paiement aujourd'hui"}
+          name='create'
+          defaultValue={notSpecifiedDate}
+          onChange={({ target: { checked } }) => setSpecifyDate(checked)}
+        />
+        {!notSpecifiedDate && (
+          <DateTimeInput source='creation_datetime' label='Date de paiement' validate={validateConditions} defaultValue={new Date().toISOString()} />
+        )}
       </SimpleForm>
     </Create>
   )
