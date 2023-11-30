5cee663bbe2d05dc65c2ff0df262bbaab4e48b4f
Dinasoa <99045924+Dinasoa@users.noreply.github.com>
2023-11-30 13:50:40 +0300

    chore: remove profile in initAWS.sh

--- initAWS.sh
+++ initAWS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws configure set aws_access_key_id "$1" --profile hei
-aws configure set aws_secret_access_key "$2" --profile hei
-aws configure set default.region "$3" --profile hei
+aws configure set aws_access_key_id "$1"
+aws configure set aws_secret_access_key "$2"
+aws configure set default.region "$3"
