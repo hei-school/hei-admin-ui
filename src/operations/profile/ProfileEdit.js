c0e71546a19fe45ee215749cc7b0bd7a22eb645e
NyAndoMayah <hei.mayah.3@gmail.com>
2023-12-22 13:48:04 +0300

    chore: add nic and birth_place

--- src/operations/profile/ProfileEdit.js
+++ src/operations/profile/ProfileEdit.js
@@ -1,14 +1,17 @@
-import { DateInput, SimpleForm, TextInput } from 'react-admin'
+import { DateInput, maxLength, SimpleForm, TextInput } from 'react-admin'
 import { EditToolBar, SexRadioButton, toUTC } from '../utils'
 import { StatusRadioButton } from '../utils/UserStatusRadioButton'
 import { CustomEdit } from '../utils/CustomEdit'
 
 const ProfileEdit = ({ isOwnProfile }) => {
-  const transformUser = ({ birth_date, entrance_datetime, ...data }) => ({
-    ...data,
-    birth_date: toUTC(new Date(birth_date)).toISOString(),
-    entrance_datetime: toUTC(new Date(entrance_datetime)).toISOString()
-  })
+  const transformUser = ({ birth_date, entrance_datetime, ...data }) => {
+    return {
+      ...data,
+      birth_date: toUTC(new Date(birth_date)).toISOString(),
+      entrance_datetime: toUTC(new Date(entrance_datetime)).toISOString()
+    }
+  }
+
   return (
     <CustomEdit title='Modifier le profil' transform={transformUser}>
       <SimpleForm toolbar={<EditToolBar />}>
@@ -19,6 +22,8 @@ const ProfileEdit = ({ isOwnProfile }) => {
         <TextInput multiline source='address' label='Adresse' fullWidth />
         <SexRadioButton />
         <TextInput source='phone' label='Téléphone' fullWidth />
+        <TextInput source='nic' label='Numéro CIN' fullWidth validate={maxLength(12, 'Le numéro CIN ne doit pas dépasser 12 caractères.')} />
+        <TextInput source='birth_place' label='Lieu de naissance' fullWidth />
         <DateInput source='birth_date' label='Date de naissance' fullWidth />
         <DateInput source='entrance_datetime' label="Date d'entrée chez HEI" fullWidth disabled={isOwnProfile} />
         <StatusRadioButton disabled={isOwnProfile} />
