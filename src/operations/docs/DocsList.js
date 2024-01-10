1f0f558b35e346495b1ccc232f2ff1ada89a200d
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-01-10 16:29:05 +0300

    style: migrate to HaList for hei docs list

--- src/operations/docs/DocsList.js
+++ src/operations/docs/DocsList.js
@@ -1,15 +1,20 @@
 import { Datagrid, List, ShowButton, TextField } from 'react-admin'
 import { CustomDateField, PrevNextPagination } from '../utils'
+import { HaList } from '../../ui/haList'
 
 const DocsList = ({ title, resource }) => {
   return (
-    <List title={title} resource={resource} pagination={<PrevNextPagination />}>
-      <Datagrid bulkActionButtons={false} rowClick={id => `/${resource}/${id}/show`}>
-        <TextField source='fileName' label='Nom du fichier' />
-        <CustomDateField source='createdAt' label='Date de création' />
-        <ShowButton />
-      </Datagrid>
-    </List>
+    <HaList
+      title={title}
+      resource={resource}
+      datagridProps={{
+        rowClick: id => `/${resource}/${id}/show`
+      }}
+    >
+      <TextField source='fileName' label='Nom du fichier' />
+      <CustomDateField source='createdAt' label='Date de création' />
+      <ShowButton />
+    </HaList>
   )
 }
 export default DocsList
