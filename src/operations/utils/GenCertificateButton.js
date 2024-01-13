93079e1a7ebe2bc4b023fd68ba5eafa9ded0bedc
RickaPrincy <rckprincy@gmail.com>
2024-01-13 17:06:31 +0300

    chore: use useCallBack

--- src/operations/utils/GenCertificateButton.js
+++ src/operations/utils/GenCertificateButton.js
@@ -2,13 +2,14 @@ import {useNotify} from "ra-core"
 import {studenstFileApi} from "../../providers/api" 
 import { Download } from "@mui/icons-material"
 import { Button } from "ra-ui-materialui"
+import { useCallback } from "react"
 
 export function GenCertificateButton({ studentId }) {
     const notify = useNotify()
     const FILE_NAME = 'Certificat_Scolarité.pdf'
   
-    const generate = () => {
-      notify('Certificat de scolarité en cours de téléchargement', { autoHideDuration: 2000 })
+    const generate = useCallback(() => {
+      notify('Certificat de scolarité en cours de téléchargement', { autoHideDuration: 5000 })
       studenstFileApi()
         .getStudentScholarshipCertificate(studentId, { responseType: 'arraybuffer' })
         .then(response => {
@@ -19,8 +20,8 @@ export function GenCertificateButton({ studentId }) {
           link.click()
           document.body.removeChild(link)
         })
-        .catch(() => notify('Échec de téléchargement. Veuillez réessayer', { autoHideDuration: 2000 }))
-    }
+        .catch(() => notify('Échec de téléchargement. Veuillez réessayer', { autoHideDuration: 5000 }))
+    },[studentId])
   
     return (
       <Button label='Certificat' onClick={generate}>
