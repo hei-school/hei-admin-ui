83ebefdde41dd41bc0b2e4daeab26bd680ec65c2
RickaPrincy <rckprincy@gmail.com>
2024-01-19 10:03:03 +0300

    fix: not predefined duedatetime fixed

--- src/operations/fees/utils/createFeesApi.js
+++ src/operations/fees/utils/createFeesApi.js
@@ -1,17 +1,13 @@
-function createComment(comment,i){
-  return `${comment} M${i}`
-}
-
 //Get the end of month after $index months
 function getEndOfMonth(predefinedYear, predefinedMonth, index){
-  const dateAfterIndex = new Date(+predefinedYear, predefinedMonth + index, 1);
-  // dateAfterIndex.setDate(dateAfterIndex.getDate() + 1);
+  const dateAfterIndex = new Date(+predefinedYear, predefinedMonth + index + 1, 1);
   return dateAfterIndex.toISOString();
 };
 
-function getNextDate({...currentDate}, index){
-  currentDate.setMonth(currentDate.getMonth() + index);
-  return currentDate.toISOString();
+function getNextDate(currentDate, index){
+  const tempDate = new Date(currentDate);
+  tempDate.setMonth(currentDate.getMonth() + index);
+  return tempDate.toISOString();
 }
 
 export function createFeesApi(feesRecord, studentId){
@@ -30,19 +26,18 @@ export function createFeesApi(feesRecord, studentId){
   
   for(let i = 0; i < number_of_payments; i++){
     const dueDatetime = isPredefinedDate 
-      ? getEndOfMonth(predefinedYear, predefinedMonth, i + 1) 
+      ? getEndOfMonth(predefinedYear, predefinedMonth, i) 
       : getNextDate(firstDueDatetime, i);
 
     feesToCreate.push({
       type,
-      comment: createComment(comment, i + 1),
+      comment: `${comment} (M${i + 1})`,
       total_amount: amount,
       student_id: studentId,
       due_datetime: dueDatetime
     })
   }
 
-  console.log(feesToCreate);
-
-  return [[]];
+  console.log(feesToCreate)
+  return [[]]
 }
