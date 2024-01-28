8c36c7c7ca4f620afe5b9bc89f68ea88782fb6ae
RickaPrincy <rckprincy@gmail.com>
2024-01-28 21:43:39 +0300

    chore: create mock api for feesTemplates

---
+++ src/__tests__/mocks/responses/fees-templates.ts
@@ -0,0 +1,42 @@
+export const feesTemplatesApi = [
+  {
+    id: "abc123",
+    name: "Frais de Annuel 1x",
+    type: "TUITION",
+    amount: 1_900_000,
+    number_of_payments: 1,
+    creation_datetime: "2024-01-28T18:31:57.808Z"
+  },
+  {
+    id: "def456",
+    name: "Frais annuel 9x",
+    type: "TUITION",
+    amount: 240_000,
+    number_of_payments: 9,
+    creation_datetime: "2024-01-28T18:35:22.123Z"
+  },
+  {
+    id: "ghi789",
+    name: "Ordinateur",
+    type: "HARDWARE",
+    amount: 500_000,
+    number_of_payments: 1,
+    creation_datetime: "2024-01-28T18:40:45.567Z"
+  },
+  {
+    id: "jkl012",
+    name: "Concours",
+    type: "STUDENT_INSURANCE",
+    amount: 100_000,
+    number_of_payments: 1,
+    creation_datetime: "2024-01-28T19:15:10.999Z"
+  },
+  {
+    id: "mno345",
+    name: "Frais médical",
+    type: "REMEDIAL_COSTS",
+    amount: 150,
+    number_of_payments: 1,
+    creation_datetime: "2024-01-28T19:30:45.222Z"
+  }
+]
