bad1a853ede011b4ecdc9209aff590bc657c76e3
RickaPrincy <rckprincy@gmail.com>
2024-01-31 13:07:13 +0300

    chore: move fees templates location

--- src/__tests__/mocks/reponses/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./student-api";
-export * from "./teacher-api";
-export * from "./manager-api";
-export * from "./fees-api";
-export * from "./payment-api";
-export * from "./profile-api";
-export * from "./fees-templates";
