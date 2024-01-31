d9f9bcca31c74c7fe9931b17b5962bef19440160
RickaPrincy <rckprincy@gmail.com>
2024-01-31 11:14:31 +0300

    test: add test for fees creation

---
+++ src/__tests__/mocks/reponses/index.js
@@ -0,0 +1,7 @@
+export * from "./student-api";
+export * from "./teacher-api";
+export * from "./manager-api";
+export * from "./fees-api";
+export * from "./payment-api";
+export * from "./profile-api";
+export * from "./fees-templates";
