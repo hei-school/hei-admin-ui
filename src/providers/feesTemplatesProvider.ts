37914a91639ef1f5709da545a717b63521b5bd21
RickaPrincy <rckprincy@gmail.com>
2024-01-31 11:20:01 +0300

    feat: implement fees provider

---
+++ src/providers/feesTemplatesProvider.ts
@@ -0,0 +1,28 @@
+import {payingApi} from "./api";
+import {HaDataProviderType} from "./HaDataProviderType";
+
+const feesTemplatesProvider: HaDataProviderType = {
+  async getList(page: number, perPage: number, filter: any) {
+    return payingApi()
+      .getFeeTemplates(
+        filter.name,
+        filter.amount,
+        filter.numberOfPayments,
+        page,
+        perPage
+      )
+      .then((response) => response.data);
+  },
+  async getOne(id: string) {
+    return payingApi()
+      .getFeeTemplateById(id)
+      .then((response) => response.data);
+  },
+  async saveOrUpdate(payload: any) {
+    return payingApi()
+      .crupdateFeeTemplate(payload, payload[0])
+      .then((response) => [response.data]);
+  },
+};
+
+export default feesTemplatesProvider;
