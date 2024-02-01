09d0a55bf04b288bd10d45829941a0ec40701cea
RickaPrincy <rckprincy@gmail.com>
2024-02-01 14:01:11 +0300

    chore: show student ref on student fees

--- src/hooks/useStudentRef.js
+++ src/hooks/useStudentRef.js
@@ -1,18 +1,30 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useDataProvider} from "react-admin";
 import {useParams} from "react-router-dom";
 import {studentIdFromRaId} from "../providers/feeProvider";
+import {useNotify} from "./useNotify";
 
 export const useStudentRef = (source) => {
+  const notify = useNotify();
   const params = useParams();
   const dataProvider = useDataProvider();
   const studentId = studentIdFromRaId(params[source]);
   const [studentRef, setStudentRef] = useState("...");
 
-  const fetchRef = async () => {
-    const student = await dataProvider.getOne("students", {id: studentId});
-    setStudentRef(student.data.ref);
-  };
+  useEffect(() => {
+    const fetchRef = async () => {
+      try {
+        const student = await dataProvider.getOne("students", {id: studentId});
+        setStudentRef(student.data.ref);
+      } catch (error) {
+        notify("Erreur de chargement. Merci de rafraîchir la page.");
+      }
+    };
 
-  return {studentRef, fetchRef, studentId};
+    if (studentId) {
+      fetchRef();
+    }
+  }, [studentId]);
+
+  return {studentRef, studentId};
 };
