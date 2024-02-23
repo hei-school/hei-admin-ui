99a7ea816063c582fc919edfa2ce079b9360704d
NyAndoMayah <hei.mayah.3@gmail.com>
2024-02-23 10:17:16 +0300

    chore: add PdfViewer

--- src/operations/heiDocs/index.js
+++
@@ -1,7 +0,0 @@
-import HeiDocsShow from "./HeiDocsShow";
-import DocsList from "../docs/DocsList";
-
-export const heiDocs = {
-  list: <DocsList title="HEI" resource="hei-docs" />,
-  show: HeiDocsShow,
-};
