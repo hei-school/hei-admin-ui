fb23a78e8a39d2145e2a785e593233b174d9e9c8
NyAndoMayah <hei.mayah.3@gmail.com>
2024-02-27 14:17:41 +0300

    chore: create docs provider

--- src/providers/HaDataProviderType.ts
+++ src/providers/HaDataProviderType.ts
@@ -5,7 +5,7 @@ export type HaDataProviderType = {
     filter: any,
     meta?: any
   ) => Promise<Array<any>>;
-  getOne: (id: string) => Promise<any>;
+  getOne: (id: string, meta?: any) => Promise<any>;
   saveOrUpdate: (resources: any) => Promise<any>;
   delete: (id: string) => Promise<any>;
 };
