6e9b7573113b04bc4ce224b1b426ca4a8df05ac8
NyAndoMayah <hei.mayah.3@gmail.com>
2024-03-05 16:33:54 +0300

    fix: operationnal doc list and doc show

--- src/operations/docs/components/DocShow.js
+++ src/operations/docs/components/DocShow.js
@@ -1,19 +1,32 @@
-import {useGetOne} from "react-admin";
+import {useEffect, useState} from "react";
+import {useDataProvider} from "react-admin";
 import {useParams} from "react-router-dom";
 import {Container} from "@mui/material";
 import {useViewType} from "../hooks/useViewType";
 import PdfViewer from "../../common/components/PdfViewer";
 
 export const DocShow = ({owner, studentId}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [doc, setDoc] = useState({});
+  const dataProvider = useDataProvider();
   const params = useParams();
 
   const id = params.id;
   const type = useViewType("SHOW");
 
-  const {data: doc, isLoading} = useGetOne("docs", {
-    id,
-    meta: {owner, type, studentId},
-  });
+  useEffect(() => {
+    const doEffect = async () => {
+      setIsLoading(true);
+      await dataProvider
+        .getOne("docs", {id, meta: {owner, studentId, type}})
+        .then((result) => {
+          setDoc(result.data);
+          setIsLoading(false);
+        })
+        .catch(() => {});
+    };
+    doEffect();
+  }, []);
 
   return (
     <Container fixed>
