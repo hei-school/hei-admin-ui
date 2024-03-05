6df853f09748444123a97b90573422075101dd7f
NyAndoMayah <hei.mayah.3@gmail.com>
2024-03-05 16:34:39 +0300

    feat: add create for hei docs

--- src/operations/docs/hei/DocList.js
+++ src/operations/docs/hei/DocList.js
@@ -1,9 +1,10 @@
 import {DocList as CommonDocList} from "../components/DocList";
+import {OwnerType} from "../types";
 
 export const DocList = () => {
   return (
     <CommonDocList
-      owner="SCHOOL"
+      owner={OwnerType.SCHOOL}
       datagridProps={{
         rowClick: (id) => `/docs/school/${id}`,
       }}
--- src/operations/docs/hei/index.js
+++ src/operations/docs/hei/index.js
@@ -1,9 +1,12 @@
+import {DocCreateDialog} from "../components/DocCreateDialog";
 import {DocShow} from "../components/DocShow";
+import {OwnerType} from "../types";
 import {DocList as HeiDocList} from "./DocList";
 
 const heiDocs = {
   list: HeiDocList,
-  show: <DocShow owner="SCHOOL" />,
+  show: <DocShow owner={OwnerType.SCHOOL} />,
+  create: DocCreateDialog,
   options: {label: "Document de HEI"},
 };
 
