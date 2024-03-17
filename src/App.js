baff92a009b95db54b78c62b62b56f6b12225370
RickaPrincy <rckprincy@gmail.com>
2024-03-17 22:40:15 +0300

    refactor: call get dummy table to check if api is ready

--- src/App.js
+++ src/App.js
@@ -21,86 +21,100 @@ import feesTemplates from "./operations/feesTemplates";
 import payments from "./operations/payments";
 import heiDocs from "./operations/docs/hei";
 import studentDocs from "./operations/docs/students";
+import {WaitUntilHot} from "./utils/retryer";
 
-const App = () => (
-  <Admin
-    title="HEI Admin"
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    i18nProvider={polyglotI18nProvider(() => frenchMessages, "fr")}
-    loginPage={HaLoginPage}
-    theme={mainTheme}
-    layout={HaLayout}
-    requireAuth
-  >
-    <Resource name="profile" {...profile} />
-    <Resource name="students" {...students} />
-    <Resource name="teachers" {...teachers} />
-    <Resource name="groups" {...groups} />
+function AppBase() {
+  return (
+    <Admin
+      title="HEI Admin"
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      i18nProvider={polyglotI18nProvider(() => frenchMessages, "fr")}
+      loginPage={HaLoginPage}
+      theme={mainTheme}
+      layout={HaLayout}
+      requireAuth
+    >
+      <Resource name="profile" {...profile} />
+      <Resource name="students" {...students} />
+      <Resource name="teachers" {...teachers} />
+      <Resource name="groups" {...groups} />
 
-    <Resource name="fees" {...fees} />
-    <Resource name="fees-templates" {...feesTemplates} />
-    <Resource name="payments" {...payments} />
-    <Resource name="docs" options={{label: "Documents"}} />
-    <Resource name="comments" />
+      <Resource name="fees" {...fees} />
+      <Resource name="fees-templates" {...feesTemplates} />
+      <Resource name="payments" {...payments} />
+      <Resource name="docs" options={{label: "Documents"}} />
+      <Resource name="comments" />
 
-    <CustomRoutes>
-      <Route exact path="/profile" element={<profile.show />} />
+      <CustomRoutes>
+        <Route exact path="/profile" element={<profile.show />} />
 
-      <Route exact path="/students/:studentId/fees" element={<fees.list />} />
-      <Route
-        exact
-        path="/students/:studentId/fees/create"
-        element={<fees.create />}
-      />
-      <Route exact path="/fees/:feeId/show" element={<fees.show />} />
-      <Route exact path="/fees" element={<fees.listByStatus />} />
+        <Route exact path="/students/:studentId/fees" element={<fees.list />} />
+        <Route
+          exact
+          path="/students/:studentId/fees/create"
+          element={<fees.create />}
+        />
+        <Route exact path="/fees/:feeId/show" element={<fees.show />} />
+        <Route exact path="/fees" element={<fees.listByStatus />} />
 
-      <Route exact path="/fees/:feeId/payments" element={<payments.list />} />
-      <Route
-        exact
-        path="/fees/:feeId/payments/create"
-        element={<payments.create />}
-      />
+        <Route exact path="/fees/:feeId/payments" element={<payments.list />} />
+        <Route
+          exact
+          path="/fees/:feeId/payments/create"
+          element={<payments.create />}
+        />
 
-      <Route exact path="/docs/school" element={<heiDocs.list />} />
-      <Route exact path="/docs/school/:id" element={heiDocs.show} />
-      <Route exact path="/docs/school/create" element={<heiDocs.create />} />
+        <Route exact path="/docs/school" element={<heiDocs.list />} />
+        <Route exact path="/docs/school/:id" element={heiDocs.show} />
+        <Route exact path="/docs/school/create" element={<heiDocs.create />} />
 
-      <Route
-        exact
-        path="/docs/students/TRANSCRIPT"
-        element={<studentDocs.list />}
-      />
-      <Route
-        exact
-        path="/docs/students/TRANSCRIPT/:id"
-        element={<studentDocs.show />}
-      />
-      <Route exact path="/docs/students/OTHER" element={<studentDocs.list />} />
+        <Route
+          exact
+          path="/docs/students/TRANSCRIPT"
+          element={<studentDocs.list />}
+        />
+        <Route
+          exact
+          path="/docs/students/TRANSCRIPT/:id"
+          element={<studentDocs.show />}
+        />
+        <Route
+          exact
+          path="/docs/students/OTHER"
+          element={<studentDocs.list />}
+        />
 
-      <Route
-        exact
-        path="/students/:studentId/docs/students/OTHER"
-        element={<studentDocs.list />}
-      />
-      <Route
-        exact
-        path="/students/:studentId/docs/students/TRANSCRIPT"
-        element={<studentDocs.list />}
-      />
-      <Route
-        exact
-        path="/students/:studentId/docs/students/TRANSCRIPT/:id"
-        element={<studentDocs.show />}
-      />
-      <Route
-        exact
-        path="/students/:studentId/docs/students/OTHER/:id"
-        element={<studentDocs.show />}
-      />
-    </CustomRoutes>
-  </Admin>
-);
+        <Route
+          exact
+          path="/students/:studentId/docs/students/OTHER"
+          element={<studentDocs.list />}
+        />
+        <Route
+          exact
+          path="/students/:studentId/docs/students/TRANSCRIPT"
+          element={<studentDocs.list />}
+        />
+        <Route
+          exact
+          path="/students/:studentId/docs/students/TRANSCRIPT/:id"
+          element={<studentDocs.show />}
+        />
+        <Route
+          exact
+          path="/students/:studentId/docs/students/OTHER/:id"
+          element={<studentDocs.show />}
+        />
+      </CustomRoutes>
+    </Admin>
+  );
+}
 
+function App() {
+  return (
+    <WaitUntilHot>
+      <AppBase />
+    </WaitUntilHot>
+  );
+}
 export default App;
