9e461f9316228313daa437cbd34fdd8e2904023a
NyAndoMayah <hei.mayah.3@gmail.com>
2024-04-19 16:16:26 +0300

    feat: only one scope choice for teacher

--- src/operations/announcements/components/SelectScope.tsx
+++ src/operations/announcements/components/SelectScope.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import {SelectInput} from "react-admin";
 import {ANNOUNCEMENT_SCOPE} from "../AnnouncementList";
+import {useRole} from "../../../security/hooks";
+import {Scope} from "@haapi/typescript-client";
 
 export const SCOPE_CHOICES = Object.entries(ANNOUNCEMENT_SCOPE).map(
   ([value, label]) => ({value, label})
 );
 
 export const SelectScope = ({setScope}: any) => {
+  const {isTeacher} = useRole();
   return (
     <SelectInput
       label="Portée de l'annonce"
       source="scope"
-      choices={SCOPE_CHOICES}
+      choices={isTeacher() ? [SCOPE_CHOICES[2]] : SCOPE_CHOICES}
       optionText="label"
       optionValue="value"
       defaultValue={SCOPE_CHOICES[0].value}
