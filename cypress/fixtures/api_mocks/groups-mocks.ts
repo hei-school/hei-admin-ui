f62f3cf51dbedcbf2a9266739bf804c745705e7b
NyAndoMayah <hei.mayah.3@gmail.com>
2024-05-23 13:02:22 +0300

    test: groups

--- cypress/fixtures/api_mocks/groups-mocks.ts
+++ cypress/fixtures/api_mocks/groups-mocks.ts
@@ -1,4 +1,11 @@
-import {Group} from "@haapi/typescript-client";
+import {
+  CreateGroup,
+  Group,
+  GroupFlow,
+  GroupFlowMoveTypeEnum,
+  Student,
+} from "@haapi/typescript-client";
+import {studentsMock} from "./students-mocks";
 
 export const groupsMock: Required<Group>[] = [
   {
@@ -28,3 +35,44 @@ export const groupsMock: Required<Group>[] = [
 ];
 
 export const group1Mock = groupsMock[0] as Required<Group>;
+
+export const group1Students = studentsMock.slice(0, 3);
+
+export const group1Student1Mock: Student = group1Students[0];
+
+export const group2Mock = groupsMock[1];
+
+export const leaveGroupFlow: GroupFlow[] = [
+  {
+    group_id: group1Mock.id,
+    move_type: GroupFlowMoveTypeEnum.LEAVE,
+    student_id: group1Student1Mock.id,
+    id: "group_flow_1",
+    flow_datetime: new Date("2023-04-01"),
+  },
+];
+
+export const moveGroupFlow: GroupFlow[] = [
+  {
+    group_id: group1Mock.id,
+    move_type: GroupFlowMoveTypeEnum.LEAVE,
+    student_id: group1Student1Mock.id,
+    id: "group_flow_2",
+    flow_datetime: new Date("2023-04-01"),
+  },
+  {
+    group_id: group2Mock.id,
+    move_type: GroupFlowMoveTypeEnum.JOIN,
+    student_id: group1Student1Mock.id,
+    id: "group_flow_3",
+    flow_datetime: new Date("2023-04-01"),
+  },
+];
+
+export const groupCreate: CreateGroup = {
+  creation_datetime: new Date("2023-04-01"),
+  id: "group_3",
+  name: "Nouveau groupe",
+  ref: "new_group",
+  students: group1Students.map((student) => student?.id!),
+};
