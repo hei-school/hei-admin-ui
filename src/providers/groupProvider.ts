fcccb4e3afd173f0b31b215aa6dfa6f79a9ba0f4
NyAndoMayah <hei.mayah.3@gmail.com>
2024-07-12 11:21:22 +0300

    feat: add group filters

--- src/providers/groupProvider.ts
+++ src/providers/groupProvider.ts
@@ -3,9 +3,9 @@ import {HaDataProviderType} from "./HaDataProviderType";
 import {teachingApi} from "./api";
 
 const groupProvider: HaDataProviderType = {
-  async getList(page: number, perPage: number, _filter: any) {
+  async getList(page: number, perPage: number, filter: any) {
     return await teachingApi()
-      .getGroups(page, perPage)
+      .getGroups(filter.ref, filter.student_ref, page, perPage)
       .then((result) => result.data);
   },
   async getOne(id: string) {
