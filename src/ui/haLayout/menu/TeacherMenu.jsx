470b0732b03cecb88c2398dca2ac0785a123c025
NyAndoMayah <hei.mayah.3@gmail.com>
2024-07-31 15:56:36 +0300

    fix: student and teachers too can have access to owncloud

--- src/ui/haLayout/menu/TeacherMenu.jsx
+++ src/ui/haLayout/menu/TeacherMenu.jsx
@@ -6,19 +6,24 @@ import {
   Home as HeiDocsIcon,
   Newspaper as AnnouncementIcon,
 } from "@mui/icons-material";
+import {useToggle} from "@/hooks";
+import {DocShow} from "@/operations/docs/hei/DocShow";
 import {ListMenu, ListMenuItem, SingleMenu} from "./utils";
 
 function TeacherMenu() {
+  const [isOpen, _set, toggle] = useToggle();
   return (
     <Box>
       <SingleMenu to="/students" label="Étudiants" icon={<StudentIcon />} />
       <ListMenu data-testid="docs" label="Documents" icon={<DocsIcon />}>
         <ListMenuItem
-          to="/docs/school"
+          to="#"
           data-testid="hei-docs"
           label="HEI"
           icon={<HeiDocsIcon />}
+          onClick={toggle}
         />
+        <DocShow open={isOpen} onClose={toggle} />
       </ListMenu>
       <SingleMenu to="/groups" label="Groupes" icon={<GroupIcon />} />
       <SingleMenu
