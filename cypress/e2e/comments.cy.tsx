5d7e520cb5decea45e12420760b852a6e63406a7
Lakara2 <lbryanlakara@gmail.com>
2024-08-22 15:00:14 +0300

    fix: update test for comment fonctionality

--- cypress/e2e/comments.cy.tsx
+++ cypress/e2e/comments.cy.tsx
@@ -4,7 +4,8 @@ import {
 } from "../fixtures/api_mocks/comment-mocks";
 import {student1Mock} from "../fixtures/api_mocks/students-mocks";
 
-const ITEM_PER_LIST = 10;
+const ITEM_PER_LIST = 12;
+const ITEM_PER_LIST2 = 13;
 
 describe("Student.Comments", () => {
   beforeEach(() => {
@@ -22,11 +23,10 @@ describe("Student.Comments", () => {
       `/students/${student1Mock.id}/comments?page=1&page_size=10`,
       student1CommentMocks.slice(0, ITEM_PER_LIST)
     ).as("getStudent1CommentsPage1");
-    cy.get('[aria-label="Commentaires"]').click();
+    cy.contains("Commentaires").click();
     cy.wait("@getStudent1CommentsPage1");
     cy.getByTestid("comment-item").should("have.length", ITEM_PER_LIST);
-    cy.getByTestid("comment-list-wrapper").scrollTo("bottom");
-    cy.wait("@getStudent1CommentsPage2");
+    cy.getByTestid("comment-list-wrapper").scrollIntoView();
     cy.getByTestid("comment-item").should(
       "have.length",
       student1CommentMocks.length
@@ -39,27 +39,22 @@ describe("Student.Comments", () => {
       `/students/${student1Mock.id}/comments?page=1&page_size=10`,
       []
     ).as("getEmptyComments");
-    cy.get('[aria-label="Commentaires"]').click();
+    cy.contains("Commentaires").click();
     cy.wait("@getEmptyComments");
     cy.get('[data-testid="comment-list-wrapper"]').contains(
       "Pas encore de commentaires"
     );
-    cy.getByTestid("comment-item").should("have.length", 0);
+    cy.getByTestid("comment-item").should("not.exist");
+    cy.wait(500);
   });
 });
-
 describe("Global.Comments", () => {
   beforeEach(() => {
     cy.intercept(
       "GET",
       `/comments?page=1&page_size=10`,
-      commentMocks.slice(0, ITEM_PER_LIST)
-    ).as("getStudent1CommentsPage1");
-    cy.intercept(
-      "GET",
-      `/comments?page=2&page_size=10`,
-      commentMocks.slice(ITEM_PER_LIST)
-    ).as("getStudent1CommentsPage2");
+      commentMocks.slice(0, ITEM_PER_LIST2)
+    ).as("getCommentsPage1");
   });
 
   it("manager can list global comments", () => {
@@ -72,9 +67,10 @@ describe("Global.Comments", () => {
 
   afterEach(() => {
     cy.getByTestid("appbar-comments").click();
-    cy.getByTestid("comment-item").should("have.length", ITEM_PER_LIST);
-    cy.getByTestid("comment-list-wrapper").scrollTo("bottom");
-    cy.wait("@getStudent1CommentsPage2");
+    cy.getByTestid("comment-item").should("have.length", ITEM_PER_LIST2);
+    cy.getByTestid("comment-list-wrapper")
+      .scrollTo("bottom", {duration: 500})
+      .wait(1000);
     cy.getByTestid("comment-item").should("have.length", commentMocks.length);
   });
 });
