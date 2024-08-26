ae784ead6d52c2b4659f2aa6612cdf7a193ff85d
Lakara2 <lbryanlakara@gmail.com>
2024-08-26 11:41:29 +0300

    refactor: ProfileShow.jsx

--- src/operations/profile/ProfileShow.jsx
+++ src/operations/profile/ProfileShow.jsx
@@ -1,6 +1,6 @@
 import {EditButton, Button} from "react-admin";
 import {Download, Comment as CommentIcon} from "@mui/icons-material";
-
+import {Box} from "@mui/material";
 import {Show} from "@/operations/common/components/Show";
 import {GetCertificate} from "@/operations/students/components";
 import {ProfileLayout} from "@/operations/common/components/ProfileLayout";
