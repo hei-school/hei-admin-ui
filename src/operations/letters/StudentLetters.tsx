06c1e749c82a0a0c361e752f8137b04e5789c0e3
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-09-02 20:51:09 +0300

    feat: create letters students list

---
+++ src/operations/letters/StudentLetters.tsx
@@ -0,0 +1,105 @@
+import {FC} from "react";
+import {List, TopToolbar, useListContext, useRecordContext} from "react-admin";
+import {IconButton, Box, useMediaQuery} from "@mui/material";
+import {Add} from "@mui/icons-material";
+import {PALETTE_COLORS} from "@/haTheme";
+import {useToggle} from "@/hooks";
+import {PrevNextPagination} from "@/ui/haList/PrevNextPagination";
+import LetterItem from "@/operations/letters/components/letterItems";
+import {Letter} from "@haapi/typescript-client";
+import {CreateLettersDialog} from "@/operations/letters/CreateLetters";
+import Emptylist from "@/operations/letters/components/Emptylist";
+
+interface ListContentStyleProps {
+  isLarge: boolean;
+  isSmall: boolean;
+}
+
+const ListContentStyle = ({
+  isLarge,
+  isSmall,
+}: ListContentStyleProps): React.CSSProperties => {
+  const getJustifyContent = () => (isSmall ? "center" : "flex-start");
+
+  const getGap = () => {
+    if (isLarge) return "1.4rem";
+    if (isSmall) return "1rem";
+    return "1.6rem";
+  };
+
+  const getPadding = () => (isLarge ? "2rem 1rem" : "2.5rem 3.2rem");
+
+  return {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: getJustifyContent(),
+    gap: getGap(),
+    padding: getPadding(),
+  };
+};
+
+const StudentLetters: FC = () => {
+  const [isOpen, , toggle] = useToggle();
+  const {id: studentId} = useRecordContext();
+
+  return (
+    <Box>
+      <List
+        title=" "
+        resource="student-letters"
+        empty={false}
+        queryOptions={{
+          meta: {
+            studentId,
+          },
+        }}
+        pagination={<PrevNextPagination />}
+        actions={
+          <TopToolbar>
+            <IconButton
+              sx={{
+                "backgroundColor": PALETTE_COLORS.primary,
+                "color": PALETTE_COLORS.white,
+                "borderRadius": "8px",
+                "padding": "0.5rem",
+                "&:hover": {
+                  backgroundColor: "#1a305a",
+                },
+              }}
+              onClick={toggle}
+            >
+              <Add
+                style={{
+                  fontSize: "1.7rem",
+                }}
+              />
+            </IconButton>
+          </TopToolbar>
+        }
+      >
+        <StudentLettersContent />
+      </List>
+      <CreateLettersDialog
+        isOpen={isOpen}
+        toggle={toggle}
+        studentId={studentId}
+      />
+    </Box>
+  );
+};
+export default StudentLetters;
+
+const StudentLettersContent: FC = () => {
+  const {data: letters = []} = useListContext<Required<Letter>>();
+  const isEmpty = letters.length === 0;
+  const isSmall = useMediaQuery("(max-width:900px)");
+  const isLarge = useMediaQuery("(min-width:1700px)");
+  return (
+    <Box sx={ListContentStyle({isLarge, isSmall})}>
+      {isEmpty && <Emptylist />}
+      {letters.map((letter) => (
+        <LetterItem key={letter.id} letter={letter} />
+      ))}
+    </Box>
+  );
+};
---
+++ src/operations/letters/index.tsx
@@ -0,0 +1,9 @@
+import {LettersList} from "@/operations/letters/LettersList";
+import {CreateLettersDialog} from "@/operations/letters/CreateLetters";
+
+const letters = {
+  list: LettersList,
+  create: CreateLettersDialog,
+};
+
+export default letters;
