74199c8b8be6c14ffc07b0af9318a8e7c7867960
Michellah <hei.michellah@gmail.com>
2024-09-03 15:45:10 +0300

    feat: added warning for document display failure

--- src/operations/docs/components/DocShow.jsx
+++ src/operations/docs/components/DocShow.jsx
@@ -2,20 +2,27 @@ import {useEffect, useState} from "react";
 import {useDataProvider} from "react-admin";
 import {useParams} from "react-router-dom";
 import {Container} from "@mui/material";
-import {useViewType} from "../hooks/useViewType";
-import PdfViewer from "../../common/components/PdfViewer";
+import {useViewType} from "@/operations/docs/hooks/useViewType";
+import PdfViewer from "@/operations/common/components/PdfViewer";
+import {useNotify} from "@/hooks";
 
 export const DocShow = ({owner, studentId}) => {
   const [isLoading, setIsLoading] = useState(false);
   const [doc, setDoc] = useState({});
   const dataProvider = useDataProvider();
   const params = useParams();
+  const notify = useNotify();
 
   const id = params.id;
   const type = useViewType("SHOW");
+  const isSuspended = useStudentStatus(studentId);
 
   useEffect(() => {
     const doEffect = async () => {
+      if (isSuspended) {
+        setIsLoading(false);
+        return;
+      }
       setIsLoading(true);
       await dataProvider
         .getOne("docs", {id, meta: {owner, studentId, type}})
@@ -26,8 +33,7 @@ export const DocShow = ({owner, studentId}) => {
         .catch(() => {});
     };
     doEffect();
-  }, [studentId, type, id, owner]);
-
+  }, [studentId, type, id, owner, isSuspended]);
   return (
     <Container fixed>
       <PdfViewer
