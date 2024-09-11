5e8a766abec84eb37eb9b7c19c8a1c9f4a21a3b4
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2024-09-11 15:38:40 +0300

    feat: remedial costs
    * feat: add remedial costs
    
    * feat: use the right enum

---
+++ src/operations/fees/components/ManagerFeeList.jsx
@@ -0,0 +1,117 @@
+import {FunctionField, TextField} from "react-admin";
+import {WarningOutlined} from "@mui/icons-material";
+import {Box, Chip} from "@mui/material";
+import feeProvider from "@/providers/feeProvider";
+import {EMPTY_TEXT} from "@/ui/constants";
+import {HaList} from "@/ui/haList/HaList";
+import {CreateButton, ImportButton} from "@/ui/haToolbar";
+import {DeleteWithConfirm} from "@/operations/common/components";
+import {DateField} from "@/operations/common/components/fields";
+import {renderMoney} from "@/operations/common/utils/money";
+import {commentFunctionRenderer} from "@/operations/utils";
+import {
+  minimalFeesHeaders,
+  optionalFeesHeaders,
+  transformFeesData,
+  valideFeesData,
+} from "@/operations/fees/importConf";
+import {rowStyle, PSP_COLORS, PSP_VALUES} from "@/operations/fees/utils";
+import {FeesFilter} from "@/operations/fees/components/FeesFilter";
+
+export const ManagerFeeList = ({studentId, studentRef}) => {
+  return (
+    <HaList
+      icon={<WarningOutlined />}
+      title={`Frais de ${studentRef}`}
+      resource={"fees"}
+      filterIndicator={false}
+      actions={<FeesActions studentId={studentId} />}
+      listProps={{
+        filterDefaultValues: {studentId},
+        storeKey: "fees",
+      }}
+      datagridProps={{
+        rowClick: (id) => `/fees/${id}/show`,
+        rowStyle,
+      }}
+      editable={false}
+    >
+      <DateField
+        source="due_datetime"
+        label="Limite de paiement du frais"
+        showTime={false}
+      />
+      <FunctionField
+        source="comment"
+        render={commentFunctionRenderer}
+        label="Commentaire"
+      />
+      <FunctionField
+        label="Reste à payer"
+        render={(record) => renderMoney(record.remaining_amount)}
+      />
+      <TextField
+        source="mpbs.psp_id"
+        label="Référence de la transaction"
+        emptyText={EMPTY_TEXT}
+      />
+      <FunctionField
+        render={(fee) =>
+          fee.mpbs ? (
+            <Chip
+              color={PSP_COLORS[fee.mpbs?.psp_type]}
+              label={PSP_VALUES[fee.mpbs?.psp_type]}
+            />
+          ) : (
+            EMPTY_TEXT
+          )
+        }
+        label="Type de transaction"
+        emptyText={EMPTY_TEXT}
+      />
+      <DateField
+        source="mpbs.creation_datetime"
+        label="Ajout de la référence de transaction"
+        showTime
+      />
+      <DateField
+        source="mpbs.last_datetime_verification"
+        label="Dernière vérification par HEI"
+        showTime
+      />
+      <DateField
+        source="mpbs.psp_own_datetime_verification"
+        label="Vérification par Orange"
+        showTime
+      />
+      <DateField
+        source="mpbs.successfully_verified_on"
+        label="Vérification réussie"
+        showTime
+      />
+      <DeleteWithConfirm
+        resourceType="fees"
+        redirect={`/students/${studentId}/fees`}
+        confirmTitle="Suppression de frais"
+        confirmContent="Confirmez-vous la suppression de ce frais ?"
+      />
+    </HaList>
+  );
+};
+
+function FeesActions({studentId}) {
+  return (
+    <Box>
+      <CreateButton resource={`students/${studentId}/fees`} />
+      <ImportButton
+        resource="frais"
+        provider={feeProvider.saveOrUpdate}
+        validateData={valideFeesData}
+        optionalHeaders={optionalFeesHeaders}
+        minimalHeaders={minimalFeesHeaders}
+        transformData={(data) => transformFeesData(data, studentId)}
+      />
+      <FeesFilter />
+    </Box>
+  );
+}
---
+++ src/operations/fees/utils/remedialCostsConf.js
@@ -0,0 +1,6 @@
+import {toUTC} from "@/utils/date";
+
+export const DEFAULT_REMEDIAL_COSTS_AMOUNT = 60000;
+export const DEFAULT_REMEDIAL_COSTS_DUE_DATETIME = toUTC(
+  new Date("2024-10-31")
+);
