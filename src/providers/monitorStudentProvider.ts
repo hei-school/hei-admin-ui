676e4027a9370655080db18a66646d26c369020c
Rakotonirinaonjaniaina <hei.onjaniaina@gmail.com>
2024-10-02 22:37:56 +0300

    feat: create monitor role linked to students with access to child info

---
+++ src/providers/monitorStudentProvider.ts
@@ -0,0 +1,51 @@
+import {monitoringApi} from "@/providers/api";
+import {HaDataProviderType} from "@/providers/HaDataProviderType";
+import authProvider from "@/providers/authProvider";
+
+const monitorStudentProvider: HaDataProviderType = {
+  async getList(page: number, perPage: number, _filter: any) {
+    const monitorId = authProvider.getCachedWhoami()?.id;
+
+    if (!monitorId) {
+      throw new Error("Monitor ID is required.");
+    }
+    const result = await monitoringApi().getLinkedStudentsByMonitorId(
+      monitorId,
+      page,
+      perPage
+    );
+
+    return {
+      data: result.data,
+    };
+  },
+
+  async getOne(id: string) {
+    if (!id) {
+      throw new Error("ID is required to fetch a single student.");
+    }
+
+    const monitorId = authProvider.getCachedWhoami().id;
+
+    if (!monitorId) {
+      throw new Error("Monitor ID is required.");
+    }
+
+    const result = await monitoringApi().getLinkedStudentsByMonitorId(
+      monitorId,
+      1,
+      15
+    );
+    return result.data.find((student) => student.id === id);
+  },
+
+  async saveOrUpdate(_payload: any, _meta: any) {
+    throw new Error("saveOrUpdate not supported for monitor-students");
+  },
+
+  async delete(_id: string) {
+    throw new Error("delete not supported for monitor-students");
+  },
+};
+
+export default monitorStudentProvider;
