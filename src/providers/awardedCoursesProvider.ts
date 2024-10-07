eca725f25c0ffc860a905a0e0e6cffce48df2a69
Michellah <hei.michellah@gmail.com>
2024-10-07 09:57:27 +0300

    feat: provider for awarded courses

---
+++ src/providers/awardedCoursesProvider.ts
@@ -0,0 +1,34 @@
+import {teachingApi} from "@/providers/api";
+import {CreateAwardedCourse} from "@haapi/typescript-client";
+import {HaDataProviderType} from "@/providers/HaDataProviderType";
+
+const awardedCoursesProvider: HaDataProviderType = {
+    getList: async(page, perPage, filter, _meta) => {
+        const {teacherId, courseId} = filter;
+
+        return teachingApi()
+            .getAllAwardedCourseByCriteria(teacherId, courseId, page, perPage)
+            .then((result) => ({data: result.data}));
+    },
+    async getOne(id: string) {
+        return teachingApi()
+            .getAwardedCoursesAssignedToTeacher(id)
+            .then((response) => response.data);
+    }, 
+    async saveOrUpdate(payload: CreateAwardedCourse[]) {
+        const {main_teacher_id} = payload[0]
+        if (!main_teacher_id) {
+            throw new Error("Teacher ID is required");
+        }
+        return teachingApi()
+            .createOrUpdateAwardedCoursesAssignToTeacher(main_teacher_id, payload)
+            .then((response) => {
+                return response.data
+            })
+    },
+    async delete(_id: string) {
+        throw new Error("Not implemented");
+    }
+}
+
+export default awardedCoursesProvider;
