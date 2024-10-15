78f95c8e01cdbdd62ac96b62ccbc3c52e9eb2154
sarobidy-23 <hei.sarobidy@gmail.com>
2024-10-15 11:06:49 +0300

    feat: multiple select filter

--- src/ui/haToolbar/Filters.jsx
+++ src/ui/haToolbar/Filters.jsx
@@ -4,14 +4,11 @@ import {
   MenuItem,
   InputLabel,
   FormControl,
-  useMediaQuery,
 } from "@mui/material";
 import useHaToolbarContext from "./useHaToolbarContext";
 
 export function TextFilter({label, source, ...rest}) {
   const {currentFilter, setOneFilter} = useHaToolbarContext();
-  const isSmall = useMediaQuery("(max-width:900px)");
-
   return (
     <TextField
       type="text"
@@ -19,17 +16,21 @@ export function TextFilter({label, source, ...rest}) {
       size="small"
       variant="outlined"
       value={currentFilter[source] || ""}
-      sx={{width: "100%", minWidth: isSmall ? "100%" : "320px"}}
+      sx={{width: "100%", minWidth: "320px"}}
       onChange={(event) => setOneFilter(source, event.target.value)}
       {...rest}
     />
   );
 }
 
-export const SelectInputFilter = ({choices, label, source, ...props}) => {
+export const SelectInputFilter = ({
+  choices,
+  label,
+  source,
+  multiple = false,
+  ...props
+}) => {
   const {currentFilter, setOneFilter} = useHaToolbarContext();
-  const isSmall = useMediaQuery("(max-width:900px)");
-
   return (
     <FormControl sx={{width: "100%"}}>
       <InputLabel id="select-label" size="small" variant="outlined">
@@ -39,9 +40,10 @@ export const SelectInputFilter = ({choices, label, source, ...props}) => {
         labelId="select-label"
         label={label}
         size="small"
+        multiple={multiple}
         variant="outlined"
-        value={currentFilter[source] || ""}
-        sx={{minWidth: isSmall ? "100%" : "350px"}}
+        value={currentFilter[source] || (multiple ? [] : "")}
+        sx={{minWidth: "350px"}}
         fullWidth
         onChange={(event) => setOneFilter(source, event.target.value)}
         {...props}
