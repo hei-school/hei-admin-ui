419f21cb649ced00cd8df06fba31834b46b11c5b
sarobidy-23 <hei.sarobidy@gmail.com>
2024-10-17 16:44:02 +0300

    feeat: events list

---
+++ src/operations/events/EventList.tsx
@@ -0,0 +1,53 @@
+import {List, useListContext} from "react-admin";
+import {Event as EventIcon} from "@mui/icons-material";
+import {Box, CircularProgress} from "@mui/material";
+import {Event} from "@haapi/typescript-client";
+import {PrevNextPagination} from "@/ui/haList/PrevNextPagination";
+import {EventCard} from "./components";
+import {EventListAction} from "./components";
+import {HaListTitle} from "@/ui/haList";
+
+export function EventList() {
+  return (
+    <List
+      title="Événements"
+      resource="events"
+      empty={false}
+      actions={false}
+      pagination={<PrevNextPagination />}
+      sx={{mt: 1}}
+    >
+      <HaListTitle
+        title="Liste des événements"
+        icon={<EventIcon />}
+        filterIndicator
+        actions={<EventListAction />}
+        mainSearch={{label: "Titre", source: "title"}}
+      />
+      <EventListContent />
+    </List>
+  );
+}
+
+function EventListContent() {
+  const {data: events = [], isLoading} = useListContext<Required<Event>>();
+  return isLoading ? (
+    <CircularProgress size={30} sx={{m: 5}} />
+  ) : (
+    <Box
+      sx={{
+        px: 1,
+        pb: 2,
+        display: "flex",
+        alignItems: "stretch",
+        flexWrap: "wrap",
+        width: "100%",
+        gap: 2,
+      }}
+    >
+      {events.map((event) => (
+        <EventCard key={event.id} event={event} />
+      ))}
+    </Box>
+  );
+}
