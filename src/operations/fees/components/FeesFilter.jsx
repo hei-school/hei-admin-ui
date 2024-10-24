f9f19fec1071f40e35eb897aed90eeadf792212b
NyAndoMayah <hei.mayah.3@gmail.com>
2024-10-24 13:51:22 +0300

    feat: add status filter for fees

--- src/operations/fees/components/FeesFilter.jsx
+++ src/operations/fees/components/FeesFilter.jsx
@@ -1,8 +1,18 @@
-import {FilterForm, TextFilter} from "@/ui/haToolbar";
+import {FilterForm, TextFilter, SelectInputFilter} from "@/ui/haToolbar";
 import {FeeStatusEnum} from "@haapi/typescript-client";
 
 export const FeesFilter = () => (
   <FilterForm>
+    <SelectInputFilter
+      data-testid="filter-fees-status"
+      label="Statut"
+      source="status"
+      choices={[
+        {id: FeeStatusEnum.LATE, name: "En retard"},
+        {id: FeeStatusEnum.PAID, name: "Payés"},
+        {id: FeeStatusEnum.UNPAID, name: "En attente"},
+      ]}
+    />
     <TextFilter label="Référence étudiante" source="student_ref" />
   </FilterForm>
 );
