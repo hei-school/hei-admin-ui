8e11b196e02835a7ab61ccb781a2d467444d470a
Rakotonirinaonjaniaina <hei.onjaniaina@gmail.com>
2024-10-30 11:38:11 +0300

    feat: add test in attendace

--- cypress/e2e/attendance.cy.tsx
+++ cypress/e2e/attendance.cy.tsx
@@ -1,11 +1,11 @@
-import { attendancesMock } from "../fixtures/api_mocks/attendances-mocks";
-import { studentsMock } from "../fixtures/api_mocks/students-mocks";
+import {attendancesMock} from "../fixtures/api_mocks/attendances-mocks";
+import {studentsMock} from "../fixtures/api_mocks/students-mocks";
 
 describe('Attendance', () => {
   beforeEach(() => {
     cy.login({ role: "MANAGER" });
 
-    cy.intercept("GET", `/attendance?page=1&page_size=10`, attendancesMock).as("getPresencePage");
+    cy.intercept("GET", `/attendance?page=1&page_size=10`, attendancesMock).as("getPresencePage1");
     cy.intercept("GET", `/students?page=1&page_size=10`, studentsMock).as("getStudent");
     cy.intercept("POST", `/attendance/movement`, (req) => {
       req.reply({
@@ -15,30 +15,51 @@ describe('Attendance', () => {
           id: attendancesMock.length + 1,
         },
       });
-    }).as("createAttendance");
+    }).as("CreateAttendanceMovement");
   });
 
-  xit('manager can see list of present', () => {
-    cy.get('[href="#/attendance"]').click();
+  const navigateToAttendancePage = () => {
+    cy.get('[href="#/attendance"]').first().click();
+  };
+
+  it('manager can see list of present', () => {
+    navigateToAttendancePage();
     cy.get("tbody tr").should("have.length", attendancesMock.length);
     cy.get("body").click();
   });
 
-  it('manager can create attendance', () => {
-    cy.get('[href="#/attendance"]').click();
+  it('manager can create attendance arrive', () => {
+    navigateToAttendancePage();
     cy.get('[href="#/attendance/create"]').click();
 
     cy.contains('button', 'Arriver').click();
     cy.wait('@CreateAttendanceMovement');
     cy.contains("Création réussie !").should('be.visible');
-    cy.wait('@getPresencePage');
-  
+    cy.wait('@getPresencePage1');
+
+    navigateToAttendancePage()
+    cy.get("tbody tr").should("have.length", attendancesMock.length); 
+  });
+
+  it('manager can create attendance exit', () => {
+    navigateToAttendancePage();
+    cy.get('[href="#/attendance/create"]').click();
+
     cy.contains('button', 'Sortie').click();
     cy.wait('@CreateAttendanceMovement');
     cy.contains("Création réussie !").should('be.visible');
+    cy.wait('@getPresencePage1');
 
-    cy.wait('@getPresencePage');
-   
-    });
+    navigateToAttendancePage()
+    cy.get("tbody tr").should("have.length", attendancesMock.length);
+  });
+
+
+  // TODO REFACTOR
+  /* it('manager can create attendance eby scan', () => {
+    navigateToAttendancePage();
+    cy.get('[href="#/attendance/scan"]').click();
+  });*/
+
 });
 
