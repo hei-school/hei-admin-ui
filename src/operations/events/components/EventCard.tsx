d3c0cdb8ac5485a2bd2f662f1baa20a85e0eb59b
sarobidy-23 <hei.sarobidy@gmail.com>
2024-11-06 16:34:14 +0300

    chore: refont event ui

--- src/operations/events/components/EventCard.tsx
+++ src/operations/events/components/EventCard.tsx
@@ -1,4 +1,5 @@
-import {Event, EventType} from "@haapi/typescript-client";
+import {Event} from "@haapi/typescript-client";
+import {CalendarMonthOutlined, MenuBook} from "@mui/icons-material";
 import {
   Box,
   Button,
@@ -7,13 +8,15 @@ import {
   CardContent,
   Stack,
   SxProps,
+  Chip,
 } from "@mui/material";
 import {EVENT_TYPE_VALUE} from "../utils";
-import {formatDate} from "@/utils/date";
+import {formatDate, getTime, isSameDay} from "@/utils/date";
 import defaultPicture from "@/assets/blank-profile-photo.png";
 import {EventEditDialog} from "../EventEditDialog";
 import {useRole} from "@/security/hooks";
 import {useToggle} from "@/hooks";
+import {StatCard} from "./Stats";
 
 export type EventCardPropsType = {
   event: Required<Event> & {
@@ -22,26 +25,14 @@ export type EventCardPropsType = {
   };
 };
 
-const EVENT_TYPE_SX: SxProps = {
-  color: "white",
-  px: 1.2,
-  py: 0.4,
-  borderRadius: "8px 0px 0px 8px",
-};
-
 const CARD_HEADER_SX: SxProps = {
   display: "flex",
-  gap: 2,
-  px: 1,
-  width: "95%",
   mx: "auto",
   borderRadius: "5px",
-  py: 1,
 };
 
 const CARD_SX: SxProps = {
   maxWidth: "350px",
-  py: 2,
   borderRadius: "8px",
   width: "100%",
   display: "flex",
@@ -59,8 +50,18 @@ export function EventCard({event}: EventCardPropsType) {
   return (
     <Card sx={CARD_SX} className="event-card">
       <Box sx={{width: "100%"}}>
-        <CardContent sx={{px: 0}}>
-          <Typography sx={{fontSize: 35, px: 1, fontWeight: "bold"}}>
+        <CardContent>
+          <Stack direction="row" justifyContent="space-between">
+            <Chip
+              label={EVENT_TYPE_VALUE[event.type]}
+              size="small"
+              color={event.type == "COURSE" ? "primary" : "default"}
+            />
+            {event.type == "COURSE" && (
+              <Chip avatar={<MenuBook />} label={course?.code} size="small" />
+            )}
+          </Stack>
+          <Typography sx={{fontSize: 30, fontWeight: "bold"}}>
             {event.title || ""}
           </Typography>
           <Box sx={CARD_HEADER_SX}>
@@ -84,37 +85,35 @@ export function EventCard({event}: EventCardPropsType) {
               justifyContent: "space-between",
             }}
           >
-            <Box sx={{"pl": 2, "& *": {fontSize: "12px !important"}}}>
-              <Stack gap={1} direction="row">
-                <Typography fontWeight="bold">Début: </Typography>
-                <Typography>
-                  {formatDate(event.begin_datetime, true)}
-                </Typography>
-              </Stack>
-              <Stack gap={1} direction="row">
-                <Typography fontWeight="bold">Fin: </Typography>
-                <Typography>{formatDate(event.end_datetime, true)}</Typography>
-              </Stack>
-              <Stack gap={1} direction="row">
-                <Typography fontWeight="bold">Type: </Typography>
-                <Typography>{EVENT_TYPE_VALUE[event.type]}</Typography>
-              </Stack>
+            <Box sx={{"& *": {fontSize: "14px !important"}}}>
+              {isSameDay(event.begin_datetime, event.end_datetime) ? (
+                <Stack gap={1} direction="row" alignItems="center">
+                  <CalendarMonthOutlined />
+                  <Typography>{`${formatDate(event.begin_datetime, false)} de ${getTime(event.begin_datetime)} à ${getTime(event.end_datetime)}`}</Typography>
+                </Stack>
+              ) : (
+                <>
+                  <Stack gap={1} direction="row">
+                    <Typography fontWeight="bold">Début: </Typography>
+                    <Typography>
+                      {formatDate(event.begin_datetime, true)}
+                    </Typography>
+                  </Stack>
+                  <Stack gap={1} direction="row">
+                    <Typography fontWeight="bold">Fin: </Typography>
+                    <Typography>
+                      {formatDate(event.end_datetime, true)}
+                    </Typography>
+                  </Stack>
+                </>
+              )}
               <Stack gap={1} direction="row">
                 <Typography fontWeight="bold">Groupes: </Typography>
                 <Typography>
                   {event.groups?.map((group) => group.ref).join(", ")}
                 </Typography>
               </Stack>
-              {event.type === EventType.COURSE && (
-                <Stack gap={1} sx={{color: "red"}} direction="row">
-                  <Typography fontWeight="bold">Cours: </Typography>
-                  <Typography sx={{color: "red"}}>{course?.code}</Typography>
-                </Stack>
-              )}
             </Box>
-            <Typography sx={EVENT_TYPE_SX}>
-              {EVENT_TYPE_VALUE[event.type]}
-            </Typography>
           </Box>
           <Typography
             noWrap
@@ -122,8 +121,6 @@ export function EventCard({event}: EventCardPropsType) {
             sx={{
               fontSize: "14px",
               minHeight: "45px",
-              px: 2,
-              my: 1,
               opacity: 0.9,
             }}
           >
@@ -131,33 +128,35 @@ export function EventCard({event}: EventCardPropsType) {
           </Typography>
         </CardContent>
       </Box>
-      <Box
-        sx={{
-          px: 2,
-          display: "flex",
-          alignItems: "center",
-          width: "100%",
-          justifyContent: "space-between",
-        }}
-      >
-        {isManager() && (
+      <Stack width="100%" p={1} gap={1}>
+        <StatCard stats={event.count || {}} />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          {isManager() && (
+            <Button
+              size="small"
+              variant="contained"
+              onClick={toggleEdit}
+              sx={{textTransform: "revert"}}
+            >
+              Editer
+            </Button>
+          )}
           <Button
             size="small"
-            variant="contained"
-            onClick={toggleEdit}
+            href={`#/events/${event.id}/participants`}
             sx={{textTransform: "revert"}}
           >
-            Editer
+            Présence
           </Button>
-        )}
-        <Button
-          size="small"
-          href={`#/events/${event.id}/participants`}
-          sx={{textTransform: "revert"}}
-        >
-          Présence
-        </Button>
-      </Box>
+        </Box>
+      </Stack>
       <EventEditDialog
         eventId={event.id}
         editShow={editShow}
