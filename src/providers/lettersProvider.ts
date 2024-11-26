c759be0170d68a2d1b0dddfa31b182e8ee022e76
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-11-26 21:16:16 +0300

    chore: display only student letters for managers

--- src/providers/lettersProvider.ts
+++ src/providers/lettersProvider.ts
@@ -1,5 +1,7 @@
 import {lettersApi} from "./api";
+import authProvider from "./authProvider";
 import {HaDataProviderType} from "./HaDataProviderType";
+const {role} = authProvider.getCachedWhoami();
 
 const lettersProvider: HaDataProviderType = {
   getList: async (page, perPage, filter = {}) => {
@@ -8,19 +10,35 @@ const lettersProvider: HaDataProviderType = {
       ADMIN: false,
       ALL: null,
     };
-
-    return lettersApi()
-      .getLetters(
-        page,
-        perPage,
-        filter.student_ref,
-        filter.letter_ref,
-        filter.status,
-        filter.student_name,
-        filter.fee_id,
-        LETTER_TYPE[filter?.is_linked_with_fee]
-      )
-      .then((result) => ({data: result.data}));
+    if (role === "MANAGER") {
+      return lettersApi()
+        .getStudentsLetters(
+          page,
+          perPage,
+          filter.student_ref,
+          filter.letter_ref,
+          filter.status,
+          filter.student_name,
+          filter.fee_id,
+          LETTER_TYPE[filter?.is_linked_with_fee]
+        )
+        .then((result) => ({data: result.data}));
+    } else if (role === "ADMIN") {
+      return lettersApi()
+        .getLetters(
+          page,
+          perPage,
+          filter.student_ref,
+          filter.letter_ref,
+          filter.status,
+          filter.student_name,
+          filter.fee_id,
+          LETTER_TYPE[filter?.is_linked_with_fee],
+          filter.role
+        )
+        .then((result) => ({data: result.data}));
+    }
+    return {data: []};
   },
   getOne: () => {
     throw new Error("Function not implemented.");
