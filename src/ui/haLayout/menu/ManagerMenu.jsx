c87182ec9fd44471cac11e17a935a2315c52b1f4
sarobidy-23 <hei.sarobidy@gmail.com>
2024-12-17 14:26:27 +0300

    chore: attendance icon missing

--- src/ui/haLayout/menu/ManagerMenu.jsx
+++ src/ui/haLayout/menu/ManagerMenu.jsx
@@ -14,6 +14,7 @@ import {
   SupervisedUserCircle as MonitorIcon,
   GradeOutlined as GradeIcon,
   LibraryBooksOutlined as LibraryIcon,
+  ListAlt as AttendanceIcon,
   AssignmentInd as StaffIcon,
 } from "@mui/icons-material";
 import {HeiListMenuItem} from "@/ui/haLayout/menu/common";
