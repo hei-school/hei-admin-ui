e2115d43f8359643caa9cbd9ca54ffddd8fa9a7b
Fiantso Harena <fiantsorav@gmail.com>
2024-12-19 16:15:23 +0300

    refactor: remove createApiInstance function and manually integrate authProvider, undefined, and getAxiosInstance for API setup

--- src/providers/api.js
+++ src/providers/api.js
@@ -14,22 +14,71 @@ import {
 import authProvider from "./authProvider";
 import {getAxiosInstance} from "@/config/axios";
 
-const createApiInstance = (ApiClass) => {
-  return new ApiClass(
+export const usersApi = () =>
+  new UsersApi(authProvider.getCachedAuthConf(), undefined, getAxiosInstance());
+
+export const payingApi = () =>
+  new PayingApi(
     authProvider.getCachedAuthConf(),
     undefined,
     getAxiosInstance()
   );
-};
 
-export const usersApi = () => createApiInstance(UsersApi);
-export const payingApi = () => createApiInstance(PayingApi);
-export const teachingApi = () => createApiInstance(TeachingApi);
-export const filesApi = () => createApiInstance(FilesApi);
-export const commentApi = () => createApiInstance(CommentsApi);
-export const healthApi = () => createApiInstance(HealthApi);
-export const promotionApi = () => createApiInstance(PromotionsApi);
-export const announcementsApi = () => createApiInstance(AnnouncementsApi);
-export const lettersApi = () => createApiInstance(LettersApi);
-export const eventsApi = () => createApiInstance(EventsApi);
-export const monitoringApi = () => createApiInstance(MonitoringApi);
+export const teachingApi = () =>
+  new TeachingApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const filesApi = () =>
+  new FilesApi(authProvider.getCachedAuthConf(), undefined, getAxiosInstance());
+
+export const commentApi = () =>
+  new CommentsApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const healthApi = () =>
+  new HealthApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const promotionApi = () =>
+  new PromotionsApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const announcementsApi = () =>
+  new AnnouncementsApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const lettersApi = () =>
+  new LettersApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const eventsApi = () =>
+  new EventsApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
+
+export const monitoringApi = () =>
+  new MonitoringApi(
+    authProvider.getCachedAuthConf(),
+    undefined,
+    getAxiosInstance()
+  );
