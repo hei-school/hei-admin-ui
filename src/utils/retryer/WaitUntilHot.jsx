039671a362ec5492895e46a7dad9c5d6ccc60538
NyAndoMayah <hei.mayah.3@gmail.com>
2025-01-16 13:11:16 +0300

    style: imports

--- src/utils/retryer/WaitUntilHot.jsx
+++ src/utils/retryer/WaitUntilHot.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {LoadingPage} from "@mui/material";
-import {healthApi} from "../../providers/api";
+import {healthApi} from "@/providers/api";
 
 const MAX_ATTEMP = 10;
 
