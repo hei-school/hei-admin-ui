4b7da893b444f8351bc02c13f62616e1397ea8ec
NyAndoMayah <hei.mayah.3@gmail.com>
2025-01-26 21:08:14 +0300

    feat: certificate validation before download

--- src/operations/students/components/GetCertificate.jsx
+++ src/operations/students/components/GetCertificate.jsx
@@ -1,15 +1,46 @@
-import {Button} from "react-admin";
+import {Button, useRecordContext} from "react-admin";
 import {Download} from "@mui/icons-material";
 import {FileDownloader} from "@/operations/common/components";
+import {useNotify} from "@/hooks";
 import {filesApi} from "@/providers/api";
+import {isOver18} from "../utils/isOver18";
 
 const FILE_NAME = "Certificat_Scolarité.pdf";
+const requiredFields = {
+  birth_date: "date de naissance",
+  birth_place: "lieu de naissance",
+  first_name: "prénom",
+  last_name: "nom de famille",
+  groups: "groupe",
+};
 
 export const GetCertificate = ({studentId, variant = " "}) => {
-  const downloadFunction = () =>
-    filesApi().getStudentScholarshipCertificate(studentId, {
+  const notify = useNotify();
+  const record = useRecordContext();
+
+  const downloadFunction = () => {
+    const missingFields = Object.entries(requiredFields)
+      .filter(([key]) => !record[key])
+      .map(([_, value]) => value);
+
+    if (missingFields.length > 0) {
+      notify(
+        `Vous ne pouvez pas télécharger votre certificat de scolarité car vos informations sont incomplètes : ${missingFields.join(", ")}.`,
+        {type: "error"}
+      );
+      return;
+    }
+    if (isOver18(record?.birth_date) && !nic) {
+      notify(
+        "Vous ne pouvez pas télécharger votre certificat de scolarité car vous êtes majeur et sans CIN.",
+        {type: "error"}
+      );
+      return;
+    }
+    return filesApi().getStudentScholarshipCertificate(studentId, {
       responseType: "arraybuffer",
     });
+  };
 
   return (
     <FileDownloader
