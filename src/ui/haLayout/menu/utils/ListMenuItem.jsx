bfab890a659b1e89adf657a6da1dcda1a42b60f3
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2025-02-10 21:41:22 +0300

    chore: refactor manager menu, ListMenuItem and SingleMenu to typescript file

--- src/ui/haLayout/menu/utils/ListMenuItem.jsx
+++
@@ -1,5 +0,0 @@
-import {SingleMenu} from "./SingleMenu";
-
-export const ListMenuItem = ({label, icon, to, target, ...rest}) => (
-  <SingleMenu {...{label, to, icon, menu: false, target, ...rest}} />
-);
