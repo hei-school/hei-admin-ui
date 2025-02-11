d57204f54ecf3a27cd6fe3394a4f2d1ef5559a1d
Lova Fenohasina Lala RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2025-02-11 17:55:46 +0300

    refactor: change env variable name

--- src/security/setting.js
+++ src/security/setting.js
@@ -1,33 +1,18 @@
-// Copyright 2022 The Casdoor Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import Sdk from "casdoor-js-sdk";
 
 export const ServerUrl = process.env.REACT_APP_SERVER_URL;
 
 const sdkConfig = {
-  serverUrl: process.env.REACT_APP_SDK_SERVER_URL,
-  clientId: process.env.REACT_APP_SDK_CLIENT_ID,
-  appName: process.env.REACT_APP_SDK_APP_NAME,
-  organizationName: process.env.REACT_APP_SDK_ORGANIZATION_NAME,
-  redirectPath: process.env.REACT_APP_SDK_REDIRECT_PATH,
+  serverUrl: process.env.REACT_APP_CASDOOR_SDK_SERVER_URL,
+  clientId: process.env.REACT_APP_CASDOOR_SDK_CLIENT_ID,
+  appName: process.env.REACT_APP_CASDOOR_SDK_APP_NAME,
+  organizationName: process.env.REACT_APP_CASDOOR_SDK_ORGANIZATION_NAME,
+  redirectPath: process.env.REACT_APP_CASDOOR_SDK_REDIRECT_PATH,
 };
 
 export const CasdoorSDK = new Sdk(sdkConfig);
 
 export const isLoggedIn = () => {
-  //const token = localStorage.getItem("token");
   const token = sessionStorage.getItem("bearerItem");
   return token !== null && token.length > 0;
 };
