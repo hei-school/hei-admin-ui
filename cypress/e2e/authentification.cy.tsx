d8df0cff139a96b35ca21b3cbc494bba0e414153
Lova Fenohasina Lala RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2025-02-12 16:59:23 +0300

    test: refactor

--- cypress/e2e/authentification.cy.tsx
+++ cypress/e2e/authentification.cy.tsx
@@ -1,4 +1,3 @@
-import {getStudent1Connected} from "../fixtures/api_mocks/authentification-mocks";
 import {student1Mock} from "../fixtures/api_mocks/students-mocks";
 
 describe("Authentification", () => {
@@ -8,7 +7,7 @@ describe("Authentification", () => {
       password: "bad password",
       success: false,
     });
-    cy.contains("Incorrect username or password", {timeout: 15000});
+    cy.contains("CONNEXION AVEC CASDOOR", {timeout: 15000});
   });
 
   it("should lands on profile page if succeeds", () => {
@@ -19,32 +18,5 @@ describe("Authentification", () => {
       .and("contain", student1Mock.address)
       .and("contain", student1Mock.email)
       .and("contain", student1Mock.phone);
-  });
-
-  it("permits user to renew password when it is forgotten", () => {
-    const {username, password} = getStudent1Connected();
-    cy.intercept(
-      "POST",
-      "https://cognito-idp.eu-west-3.amazonaws.com/",
-      (req) => {
-        req.reply({
-          CodeDeliveryDetails: {
-            AttributeName: "email",
-            DeliveryMedium: "EMAIL",
-            Destination: username,
-          },
-        });
-      }
-    ).as("awsRequest");
-    cy.visit("/login");
-    cy.contains("Mot de passe oublié?").click();
-    cy.getByTestid("mail_input").type(username);
-    cy.contains("ENVOYER").click();
-    cy.wait("@awsRequest");
-    cy.getByTestid("code_input").type("12345678");
-    cy.getByTestid("password_input").type(password.concat("&"));
-    cy.getByTestid("confirm_password_input").type(password.concat("&"));
-
-    cy.contains("RÉINITIALISER").click();
   });
 });
