01534e363549691766f531094cc51cf442cce0c0
FiantsoHarena <fiantsorav@gmail.com>
2025-02-22 11:48:19 +0300

    chore(ui): implement main UI for displaying exam participants' grades

--- src/operations/grades/ExamGradeList.tsx
+++ src/operations/grades/ExamGradeList.tsx
@@ -1,3 +1,27 @@
+import {HaList} from "@/ui/haList";
+import {Book} from "@mui/icons-material";
+import {Box} from "@mui/material";
+import {TextField} from "react-admin";
+
 export const ExamGradeList = () => {
-  return <h1>Exam Grades list</h1>;
+  return (
+    <Box>
+      <HaList
+        icon={<Book />}
+        resource="exams"
+        title="Liste des participants à l'examen Test"
+        datagridProps={{
+          rowClick: false,
+        }}
+        actions={false}
+      >
+        <TextField source="ref" label="Référence" />
+        <TextField source="last_name" label="Nom" />
+        <TextField source="first_name" label="Prénom(s)" />
+        <TextField source="score" label="Note" />
+        <TextField source="created_at" label="Créée le" />
+        <TextField source="updated_at" label="Mis à jour le" />
+      </HaList>
+    </Box>
+  );
 };
