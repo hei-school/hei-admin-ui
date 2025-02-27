62757c42cacd49e1b84e2e5a31614c312b7f19c1
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2025-02-27 11:34:50 +0300

    chore: update announcement card design to match Rindra Fenomanana's design

--- src/operations/announcements/components/EmailField.tsx
+++ src/operations/announcements/components/EmailField.tsx
@@ -1,5 +1,14 @@
+import {PALETTE_COLORS} from "@/haTheme";
 import {Link} from "react-router-dom";
 
 export const EmailField = ({value}: any) => (
-  <Link to={`mailto:${value}`}>{value}</Link>
+  <Link
+    to={`mailto:${value}`}
+    target="_blank"
+    style={{
+      color: PALETTE_COLORS.primary,
+    }}
+  >
+    {value}
+  </Link>
 );
