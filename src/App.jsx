f5932196a16a295919953eca98e4877c1467560c
Lova Fenohasina Lala RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2025-03-04 11:20:46 +0300

    refactor: casdoor callback route

--- src/App.jsx
+++ src/App.jsx
@@ -266,7 +266,10 @@ function App() {
       <BrowserRouter>
         <AwsWafCaptchaHandler />
         <Routes>
-          <Route path={process.env.REACT_APP_CASDOOR_SDK_REDIRECT_PATH} element={<CasdoorAuthCallback />} />
+          <Route
+            path={process.env.REACT_APP_CASDOOR_SDK_REDIRECT_PATH || "/auth/callback"}
+            element={<CasdoorAuthCallback />}
+          />
           <Route path="/human-verification" element={<HumanVerification />} />
           <Route path="*" element={<AppBase />} />
         </Routes>
