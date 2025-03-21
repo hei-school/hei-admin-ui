dfa5c60347b29313b3c3a65db1e4862e6b3cc5bb
fanomezana <hei.fanomezana.2@gmail.com>
2025-03-21 20:39:43 +0300

    chore: configure Cypress e2e test coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import codeCoverageTask from "@cypress/code-coverage/task";
 import {defineConfig} from "cypress";
 import dotenv from "dotenv";
 
@@ -24,7 +25,8 @@ export default defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      codeCoverageTask(on, config);
+      return config;
     },
     baseUrl: "http://localhost:5173/",
     requestTimeout: REQUEST_TIMEOUT,
