16852c553245e5e879dce5953d7597c0808d5a99
FiantsoHarena <fiantsorav@gmail.com>
2025-04-08 23:09:04 +0300

    chore: reorganize exam card list component

--- src/operations/exams/ExamList.tsx
+++ src/operations/exams/ExamList.tsx
@@ -1,181 +1,233 @@
-import {PALETTE_COLORS} from "@/haTheme";
-import {HaListTitle} from "@/ui/haList";
-import {PrevNextPagination} from "@/ui/haList/PrevNextPagination";
-import {CreateButton} from "@/ui/haToolbar";
-import {formatDate} from "@/utils/date";
-import {ExamInfo} from "@haapi/typescript-client";
 import {
   ImportContactsOutlined as BookIcon,
-  CalendarTodayOutlined as CalendarIcon,
-  GradeOutlined as GradeIcon,
   PeopleOutline as GroupIcon,
   PermIdentityOutlined as PersonIcon,
 } from "@mui/icons-material";
 import {
   Box,
-  Card,
   CardContent,
-  CardHeader,
   Divider,
-  Grid,
+  LinearProgress,
+  Skeleton,
+  Stack,
   Typography,
 } from "@mui/material";
-import {List, useListContext} from "react-admin";
-import {useNavigate} from "react-router-dom";
-import {FilterExam} from "./components";
+import {Clock} from "lucide-react";
+import {CSSProperties, FC, ReactNode} from "react";
+import {Link, List, useListContext} from "react-admin";
+
+import {PALETTE_COLORS} from "@/haTheme";
+import {useRole} from "@/security/hooks";
+import {ResponsiveGrid} from "@/ui/components";
+import {HaListTitle} from "@/ui/haList";
+import {PrevNextPagination} from "@/ui/haList/PrevNextPagination";
+import {CreateButton} from "@/ui/haToolbar";
+import {formatDate} from "@/utils/date";
+import {ExamInfo} from "@haapi/typescript-client";
+
+const cardStyle: CSSProperties = {
+  minWidth: "300px",
+  marginTop: "50px",
+  borderRadius: "7px",
+  overflow: "visible",
+  position: "relative",
+  borderTop: "5px solid",
+  borderBottom: "5px solid",
+  boxShadow: "0 0 10px rgb(182, 182, 182)",
+};
 
 export const ExamList = () => {
+  const {isManager, isAdmin, isTeacher} = useRole();
+
   return (
-    <Box>
-      <List
-        title="Examens"
-        resource="exams"
-        empty={false}
-        actions={false}
-        pagination={<PrevNextPagination />}
-        sx={{mt: 1}}
-      >
-        <FilterExam />
-        <Divider sx={{mt: 3, mb: 2}} />
-        <HaListTitle
-          title="Liste des examens"
-          icon={<GradeIcon />}
-          filterIndicator={true}
-          actions={
-            <Box>
-              <CreateButton resource="exams" />
-            </Box>
-          }
-          mainSearch={{label: "", source: ""}}
-        />
-        <ExamCard />
-      </List>
-    </Box>
+    <List
+      sx={{
+        "mt": 2,
+        "& .RaList-content": {
+          boxShadow: "none",
+          backgroundColor: "none",
+        },
+      }}
+      title="Liste des examens"
+      perPage={12}
+      actions={false}
+      empty={false}
+      pagination={<PrevNextPagination />}
+    >
+      <ExamLoader />
+      <HaListTitle
+        filterIndicator
+        title="Liste des examens"
+        mainSearch={{source: "", label: ""}}
+        icon={<BookIcon />}
+        actions={(isManager() || isAdmin() || isTeacher()) && <ExamActions />}
+      />
+      <ExamsGridContent />
+    </List>
   );
 };
 
-export const ExamCard = () => {
-  const {data: exams = []} = useListContext();
-  const navigate = useNavigate();
+const ExamLoader = () => {
+  const {isLoading} = useListContext();
+  return isLoading && <LinearProgress />;
+};
+
+const ExamActions = () => (
+  <Box>
+    <CreateButton resource="exams" />
+  </Box>
+);
+
+const ExamsGridContent = () => {
+  const {data: exams = [], isLoading} = useListContext();
 
   return (
-    <Box sx={{padding: "20px", minHeight: "100vh"}}>
-      <Grid container direction="row" spacing={4}>
-        {exams.map((exam: ExamInfo) => (
-          <Grid item xs={12} sm={6} md={4} lg={4}>
-            <Card
-              sx={{
-                "borderRadius": "12px",
-                "boxShadow": "0 4px 16px rgba(0, 0, 0, 0.05)",
-                "margin": "20px 0",
-                "padding": "16px",
-                "background": "linear-gradient(135deg, #f8f9fa, #f1f3f5)",
-                "transition": "0.3s",
-                "cursor": "pointer",
-                "&:hover": {
-                  boxShadow: "0 8px 24px rgba(0, 0, 0, 0.1)",
-                  textDecoration: "underline",
-                },
-                "width": "100%",
-              }}
-              onClick={() => navigate(`/exams/${exam.id}/grades`)}
-            >
-              <CardHeader
-                sx={{
-                  borderRadius: "15px",
-                }}
-                title={
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      color: "#005b87",
-                    }}
-                  >
-                    <Typography
-                      variant="h6"
-                      sx={{
-                        fontWeight: "bold",
-                      }}
-                    >
-                      {exam.title}
-                    </Typography>
-                    <Box
-                      sx={{
-                        backgroundColor: "#005b87",
-                        color: "white",
-                        padding: "4px 8px",
-                        borderRadius: "6px",
-                        fontSize: "0.875rem",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Coef. {exam.coefficient}
-                    </Box>
-                  </Box>
-                }
-              />
-              <CardContent sx={{padding: "16px"}}>
-                <Typography
-                  sx={{
-                    marginBottom: "12px",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "0.9rem",
-                  }}
-                >
-                  <PersonIcon sx={{fontSize: "1.5rem", marginRight: "10px"}} />
-                  Enseignant: {
-                    exam.awarded_course?.main_teacher?.first_name
-                  }{" "}
-                  {exam.awarded_course?.main_teacher?.last_name}
-                </Typography>
-                <Typography
-                  sx={{
-                    marginBottom: "12px",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "0.9rem",
-                  }}
-                >
-                  <BookIcon sx={{fontSize: "1.5rem", marginRight: "10px"}} />
-                  Cours: {exam.awarded_course?.course?.code}
-                </Typography>
-
-                <Typography
-                  sx={{
-                    marginBottom: "12px",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "0.9rem",
-                  }}
-                >
-                  <GroupIcon sx={{fontSize: "1.5rem", marginRight: "10px"}} />
-                  Groupe: {exam.awarded_course?.group?.ref}
-                </Typography>
-                <Typography
-                  sx={{
-                    marginBottom: "12px",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "0.9rem",
-                  }}
-                >
-                  <CalendarIcon
-                    sx={{
-                      fontSize: "1.5rem",
-                      marginRight: "10px",
-                      color: PALETTE_COLORS.red,
-                    }}
-                  />
-                  Date et heure: {formatDate(exam.examination_date)}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    <ResponsiveGrid gap="1.5rem">
+      {exams.map((exam: ExamInfo) => (
+        <ExamCard key={exam.id} exam={exam} isLoading={isLoading} />
+      ))}
+    </ResponsiveGrid>
   );
 };
+
+const ExamCard: FC<{exam: ExamInfo; isLoading: boolean}> = ({
+  exam,
+  isLoading,
+}) => (
+  <Link
+    to={`/exams/${exam.id}/grades`}
+    sx={{
+      ...cardStyle,
+      "width": "100%",
+      "maxWidth": "100%",
+      "marginBottom": "1rem",
+      "boxSizing": "border-box",
+      "transition": "0.3s all ease-in-out",
+      "borderColor": PALETTE_COLORS.primary,
+      "&:hover": {transform: "scale(1.05)"},
+    }}
+  >
+    <CardContent component="div">
+      {isLoading && <Skeleton variant="rectangular" width={100} height={100} />}
+      <Box
+        sx={{
+          backgroundColor: PALETTE_COLORS.primary,
+          borderRadius: "7px 7px 0 0",
+        }}
+      />
+      <Box>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography
+            variant="h6"
+            fontWeight="bold"
+            color={PALETTE_COLORS.primary}
+            noWrap
+            padding="1rem"
+            textTransform="capitalize"
+          >
+            {exam.title}
+          </Typography>
+          <ExamTag
+            label={`Coef: ${exam.coefficient}`}
+            color={PALETTE_COLORS.yellow}
+          />
+        </Stack>
+
+        <Box sx={{px: "1rem", pb: "1rem"}}>
+          <Stack direction="row" alignItems="center" spacing={1} mb={2}>
+            <Clock width={20} />
+            <Typography variant="subtitle1">
+              {formatDate(exam.examination_date)}
+            </Typography>
+          </Stack>
+        </Box>
+        <Divider />
+        <ExamDetails exam={exam} />
+      </Box>
+    </CardContent>
+  </Link>
+);
+
+const ExamDetails = ({exam}: {exam: ExamInfo}) => (
+  <Stack direction="column" spacing={1} mt={2}>
+    <InfoRow
+      icon={<BookIcon sx={{fontSize: "1.5rem", mr: "5px"}} />}
+      label="Cours:"
+      value={exam?.awarded_course?.course?.code}
+      color="#f4e1ac"
+      textColor={PALETTE_COLORS.black}
+    />
+    <InfoRow
+      icon={<GroupIcon sx={{fontSize: "1.5rem", mr: "5px"}} />}
+      label="Groupe:"
+      value={exam?.awarded_course?.group?.ref}
+    />
+    <InfoRow
+      icon={<PersonIcon sx={{fontSize: "1.5rem", mr: "5px"}} />}
+      label="Enseignant:"
+      value={`${exam?.awarded_course?.main_teacher?.first_name ?? ""} ${exam?.awarded_course?.main_teacher?.last_name ?? ""}`}
+      color="#f4e1ac"
+      textColor={PALETTE_COLORS.black}
+    />
+  </Stack>
+);
+
+const InfoRow = ({
+  icon,
+  label,
+  value,
+  color = "primary.light",
+  textColor = "white",
+}: {
+  icon: ReactNode;
+  label: string;
+  value?: string;
+  color?: string;
+  textColor?: string;
+}) => (
+  <Box display="flex" justifyContent="space-between">
+    <Typography display="flex" alignItems="center">
+      {icon} {label}
+    </Typography>
+    <Box
+      bgcolor={color}
+      color={textColor}
+      fontSize="14px"
+      fontWeight="bold"
+      borderRadius={20}
+      px={1}
+      py="3px"
+      display="flex"
+      alignItems="center"
+      whiteSpace="nowrap"
+      textTransform="uppercase"
+      textOverflow="ellipsis"
+      width="fit-content"
+    >
+      {value}
+    </Box>
+  </Box>
+);
+
+const ExamTag = ({label, color}: {label: string; color: string}) => (
+  <Box
+    bgcolor={color}
+    color="white"
+    fontSize="14px"
+    fontWeight="bold"
+    borderRadius={20}
+    px={1.5}
+    py="3px"
+    whiteSpace="nowrap"
+    overflow="hidden"
+    textOverflow="ellipsis"
+    display="inline-block"
+    minWidth="fit-content"
+  >
+    {label}
+  </Box>
+);
