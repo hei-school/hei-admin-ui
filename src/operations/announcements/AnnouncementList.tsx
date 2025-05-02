18004542f59a8fb8e5cedc115d8b40aeed39b7f0
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2025-05-02 11:29:38 +0300

    chore: extract annoncement card to component

--- src/operations/announcements/AnnouncementList.tsx
+++ src/operations/announcements/AnnouncementList.tsx
@@ -7,31 +7,18 @@ import {CreateButton} from "@/ui/haToolbar";
 import {Announcement, Scope} from "@haapi/typescript-client";
 import {
   Newspaper as AnnouncementIcon,
-  Campaign,
   FilterList,
   People,
   Public,
   School,
   Work,
 } from "@mui/icons-material";
-import {
-  Avatar,
-  Box,
-  Card,
-  CardMedia,
-  Chip,
-  LinearProgress,
-  Paper,
-  Skeleton,
-  Tooltip,
-  Typography,
-} from "@mui/material";
+import {Box, Chip, LinearProgress, Paper, Tooltip} from "@mui/material";
 import React, {FC, useState} from "react";
-import {Link, List, useListContext, useListFilterContext} from "react-admin";
+import {List, useListContext, useListFilterContext} from "react-admin";
 import {AnnouncementFilter} from "./components";
-import {EmailField} from "./components/EmailField";
+import {AnnouncementCard} from "./components/AnnoucementCard";
 import {ANNOUNCEMENT_SCOPE} from "./utils/constants/announcementsScopes";
-import {getBgImg} from "./utils/getBgImg";
 
 const cardStyle: React.CSSProperties = {
   minWidth: "300px",
@@ -62,113 +49,15 @@ const AnnouncementsGrid = () => {
   return (
     <ResponsiveGrid gap="1.5rem">
       {announcements.map((announcement: Announcement) => (
-        <Link
+        <AnnouncementCard
           key={announcement.id}
-          to={`/announcements/${announcement.id}/show`}
-          sx={{
-            ...cardStyle,
-            "borderColor": getChipColor(announcement?.scope!),
-            "&:hover": {transform: "scale(1.05)"},
-            "maxWidth": "100%",
-            "width": "100%",
-            "boxSizing": "border-box",
-            "marginBottom": "1rem",
-          }}
-        >
-          <Card component="div">
-            {isLoading ? (
-              <Skeleton variant="rectangular" width={100} height={100} />
-            ) : (
-              <CardMedia
-                component="img"
-                image={getBgImg(announcement?.scope!)}
-                alt="Announcement Background"
-                sx={{
-                  borderRadius: "50%",
-                  height: "100px",
-                  width: "100px",
-                  marginLeft: "1.5rem",
-                  position: "absolute",
-                  top: "-45px",
-                  border: "4px solid",
-                  borderColor: getChipColor(announcement?.scope!),
-                }}
-              />
-            )}
-            <Box
-              sx={{
-                backgroundColor: getChipColor(announcement?.scope!),
-                height: "60px",
-                padding: "1rem",
-                borderRadius: "7px 7px 0 0",
-              }}
-            />
-            <Box>
-              <Typography
-                variant="h6"
-                fontWeight="bold"
-                noWrap
-                textOverflow="ellipsis"
-                padding="1rem"
-              >
-                {announcement.title}
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  padding: "1rem",
-                  position: "relative",
-                }}
-              >
-                <Campaign
-                  sx={{
-                    color: getChipColor(announcement?.scope!),
-                    fontSize: "2.5rem",
-                    position: "absolute",
-                    top: 0,
-                    transform: "rotate(-10deg)",
-                  }}
-                />
-                <Typography sx={{textIndent: "2.5rem"}}>
-                  Cliquez sur la carte pour accéder à l'annonce complète et
-                  découvrir tous les détails pertinents.
-                </Typography>
-              </Box>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                marginInline="1rem"
-                paddingBlock="1rem"
-                borderTop="1px solid rgba(100, 100, 100, 0.3)"
-              >
-                <Box>
-                  <EmailField value={announcement.author?.email} />
-                  <Typography>
-                    {new Date(
-                      announcement?.creation_datetime!
-                    ).toLocaleString()}
-                  </Typography>
-                </Box>
-                {isLoading ? (
-                  <Skeleton variant="circular" width={50} height={50} />
-                ) : (
-                  <Avatar
-                    alt={announcement.author?.first_name}
-                    src={announcement.author?.profile_picture}
-                    sx={{
-                      height: 50,
-                      width: 50,
-                      border: "2px solid",
-                      borderColor: getChipColor(announcement?.scope!),
-                    }}
-                  />
-                )}
-              </Box>
-            </Box>
-          </Card>
-        </Link>
+          id={announcement.id!}
+          scope={announcement.scope!}
+          author={announcement.author!}
+          creation_datetime={announcement.creation_datetime!}
+          isLoading={isLoading}
+          title={announcement.title!}
+        />
       ))}
     </ResponsiveGrid>
   );
