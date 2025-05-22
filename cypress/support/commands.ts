77af66f0477044fb5130b4e64d76199264640928
NyAndoMayah <hei.mayah.3@gmail.com>
2025-05-22 20:31:38 +0300

    fix: cy wait for getRedirectionURL

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -95,7 +95,7 @@ Cypress.Commands.add("login", (options: LoginConfig) => {
 
   cy.get('[data-testid="casdoor-login-btn"]', {timeout: 15000}).click();
 
-  cy.wait("@getRedirectionURL");
+  cy.wait("@getRedirectionURL", {timeout: 15000});
 
   if (!isSuccess) {
     cy.visit(`/auth/callback?code=${role}&state=HEI Admin`);
