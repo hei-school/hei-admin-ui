07328e461d3cb1b6d3a76ed8c847032a7a2934fe
Fiantso Harena <fiantsorav@gmail.com>
2025-06-17 14:18:57 +0300

    test: when getting fees for a specific student

--- cypress/e2e/providers/fee.provider.cy.ts
+++ cypress/e2e/providers/fee.provider.cy.ts
@@ -1,10 +1,10 @@
-import {
+import feeProvider, {
   feeIdFromRaId,
   studentIdFromRaId,
   toApiIds,
   toRaId,
 } from "@/providers/feeProvider";
-import {fee1Mock} from "../../fixtures/api_mocks/fees-mocks";
+import {fee1Mock, feesMock} from "../../fixtures/api_mocks/fees-mocks";
 import {student1Mock} from "../../fixtures/api_mocks/students-mocks";
 
 describe("Fee Provider", () => {
@@ -39,6 +39,32 @@ describe("Fee Provider", () => {
       it("should extract fee ID from RA ID", () => {
         const result = feeIdFromRaId(raId);
         expect(result).to.equal(feeId);
+      });
+    });
+  });
+
+  describe("getList()", () => {
+    context("when getting fees for a specific student", () => {
+      it("should get fees correctly", () => {
+        const page = 1;
+        const perPage = 10;
+        const filter = {studentId: studentId};
+
+        cy.intercept(
+          "GET",
+          `/students/${studentId}/fees?page=${page}&page_size=${perPage}`,
+          feesMock
+        ).as("getStudentFees");
+        feeProvider.getList(page, perPage, filter).then((result) => {
+          cy.wait("@getStudentFees");
+          expect(result.data).to.have.length(feesMock.length);
+          result.data.forEach((fee, index) => {
+            expect(fee.id).to.equal(
+              toRaId(feesMock[index].student_id!, feesMock[index].id!)
+            );
+            expect(fee).to.include(feesMock[index]);
+          });
+        });
       });
     });
   });
