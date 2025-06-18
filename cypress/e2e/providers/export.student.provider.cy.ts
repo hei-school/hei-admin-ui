01ae939cab502850ddbc42d5ef79256f0ed8d54b
Fiantso Harena <fiantsorav@gmail.com>
2025-06-18 15:48:02 +0300

    test: edge case should handle undefined id

--- cypress/e2e/providers/export.student.provider.cy.ts
+++ cypress/e2e/providers/export.student.provider.cy.ts
@@ -1,6 +1,9 @@
+import {usersApi} from "@/providers/api";
 import exportStudentProvider from "@/providers/exportStudentProvider.ts";
 
 describe("Export Student Provider", () => {
+  const mockStudentId = "student-123";
+
   describe("unimplemented methods", () => {
     it("getList() should throw error", () => {
       expect(() => exportStudentProvider.getList(1, 10, {})).to.throw(
@@ -18,6 +21,34 @@ describe("Export Student Provider", () => {
       expect(() => exportStudentProvider.delete("")).to.throw(
         "Not implemented"
       );
+    });
+  });
+
+  describe("edge cases", () => {
+    it("should handle undefined id", () => {
+      exportStudentProvider.getOne("", {}).catch((err) => {
+        expect(err.message).to.include("Cannot read properties");
+      });
+    });
+
+    it("should handle large file responses", () => {
+      const largeBuffer = new ArrayBuffer(1024 * 1024 * 5);
+      cy.stub(usersApi(), "generateStudentsInXlsx").resolves({
+        data: largeBuffer,
+      });
+      exportStudentProvider.getOne(mockStudentId, {}).then((result) => {
+        expect(result.file.byteLength).to.equal(1024 * 1024 * 5);
+      });
+    });
+
+    it("should handle empty file responses", () => {
+      const emptyBuffer = new ArrayBuffer(0);
+      cy.stub(usersApi(), "generateStudentsInXlsx").resolves({
+        data: emptyBuffer,
+      });
+      exportStudentProvider.getOne(mockStudentId, {}).then((result) => {
+        expect(result.file.byteLength).to.equal(0);
+      });
     });
   });
 });
