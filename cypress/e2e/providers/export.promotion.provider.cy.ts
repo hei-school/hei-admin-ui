479c2b5070414c40e31d9f2e32c8fefb145af407
Fiantso Harena <fiantsorav@gmail.com>
2025-06-18 14:27:57 +0300

    test: should generate and return student list for a promotion

--- cypress/e2e/providers/export.promotion.provider.cy.ts
+++ cypress/e2e/providers/export.promotion.provider.cy.ts
@@ -1,6 +1,28 @@
+import {promotionApi} from "@/providers/api";
 import exportPromotionProvider from "@/providers/exportPromotionProvider";
 
 describe("Export Promotion Provider", () => {
+  const mockPromotionId = "promotion-123";
+  const mockArrayBuffer = new ArrayBuffer(8);
+  const mockResponse = {data: mockArrayBuffer};
+
+  beforeEach(() => {
+    cy.stub(promotionApi(), "getStudentsByPromotion").resolves(mockResponse);
+  });
+
+  describe("getOne()", () => {
+    it("should generate and return student list for a promotion", () => {
+      exportPromotionProvider.getOne(mockPromotionId).then((result) => {
+        expect(promotionApi().getStudentsByPromotion).to.have.been.calledWith(
+          mockPromotionId,
+          {responseType: "arraybuffer"}
+        );
+        expect(result.id).to.equal(mockPromotionId);
+        expect(result.file).to.equal(mockArrayBuffer);
+      });
+    });
+  });
+
   describe("unimplemented methods", () => {
     it("getList() should throw not implemented error", () => {
       expect(() => exportPromotionProvider.getList(1, 10, {})).to.throw(
