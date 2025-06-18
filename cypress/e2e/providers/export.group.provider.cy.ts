90dabb1942550c5d1daf004f7880a634284d4ffe
Fiantso Harena <fiantsorav@gmail.com>
2025-06-18 14:02:15 +0300

    test: should generate and return an XLSX file for group students

--- cypress/e2e/providers/export.group.provider.cy.ts
+++ cypress/e2e/providers/export.group.provider.cy.ts
@@ -1,1 +1,44 @@
-describe("Export Group Provider", () => {});
+import {teachingApi} from "@/providers/api";
+import exportGroupProvider from "@/providers/exportGroupProvider";
+
+describe("Export Group Provider", () => {
+  const mockGroupId = "group-123";
+  const mockArrayBuffer = new ArrayBuffer(8);
+  const mockResponse = {data: mockArrayBuffer};
+
+  beforeEach(() => {
+    cy.stub(teachingApi(), "generateStudentsGroupInXlsx").resolves(
+      mockResponse
+    );
+  });
+
+  describe("getOne()", () => {
+    it("should generate and return an XLSX file for group students", () => {
+      exportGroupProvider.getOne(mockGroupId).then((result) => {
+        expect(
+          teachingApi().generateStudentsGroupInXlsx
+        ).to.have.been.calledWith(mockGroupId, {responseType: "arraybuffer"});
+        expect(result.id).to.equal(mockGroupId);
+        expect(result.file).to.equal(mockArrayBuffer);
+      });
+    });
+  });
+
+  describe("unimplemented methods", () => {
+    it("getList() should throw not implemented error", () => {
+      expect(() => exportGroupProvider.getList(1, 10, {})).to.throw(
+        "Not implemented"
+      );
+    });
+
+    it("saveOrUpdate() should throw not implemented error", () => {
+      expect(() => exportGroupProvider.saveOrUpdate({})).to.throw(
+        "Not implemented"
+      );
+    });
+
+    it("delete() should throw not implemented error", () => {
+      expect(() => exportGroupProvider.delete("")).to.throw("Not implemented");
+    });
+  });
+});
