43fb8d43ec59e186731ed6f161076e7d66d4e662
Fiantso Harena <fiantsorav@gmail.com>
2025-06-18 16:54:34 +0300

    test: should call generateTeachersInXlsx with correct parameters

--- cypress/e2e/providers/export.teacher.provider.cy.ts
+++ cypress/e2e/providers/export.teacher.provider.cy.ts
@@ -1,6 +1,25 @@
+import {usersApi} from "@/providers/api";
 import exportTeacherProvider from "@/providers/exportTeacherProvider";
 
 describe("Export Teacher Provider", () => {
+  const mockTeacherId = "teacher-123";
+  const mockArrayBuffer = new ArrayBuffer(8);
+  const mockResponse = {data: mockArrayBuffer};
+
+  beforeEach(() => {
+    cy.stub(usersApi(), "generateTeachersInXlsx").resolves(mockResponse);
+  });
+
+  describe("getOne()", () => {
+    it("should call generateTeachersInXlsx with correct parameters", () => {
+      exportTeacherProvider.getOne(mockTeacherId).then(() => {
+        expect(usersApi().generateTeachersInXlsx).to.have.been.calledWith({
+          responseType: "arraybuffer",
+        });
+      });
+    });
+  });
+
   describe("unimplemented methods", () => {
     it("getList should throw error", () => {
       expect(() => exportTeacherProvider.getList(1, 10, {})).to.throw(
