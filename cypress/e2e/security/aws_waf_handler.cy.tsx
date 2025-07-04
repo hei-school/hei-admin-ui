a969d48339c0d2571d4cc2737fea2ed542bbea1c
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2025-07-04 11:01:29 +0300

    fix: resolve public link none display on prod

--- cypress/e2e/security/aws_waf_handler.cy.tsx
+++ cypress/e2e/security/aws_waf_handler.cy.tsx
@@ -1,3 +1,5 @@
+import {calendarMock} from "../../fixtures/api_mocks/calendar-mock";
+
 describe("Aws waf handler", () => {
   it("should display captcha dialog on status: 405", () => {
     cy.mockLogin({
@@ -17,5 +19,20 @@ describe("Aws waf handler", () => {
     cy.location("pathname").should("include", "human-verification");
 
     cy.getByTestid("aws-waf-captcha-dialog").should("be.visible");
+  });
+});
+
+describe("Public link display calendar", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "events?page=1&page_size=100**", calendarMock).as(
+      "getEvents"
+    );
+  });
+
+  it("their is event", () => {
+    cy.visit("/calendar");
+    cy.wait("@getEvents");
+    cy.get(".rbc-event").should("have.length.at.least", 1);
+    cy.contains("F").should("exist");
   });
 });
--- src/operations/public/Calendar.tsx
+++ src/operations/public/Calendar.tsx
@@ -1,6 +1,12 @@
 import moment from "moment";
 import {useEffect, useState} from "react";
-import {Calendar, momentLocalizer, View, Views} from "react-big-calendar";
+import {
+  Calendar,
+  DateRange,
+  momentLocalizer,
+  View,
+  Views,
+} from "react-big-calendar";
 
 import {
   dateFormats,
@@ -10,6 +16,7 @@ import {
   transformApiDataToCalendarEvents,
 } from "@/operations/public/utils";
 
+import {toISO} from "@/utils/date";
 import {Event} from "@haapi/typescript-client";
 import {Box, CircularProgress, Typography} from "@mui/material";
 import axios from "axios";
@@ -25,27 +32,55 @@ export default function CalendarView() {
   const ITEM_PER_PAGE = 100;
   const [view, setView] = useState<View>(Views.WEEK);
   const [events, setEvents] = useState<Event[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const fetchEvents = async () => {
+  const formatDateForApi = (date: Date) =>
+    encodeURIComponent(toISO(date).replace(".000Z", "+03:00"));
+
+  const fetchEvents = async (startDate: Date, endDate: Date) => {
+    setLoading(true);
     try {
+      const from = formatDateForApi(startDate);
+      const to = formatDateForApi(endDate);
+
       const {data} = await axios.get<Event[]>(
-        `${API_URL}/events?page=1&page_size=${ITEM_PER_PAGE}`
+        `${API_URL}events?page=1&page_size=${ITEM_PER_PAGE}&from=${from}&to=${to}`
       );
       setEvents(data);
     } catch (error) {
-      console.error("Error fetching events:", error);
+      console.error("Erreur lors du chargement des événements :", error);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchEvents();
+    const today = new Date();
+    const startOfWeek = moment(today).startOf("week").toDate();
+    const endOfWeek = moment(today).endOf("week").toDate();
+    fetchEvents(startOfWeek, endOfWeek);
   }, []);
 
   const handleOnChangeView = (selectedView: View) => {
     setView(selectedView);
+  };
+
+  const handleRangeChange = (range: Date[] | DateRange) => {
+    let start: Date;
+    let end: Date;
+
+    if (Array.isArray(range)) {
+      start = range[0];
+      end = range[range.length - 1];
+    } else if (range.start && range.end) {
+      start = range.start;
+      end = range.end;
+    } else {
+      console.warn("Plage inattendue reçue :", range);
+      return;
+    }
+
+    fetchEvents(start, end);
   };
 
   const calendarEvents = transformApiDataToCalendarEvents(events);
@@ -70,7 +105,7 @@ export default function CalendarView() {
         >
           <CircularProgress sx={{color: "white"}} />
           <Typography variant="h6" color="white">
-            Chargement des évènements
+            Chargement des événements
           </Typography>
         </Box>
       )}
@@ -83,8 +118,9 @@ export default function CalendarView() {
         views={[Views.MONTH, Views.WEEK, Views.DAY]}
         style={{height: "100vh"}}
         onView={handleOnChangeView}
-        min={new Date(2025, 1, 0, 8, 0, 0)}
-        max={new Date(2025, 1, 0, 18, 0, 0)}
+        onRangeChange={handleRangeChange}
+        min={new Date(0, 1, 0, 8, 0, 0)}
+        max={new Date(0, 1, 0, 18, 0, 0)}
         eventPropGetter={eventStyleGetter}
         dayPropGetter={dayPropGetter}
         messages={frenchMessages}
