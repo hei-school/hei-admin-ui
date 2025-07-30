1e50684332fd794b5a5c46a69a30b48fe225a498
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2025-07-30 16:58:28 +0300

    chore: add student group on monitor list students and refactor to tsx

--- src/operations/monitors/component/MonitorStudentList.tsx
+++ src/operations/monitors/component/MonitorStudentList.tsx
@@ -1,13 +1,13 @@
 import {PALETTE_COLORS} from "@/haTheme";
 import authProvider from "@/providers/authProvider";
-import {useRole} from "@/security/hooks";
 import {HaList} from "@/ui/haList";
 import {CreateButton} from "@/ui/haToolbar";
+import {Group} from "@haapi/typescript-client";
 import {GroupOutlined} from "@mui/icons-material";
-import {ShowButton, TextField} from "react-admin";
+import {Avatar} from "@mui/material";
+import {FunctionField, ShowButton, TextField} from "react-admin";
 
 function MonitorStudentList() {
-  const role = useRole();
   const {id: monitorId} = authProvider.getCachedWhoami();
 
   return (
@@ -16,7 +16,9 @@ function MonitorStudentList() {
       icon={<GroupOutlined />}
       title="Liste des étudiants"
       mainSearch={{label: "Prénom·s", source: "first_name"}}
-      actions={<CreateButton data-testid="create-button" />}
+      actions={
+        <CreateButton resource="monitor-students" data-testid="create-button" />
+      }
       listProps={{
         queryOptions: {
           meta: {
@@ -25,9 +27,24 @@ function MonitorStudentList() {
         },
       }}
     >
+      <FunctionField
+        label="Profil"
+        render={(record) => <Avatar src={record.profile_picture} />}
+      />
       <TextField source="ref" label="Référence" />
       <TextField source="first_name" label="Prénom·s" />
       <TextField source="last_name" label="Nom·s" />
+      <FunctionField
+        label="Groupe"
+        render={(record) => {
+          const groups = record?.groups;
+          return groups && groups.length > 0 ? (
+            <span>{groups.map((group: Group) => group.ref).join(", ")}</span>
+          ) : (
+            <span style={{color: PALETTE_COLORS.red}}>Aucun groupe</span>
+          );
+        }}
+      />
       <ShowButton sx={{color: PALETTE_COLORS.yellow}} />
     </HaList>
   );
