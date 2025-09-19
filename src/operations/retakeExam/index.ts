66f0f13e6873f80e18fc6f9cc30a442745feb469
mbomain <ma.andriamparani@gmail.com>
2025-09-19 23:35:24 +0300

    chore: reformat code

--- src/operations/retakeExam/index.ts
+++ src/operations/retakeExam/index.ts
@@ -1,7 +1,5 @@
 import {RetakeExamCreate} from "./RetakeExamCreate";
-import {RetakeExamList} from "./RetakeExamList ";
 
 export const retakeExam = {
-  list: RetakeExamList,
   create: RetakeExamCreate,
 };
