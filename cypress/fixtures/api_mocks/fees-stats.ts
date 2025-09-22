0e3f5fde1ee5eef23310d757492ba809dc55c0fc
mbomain <ma.andriamparani@gmail.com>
2025-09-22 10:39:09 +0300

    chore: reformat code

--- cypress/fixtures/api_mocks/fees-stats.ts
+++ cypress/fixtures/api_mocks/fees-stats.ts
@@ -1,4 +1,4 @@
-import { AdvancedFeesStatistics } from "@haapi-b0fc7615/typescript-client";
+import {AdvancedFeesStatistics} from "@haapi-b0fc7615/typescript-client";
 
 export const advancedStats: AdvancedFeesStatistics = {
   total_expected_fees_count: {
