92054c598725a0180f252b758f5a0777914a4977
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2025-12-18 15:16:37 +0300

    chore: refactor import grade

---
+++ src/providers/gradeImportProvider.ts
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
+import { HaDataProviderType } from "./HaDataProviderType";
+import { gradesApi } from "./api";
+
+const gradeImportProvider: HaDataProviderType = {
+
+    saveOrUpdate: async (_resources, meta) => {
+        const {file, comment, mode}= meta.data;
+        const examId = meta.meta.examId;
+
+        let response;
+        
+        console.log(meta)
+
+       if (mode === "UPDATE") {
+            response = await gradesApi().importStudentsExamGradeUpdated(
+                examId,
+                file.rawFile,
+                comment
+            );
+        } else{ 
+            response = await gradesApi().importStudentsExamGrade(
+                    examId,
+                file.rawFile
+                
+            );
+        }
+        return [
+            {
+                id: uuidv4(),
+                ...response.data,
+            },
+        ];
+    },
+
+    getList: () => {
+        throw new Error("Not implemented");
+    },
+
+    getOne: () => {
+        throw new Error("Not implemented");
+    },
+
+    delete: () => {
+        throw new Error("Not implemented");
+    },
+};
+
+export default gradeImportProvider;
