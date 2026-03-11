392d09961fec958498b874026ac63d8358d8d130
manitra <ma.andriamparani@gmail.com>
2026-03-11 20:44:34 +0300

    test: fee export

--- cypress/e2e/fees.export.cy.ts
+++ cypress/e2e/fees.export.cy.ts
@@ -5,20 +5,27 @@ import {unpaidFeeMock} from "../fixtures/api_mocks/fees-mocks";
 describe("Fees Export", () => {
   beforeEach(() => {
     cy.mockLogin({role: WhoamiRoleEnum.MANAGER});
-    cy.visit("/fees");
+
     cy.intercept("GET", `advanced-stats**`, advanceStatsMocks).as(
       "getAdvancedStats"
     );
+
     cy.intercept("GET", `/fees**`, {data: [unpaidFeeMock]}).as("getFees");
-    cy.intercept("GET", "/fees/raw?**", {
+
+    cy.intercept("GET", "/fees/export?**", {
       statusCode: 200,
+      body: "https://fake-presigned-url.com/file.xlsx",
+    }).as("exportFees");
+
+    cy.intercept("GET", "https://fake-presigned-url.com/**", {
+      statusCode: 200,
+      body: Cypress.Buffer.from("Fake Excel content", "utf-8"),
       headers: {
-        "content-disposition": 'attachment; filename="fees.xlsx"',
         "content-type":
           "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
       },
-      body: Cypress.Buffer.from("Fake Excel content", "utf-8"),
     }).as("downloadExcel");
+    cy.visit("/fees");
   });
 
   it("should open export dialog, select status, pick month, and export fees data", () => {
@@ -31,14 +38,25 @@ describe("Fees Export", () => {
 
     cy.get('input[type="month"]').clear().type("2024-07");
     cy.get('input[type="month"]').should("have.value", "2024-07");
+
     cy.get("input[disabled]").eq(0).should("have.value", "01/07/2024");
     cy.get("input[disabled]").eq(1).should("have.value", "31/07/2024");
+
     cy.getByTestid("download-button").click();
+
+    cy.wait("@exportFees");
+    cy.wait("@downloadExcel");
+
     const downloadedFile = "cypress/downloads/Liste frais En retard.xlsx";
     cy.readFile(downloadedFile).should("exist");
   });
 
   it("should show error message on export failure", () => {
+    cy.intercept("GET", "/fees/export?**", {
+      statusCode: 500,
+      body: {},
+    }).as("exportFeesError");
+
     cy.getByTestid("menu-list-action").click();
     cy.contains("Exporter").click();
     cy.contains("Exporter les frais au format XLSX").should("be.visible");
@@ -47,15 +65,12 @@ describe("Fees Export", () => {
     cy.get("li[data-value]").first().click();
 
     cy.get('input[type="month"]').clear().type("2024-07");
-    cy.get('input[type="month"]').should("have.value", "2024-07");
+
     cy.get("input[disabled]").eq(0).should("have.value", "01/07/2024");
     cy.get("input[disabled]").eq(1).should("have.value", "31/07/2024");
 
-    cy.intercept("GET", "/fees/raw?**", {
-      statusCode: 500,
-      body: {},
-    }).as("exportFeesError");
     cy.getByTestid("download-button").click();
+
     cy.wait("@exportFeesError");
     cy.contains(
       "Une erreur est survenue lors de l'exportation du fichier."
