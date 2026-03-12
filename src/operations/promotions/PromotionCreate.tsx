10a69cdc3d284e02abdf7f2b16a548154cb79662
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2026-03-12 13:34:32 +0300

    feat: add new select input when creating promotion

--- src/operations/promotions/PromotionCreate.tsx
+++ src/operations/promotions/PromotionCreate.tsx
@@ -1,7 +1,8 @@
 import {CrupdatePromotion} from "@haapi-b0fc7615/typescript-client";
-import {SimpleForm, TextInput, required} from "react-admin";
+import {SelectInput, SimpleForm, TextInput, required} from "react-admin";
 import {v4 as uuid} from "uuid";
 import {Create} from "../common/components";
+import {CYCLE_LEVEL_CHOICES} from "./utils/constant";
 
 export function PromotionCreate() {
   return (
@@ -19,6 +20,16 @@ export function PromotionCreate() {
           label="Référence"
           validate={required()}
           fullWidth
+        />
+        <SelectInput
+          source="cycle_level"
+          label="Niveau du cycle"
+          choices={CYCLE_LEVEL_CHOICES}
+          optionValue="id"
+          optionText="name"
+          fullWidth
+          required
+          emptyText="--Niveau du cycle--"
         />
       </SimpleForm>
     </Create>
---
+++ src/operations/promotions/utils/constant.ts
@@ -0,0 +1,7 @@
+import {CycleEnum} from "@haapi-b0fc7615/typescript-client";
+
+export const CYCLE_LEVEL_CHOICES = [
+  {id: CycleEnum.BACHELOR, name: "BACHELOR"},
+  {id: CycleEnum.MASTER, name: "MASTER"},
+  {id: CycleEnum.DOCTORATE, name: "DOCTORATE"},
+];
