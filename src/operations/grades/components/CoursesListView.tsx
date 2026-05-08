4affd536ec04b148e57aa5cbc2c76214ed9cceeb
manitra <ma.andriamparani@gmail.com>
2026-05-08 10:15:19 +0300

    feat add button for change grade in profile

--- src/operations/grades/components/CoursesListView.tsx
+++ src/operations/grades/components/CoursesListView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import {EmptyListComponent} from "@/operations/common/components/EmptyListComponent";
 import {ToRaRecord} from "@/providers/types";
 import {CourseResult} from "@haapi-b0fc7615/typescript-client";
